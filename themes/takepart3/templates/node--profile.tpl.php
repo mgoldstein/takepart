9d4941c6d697eb32bd91268c32b77fff700d84b4
Eric Mckenna <emckenna@phase2technology.com>
2011-05-24 17:24:44 -0400

    updated author feature with with for contributions after unified author changes. [#TPB-146] Build functionality for Author node pages

--- themes/takepart3/templates/node--profile.tpl.php
+++ themes/takepart3/templates/node--profile.tpl.php
@@ -2,14 +2,19 @@
   <div id='author_info'>
     <?php if (!empty($title)): ?>
       <h2><?php print $title ?></h2>
+    <?php endif; ?>
+    <?php if (!empty($node->field_profile_job_title)): ?>
       <h3><?php print $node->field_profile_job_title['und'][0]['safe_value'] ?></h3>
+    <?php endif; ?>
+    <?php if (!empty($node->field_author_links)): ?>
       <div class='author-links social-links wireframe'><?php print takepart3_dolinks($node->field_author_links['und']);?></div>
+    <?php endif; ?>
+    <?php if (!empty($node->body)): ?>
       <div class='auhor-bio wireframe'><?php print $node->body['und'][0]['safe_value'] ?></div>
     <?php endif; ?>
   </div>
   <div id='author-contributions'>
     <h2>Contributions</h2>
-    <?php print $node->nid; ?><br/>
-    <?php print views_embed_view('contributions', 'page', array($node->nid)); ?>
+    <?php print views_embed_view('author_contributions', 'page', array($node->nid)); ?>
   </div>
 </div>
