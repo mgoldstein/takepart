cc0b4e128663b610a8f8d1d8e082b2d72a4f653f
Eric Mckenna <emckenna@phase2technology.com>
2011-05-26 16:52:20 -0400

    updated hot topics and main menu with themeing

--- themes/takepart3/template.php
+++ themes/takepart3/template.php
@@ -21,3 +21,57 @@ function takepart3_dolinks($links_field) {
   return implode("<span class='delimiter'>|</span>", $links);
   
 }
+
+function takepart3_preprocess_page(&$variables) {
+
+  //debug($main_menu_data);
+  //debug(array_keys($main_menu_data));
+
+  $variables['top_nav']       = _render_tp3_main_menu();
+  $variables['hottopic_nav']  = _render_tp3_hottopics_menu();
+  return $variables;
+}
+
+/**
+ * Helper to output the custom HTML for out main menu.
+ */
+function _render_tp3_main_menu() {
+  $menu_data = menu_tree_page_data("main-menu");
+
+  $links = array();
+  foreach($menu_data as $menu_item) {
+    //debug($menu_item['link']);
+    $opts = array(
+      'attributes' => $menu_item['link']['options']['attributes'],
+    );
+    
+    $link = l($menu_item['link']['title'], $menu_item['link']['href'], $opts);
+    $links[] = "<li>". $link ."</li>";
+  }
+  
+  return "<ul id='top-nav'>" . implode($links) ."</ul>";
+}
+
+
+/**
+ * Helper to output the custom HTML for out hot topic menu.  
+ * could be refactored to just one menu themer, but since the class and such were
+ * sllightly different, waiting to see how the submenus pan out.
+ */
+function _render_tp3_hottopics_menu() {
+  $menu_data = menu_tree_page_data("menu-hot-topics");
+
+  $links = array( 0 => "<li class='title'>hot topics:</li>" );
+  
+  foreach($menu_data as $menu_item) {
+    //debug($menu_item['link']);
+    $opts = array(
+      'attributes' => $menu_item['link']['options']['attributes'],
+    );
+    
+    $link = l($menu_item['link']['title'], $menu_item['link']['href'], $opts);
+    $links[] = "<li>". $link ."</li>";
+  }
+  
+  return "<ul class='clearfix'>" . implode($links) ."</ul>";
+}
