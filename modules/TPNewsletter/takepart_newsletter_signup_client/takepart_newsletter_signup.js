fd469d8015c8dc98becf02a8a6196f9e714b7ef0
Eric Mckenna <emckenna@phase2technology.com>
2011-07-05 15:13:00 -0400

    added in service

--- modules/TPNewsletter/takepart_newsletter_signup_client/takepart_newsletter_signup.js
+++ modules/TPNewsletter/takepart_newsletter_signup_client/takepart_newsletter_signup.js
@@ -1,11 +1,11 @@
 
   function takepart_newsletter_isValidEmailAddress(emailAddress, formId) {
-      
     var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
     var isValid = pattern.test(emailAddress);
     
     if (!isValid) {
       jQuery("#takepart-newsletter-"+formId+"-body .form-messages").text("Email address is invalid.");
+
     }
     
     return isValid;
