2118e6651299ad3d3dc3d71c82240e68b59915ad
Erik Summerfield <esummerfield@phase2technology.com>
2011-08-01 12:46:26 -0400

    [#TPB-557] Implement sidebar blocks on photo gallery page

---
+++ modules/features/takepart_custom/takepart_custom.ss_controls.js
@@ -0,0 +1,10 @@
+
+(function ($) {
+  Drupal.behaviors.ss_controls = { 
+    attach : function(context) {
+    $(".views-slideshow-controls-top").appendTo($("#ss_controls"));
+    $(".views-slideshow-controls-bottom").appendTo($("#ss_controls"));
+    },
+  };
+})(jQuery);
+
