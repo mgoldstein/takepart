a107f0c3bcb304e6e2c7d70be550ca173fd96357
Erik Summerfield <esummerfield@phase2technology.com>
2011-08-05 14:57:48 -0400

    [#TPB-375] Allow Galleries to be embedded using Node Embed

--- themes/takepart3/templates/node--embed.tpl.php
+++ themes/takepart3/templates/node--embed.tpl.php
@@ -1,3 +1,5 @@
+
+<?php if (!empty($pre_object)) print render($pre_object) ?>
 <div class='<?php print $classes ?> clearfix node-embedded' <?php print ($attributes) ?>>
   <?php if (!empty($title)): ?>
     <h2 <?php if (!empty($title_attributes)) print $title_attributes ?>>
@@ -7,3 +9,4 @@
   <?php print render($content); ?>
 
 </div>
+<?php if (!empty($post_object)) print render($post_object) ?>
