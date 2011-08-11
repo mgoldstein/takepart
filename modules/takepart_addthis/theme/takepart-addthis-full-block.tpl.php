a8cc2a72acddb028ec97bb8edf6de5e8536e1496
James O'Beirne <jobeirne@phase2technology.com>
2011-08-11 19:23:50 -0400

    Adding takepart_addthis module.

---
+++ modules/takepart_addthis/theme/takepart-addthis-full-block.tpl.php
@@ -0,0 +1,14 @@
+<!-- AddThis Button BEGIN -->
+<div class="takepart_addthis_leftpanel clearfix">
+  <div class="addthis_toolbox addthis_default_style">
+    <a class="addthis_button_google_plusone"></a>
+    <a class="addthis_button_facebook_like" fb:like:layout="button_count"></a>
+    <a class="addthis_button_tweet"></a>
+  </div>
+  <div class="addthis_toolbox">
+     <div class="custom_images"><?php print $variables['addthis_custom_buttons'] ?></div>
+  </div>
+</div>
+<script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=<?php print $variables['addthis_pubid'] ?>"></script>
+<!-- AddThis Button END -->
+
