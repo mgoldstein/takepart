11fa843072d8771e54403e6dfda6c218c58bbe65
Tracy Smith <tsmith@phase2technology.com>
2011-08-14 19:04:13 -0400

    BugzID: 16545 - added tweet options and tweaked facebook settings

--- modules/takepart_addthis/theme/takepart-addthis-footer-block.tpl.php
+++ modules/takepart_addthis/theme/takepart-addthis-footer-block.tpl.php
@@ -2,7 +2,7 @@
 <div class="takepart_addthis_footer">
   <div class="addthis_toolbox addthis_default_style">
       <a class="addthis_button_facebook"></a>
-      <a class="addthis_button_twitter"></a>
+      <a class="addthis_button_twitter" tw:via="<?php print $variables['takepart_addthis_tweet_via']; ?>" ></a>
       <a class="addthis_button_google_plusone"></a>
       <a class="addthis_button_stumbleupon"></a>
       <a class="addthis_button_digg"></a>
