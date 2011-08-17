15023b184ee41508ff1d5ce45c5a4297121082ef
Tracy Smith <tsmith@phase2technology.com>
2011-08-17 02:18:13 -0400

    BugzID: 16994 - Moved order of social share icons

--- modules/takepart_addthis/theme/takepart-addthis-simple-block.tpl.php
+++ modules/takepart_addthis/theme/takepart-addthis-simple-block.tpl.php
@@ -1,11 +1,10 @@
 <!-- AddThis Button BEGIN -->
-<h2>Share</h2>
 <div class="takepart_addthis_simple">
+  <span id="addthis_simple_sharethis">Share:</span>
   <div class="addthis_toolbox addthis_default_style">
       <a class="addthis_button_facebook"></a>
       <a class="addthis_button_twitter" tw:via="<?php print $variables['takepart_addthis_tweet_via']; ?>" ></a>
   </div>
-  <span id="addthis_simple_sharethis">Share</span>
 </div>
 <?php
   drupal_add_js('var addthis_config = {"data_track_clickback":true};', 'inline');
