b2b98bac1f711a45e45b7ae105b6ed2b4cf36c1f
Marc Goldstein <mgoldstein@takepart.com>
2011-09-20 13:27:11 -0700

    BugzID: #17985 Remove site name from exaddthis.js

--- modules/takepart_addthis/js/exaddthis.js
+++ modules/takepart_addthis/js/exaddthis.js
@@ -1,4 +1,7 @@
 var addthis_config = {"data_track_clickback":true};
+var pn = document.title;
+var shortname = pn.replace("TakePart - News, Culture, Videos and Photos That Make the World Better", "");
 var addthis_share = {
-  templates: { twitter: '{{title}} {{url}} via @TakePart'}
+  // templates: { twitter: 'check this out: {{title}}  {{url}} via @TakePart'}
+  templates: { twitter: '' + shortname + ' {{url}} via @TakePart'}
 }
