6346ee22b061a80c721ed7d8e797c8daa482c2fe
Charles Toepfer <ctoepfer@takepart.com>
2012-01-03 12:49:28 -0800

    Added URL parameters for Twitter target / count

--- drupal/profiles/takepart/modules/takepart_addthis/theme/takepart-addthis-full-block.tpl.php
+++ drupal/profiles/takepart/modules/takepart_addthis/theme/takepart-addthis-full-block.tpl.php
@@ -6,7 +6,7 @@ $node = menu_get_object();
   <div class="addthis_toolbox addthis_default_style">
     <a class="addthis_button_google_plusone"></a>
     <a class="addthis_button_facebook_like" fb:like:layout="button_count" fb:like:action="<?php print $variables['takepart_addthis_facebook_like_text'];?>"></a> 
-    <a class="addthis_button_tweet" tw:via="<?php print $variables['takepart_addthis_tweet_via']; ?>" tw:text="<?php print $node->title; ?>"></a>
+    <a class="addthis_button_tweet" tw:via="<?php print $variables['takepart_addthis_tweet_via']; ?>" tw:text="<?php print $node->title; ?>" tw:url="<?php echo ((empty($_SERVER["HTTPS"])) ? 'http://' : 'https://') . $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"]; ?>" tw:counturl="<?php echo ((empty($_SERVER["HTTPS"])) ? 'http://' : 'https://') . $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"]; ?>"></a>
     <a class="addthis_button_linkedin_counter"></a>
   </div>
   <div class="addthis_toolbox">
