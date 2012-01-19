b80d32e315d5e2dbe92a37576b5ece7f860e5bdb
Matthew Bostwick <mbostwick@takepart.com>
2012-01-19 14:28:16 -0500

    MySQL Test - Changed structure so program will only produce 1 OK or FAILURE so LB can pick it up correctly.

--- drupal/systems/tests/mysql_test.php
+++ drupal/systems/tests/mysql_test.php
@@ -1,6 +1,6 @@
 <?php
 
-// Simple tests of database.
+// Simple database test - checks connection and select query.
 
 error_reporting(E_ALL);
 ini_set('display_errors','On');
@@ -9,28 +9,26 @@ define('DRUPAL_ROOT', getcwd());
 
 require_once DRUPAL_ROOT . '/includes/install.inc';
 include_once DRUPAL_ROOT . '/sites/default/settings.local.inc';
-include_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 
 $link = mysql_connect("{$databases['default']['default']['host']}", "{$databases['default']['default']['username']}", "{$databases['default']['default']['password']}");
 
 if (!$link) {
-  echo "MySQL Connection - FAILURE\n<br> . mysql_error()";
+  echo "MySQL Connection - FAILURE\n<br>\n . mysql_error()";
 }
 else
 {
-  echo "MySQL Connection - OK\n<BR>\n";
-}
+  // Select first row for users table.  If uid does not equal 0, fail.
+  $query = "select uid from {$databases['default']['default']['database']}.users where uid=0;";
+  $result = mysql_query($query);
 
-// Select first row for users table.  If uid does not equal 0, fail.
-$query = "select uid from {$databases['default']['default']['database']}.users where uid=0;";
-$result = mysql_query($query);
-
-while ($row = mysql_fetch_array($result)) {
-  if ($row['uid'] == 0)
-    echo "MySQL SELECT - OK";
-  else
-    echo "MySQL SELECT - FAILURE";
+  while ($row = mysql_fetch_array($result)) {
+    if ($row['uid'] == 0)
+      echo "MySQL - OK";
+    else
+      echo "MySQL SELECT - FAILURE\n<BR>\n . mysql_error()";
+  }
 }
 
 mysql_close($link);
+
 ?>
