ec93a8af7ca92a24e9636897a19fdb0bc27e628d
Charles Toepfer <ctoepfer@takepart.com>
2012-02-10 04:49:07 -0800

    new footer

--- drupal/profiles/takepart/themes/takepart3/templates/pages/footer.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/templates/pages/footer.tpl.php
@@ -2,6 +2,7 @@
 <div id="footer" class='clear'>
 
 <?php print render($page['footer']) ?>
+    <!-- 
 	<ul id="campaign-links">
 		<li><a href="http://www.climatecrisis.net/" id="climate-crisis">Climate
 				Crisis</a></li>
@@ -12,9 +13,9 @@
 		<li><a href="http://www.waitingforsuperman.com/action/"
 			id="waiting-for-superman">Waiting For Superman</a></li>
 	</ul>
-	
+	 -->
 	<?php if(!(is_null($film_camp_nav) || empty($film_camp_nav))): ?>
-	<div id="our-film-campaigns" class="footer-column-wrapper clearfix">
+	<div id="our-film-campaigns" class="footer-column-wrapper">
 		<div class="column title">
 			<a href="#">our film campaigns</a>
 		</div>
@@ -24,7 +25,7 @@
     <?php endif; ?>
      
     <?php if(!(is_null($friends_takepart_nav) || empty($friends_takepart_nav))): ?> 
-	<div id="friends-of-takepart" class="footer-column-wrapper clearfix">
+	<div id="friends-of-takepart" class="footer-column-wrapper">
 		<div class="column title">
 			<a href="#">friends of takepart</a>
 		</div>
@@ -35,7 +36,7 @@
     
 
     <?php if(!(is_null($takepart_topics_nav) || empty($takepart_topics_nav))): ?>
-	<div id="footer-set-links" class="footer-column-wrapper clearfix">
+	<div id="footer-set-links" class="footer-column-wrapper">
 		<div class="column title">
 			<a href="#">Topics</a>
 		</div>
@@ -44,29 +45,47 @@
     </div>
     <?php endif; ?>
     
+    
     <?php if(!(is_null($corporate_links_nav) || empty($corporate_links_nav))): ?>
-	<div id="footer-links" class="clear clearfix">
-		<ul class="clearfix" id="soc-links">
-			<li id="fb"><a target="_blank"
-				href="http://www.facebook.com/takepart">facebook</a></li>
-			<li id="twitter"><a target="_blank"
-				href="http://www.twitter.com/takepart">twitter</a></li>
-		</ul>
-		<!-- These should be converted to a menu, so TP can manage these links without changing code as they may change from time to time -->
-      <?php print $corporate_links_nav;?>
+	<div id="footer-set-links" class="footer-column-wrapper">
+		<div class="column title">
+			<a href="#">About TakePart</a>
+		</div>
+		<!--/column-->
+     <?php print $corporate_links_nav;?>
     </div>
     <?php endif; ?>
-
-	<div id="footer-about">
-		<p>
-			TakePart is the Social Action Network&trade; of <a target="_blank"
-				href="http://www.participantmedia.com/">Participant Media</a>
-		</p>
-		<div id="footer-legal">
-			&copy; 2008-
-			<?php echo date('Y'); ?>
-			TakePart, LLC
+    
+   
+    
+    
+	<div id="footer-links-new" class="footer-column-wrapper">
+	    <div class="column title">
+			<a href="#">Connect</a>
 		</div>
-	</div>
+        <ul id="bottom-follow">
+              <li class="fb"><a href="http://www.facebook.com/takepart" target="_blank">facebook</a></li>
+              <li class="twitter"><a href="http://www.twitter.com/takepart" target="_blank">twitter</a></li>
+              <li class="youtube"><a href="http://www.youtube.com/takepart" target="_blank">youtube</a></li>
+              <li class="rss"><a href="/rss">rss</a></li>
+        </ul>
+		<!-- These should be converted to a menu, so TP can manage these links without changing code as they may change from time to time -->
+      
+
+    
+        <div id="footer-about">
+    		<p>
+    			TakePart is the Social Action Network&trade; of <a target="_blank"
+    				href="http://www.participantmedia.com/">Participant Media</a>
+    		</p>
+    		<div id="footer-legal">
+    			&copy; 2008-
+    			<?php echo date('Y'); ?>
+    			TakePart, LLC
+    		</div>
+    	</div>
+          
+    </div>
 	
 </div>
+<div class="footer_rule"></div>
