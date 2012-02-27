2d2f0f31061217fd31099daa26c1fcef4348d8aa
Charles Toepfer <ctoepfer@takepart.com>
2012-02-27 00:07:01 -0800

    fixed partial file names when matching alt tags on gallery view templates

--- drupal/profiles/takepart/themes/takepart3/templates/views/views-view-field--field-gallery-images.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/templates/views/views-view-field--field-gallery-images.tpl.php
@@ -4,14 +4,7 @@ try {
   //@todo, hacky, move this to a preprocess function:  
   preg_match('/src=[\'"]?([^\'" >]+)[\'" >]/', $output, $matches);
   $imgfile = urldecode(basename($matches[1]));
-  print "\n";
-  print "\n";
-  print "\n";
-  print $imgfile;
-  print "\n";
   for ($i=0; $i<sizeof($row->_field_data['nid']['entity']->field_gallery_images['und']); $i++) {
-    print $row->_field_data['nid']['entity']->field_gallery_images['und'][$i]['file']->filename;
-    print "\n";
     if(substr($imgfile,0,($trim*-1))  == substr($row->_field_data['nid']['entity']->field_gallery_images['und'][$i]['file']->filename,0,($trim*-1)) ) {
       $mediaalttag = ($row->_field_data['nid']['entity']->field_gallery_images['und'][$i]['file']->field_media_alt['und'][0]['value']);
       break;
