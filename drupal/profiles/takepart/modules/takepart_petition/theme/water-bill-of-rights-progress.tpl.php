728b8c697f060c778d65c581b174cf0eee02964c
JP McDowell <jp@takepart.com>
2012-02-28 12:18:09 -0800

    Added progress template and callback, change signature view theme to wrap values in divs

---
+++ drupal/profiles/takepart/modules/takepart_petition/theme/water-bill-of-rights-progress.tpl.php
@@ -0,0 +1,12 @@
+
+<div>
+  <div>
+    <img src="/profiles/takepart/modules/takepart_petition/images/Water_Logo_300x160.png" />
+  </div>
+  <div>
+    <p id="tp_signatures_to_date"></p>
+    <img src="/profiles/takepart/modules/takepart_petition/images/petition-status-bar.png" />
+    <p>Signature Goal: <?php print number_format($variables['goal']) ?></p>
+    <p>PROGRESS: 70%</p>
+  </div>
+</div>
