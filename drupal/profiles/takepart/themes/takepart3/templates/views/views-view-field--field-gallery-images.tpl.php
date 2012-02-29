8c9dc01bae4aba0c209d149f3a13e4d709ddea38
Charles Toepfer <ctoepfer@takepart.com>
2012-02-28 22:03:56 -0800

    Fixed matching algo for images in theme

--- drupal/profiles/takepart/themes/takepart3/templates/views/views-view-field--field-gallery-images.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/templates/views/views-view-field--field-gallery-images.tpl.php
@@ -1,11 +1,12 @@
 <?php
 try {
-  $trim = 6;
+  $trim = 8;
   //@todo, hacky, move this to a preprocess function:  
   preg_match('/src=[\'"]?([^\'" >]+)[\'" >]/', $output, $matches);
   $imgfile = urldecode(basename($matches[1]));
+  $imgfile = str_replace ("size_", "", $imgfile);
   for ($i=0; $i<sizeof($row->_field_data['nid']['entity']->field_gallery_images['und']); $i++) {
-    if(substr($imgfile,0,($trim*-1))  == substr($row->_field_data['nid']['entity']->field_gallery_images['und'][$i]['file']->filename,0,($trim*-1)) ) {
+    if(substr($imgfile,0,($trim))  == substr($row->_field_data['nid']['entity']->field_gallery_images['und'][$i]['file']->filename,0,($trim)) ) {
       $mediaalttag = ($row->_field_data['nid']['entity']->field_gallery_images['und'][$i]['file']->field_title['und'][0]['value']);
       if(!$mediaalttag) {
         $mediaalttag = ($row->_field_data['nid']['entity']->field_gallery_images['und'][$i]['file']->field_image_title['und'][0]['value']);
@@ -16,8 +17,6 @@ try {
       break;
     }
   }
-} catch (Exception $e) {
-  $mediaalttag = "";
-}
+} catch (Exception $e) { }
 ?>
 <?php print str_replace('alt=""', 'alt="'.$mediaalttag.'"', $output); ?>
