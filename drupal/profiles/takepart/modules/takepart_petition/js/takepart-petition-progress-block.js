17544e620fd4994b8a373f04f1ba3e2b40ccd874
JP McDowell <jp@takepart.com>
2012-02-28 21:17:28 -0800

    Implemented updating of the petition progress bar, fixed an alignment issue with signature view header, set the maximum length of signature comments to 420 characters

--- drupal/profiles/takepart/modules/takepart_petition/js/takepart-petition-progress-block.js
+++ drupal/profiles/takepart/modules/takepart_petition/js/takepart-petition-progress-block.js
@@ -4,8 +4,11 @@ var jqxhr = jQuery.getJSON('/lastcall/ajax/signature-count/'+tp_petition_signatu
     var count = parseInt(response.data);
     if (! isNaN(count)) {
       var percent = Math.floor((count / tp_petition_signatures_goal) * 100.0);
+      var bar_num = percent % 10;
+      var bar_url = "/profiles/takepart/modules/takepart_petition/images/petition-status-bar-" + bar_num + ".png";
       jQuery('#tp_signatures_to_date').text("Signatures to Date: " + count);
       jQuery('#tp_signatures_percent').text("PROGRESS: " + percent + "%");
+      jQuery('#tp_signatures_bar').attr('src', bar_url);
     }
   } else if (response.status == -1) {
   }
