6fe9b52e579c7589ecfea304f715524235ff64c2
JP McDowell <jp@takepart.com>
2012-02-29 20:32:28 -0800

    Update the addthis ajax form block to include the changes made for the addthis petition block

--- drupal/profiles/takepart/modules/takepart_addthis/theme/takepart-addthis-ajax-form-block.tpl.php
+++ drupal/profiles/takepart/modules/takepart_addthis/theme/takepart-addthis-ajax-form-block.tpl.php
@@ -1,17 +1,25 @@
-<div>
-<h2>Thank You For Signing!</h2>
-<p>Your signature has been added to the petition, and our movement is one person stronger!</p>
-<p>Every signature matters - share yours with people you know who also care about progress. Ask your friends, family, coworkers, and neighbors to join our campaign.</p>
-<p>You can share the petition on Facebook or Twitter, or send an email using the form below.</p>
-
+<div style="clear: both; margin-bottom: 20px;">
+  <div class="tp-pet-thank-you-heading">Thank You For Signing!</div>
+  <p>Your signature has been added to the petition, and our movement is one person stronger!</p>
+  <p>Every signature matters - share yours with people you know who also care about progress. Ask your friends, family, coworkers, and neighbors to join our campaign.</p>
+  <p>You can share the petition on Facebook or Twitter, or send an email using the form below.</p>
+  <div class="addthis_toolbox addthis_default_style addthis_32x32_style">
+  <a class="addthis_button_facebook">
+    <img src="/profiles/takepart/modules/takepart_addthis/images/fb_share.png" alt="Share on Facebook" />
+  </a>
+  <a class="addthis_button_twitter" tw:via="TakePart">
+    <img src="/profiles/takepart/modules/takepart_addthis/images/twitter_share.png" alt="Share on Twitter" />
+  </a>
+  </div>
 </div>
-<!-- AddThis Button BEGIN -->
-<div class="addthis_toolbox addthis_default_style addthis_32x32_style">
-<a class="addthis_button_facebook">
-        <img src="/profiles/takepart/modules/takepart_addthis/images/fb_share.png"
-          alt="Share on Facebook" /></a>
-<a class="addthis_button_twitter">
-        <img src="/profiles/takepart/modules/takepart_addthis/images/twitter_share.png"
-          alt="Share on Twitter" /></a>
-</div>
-<!-- AddThis Button END -->
+<script type="text/javascript">
+  var addthis_config = {
+    ui_email_note: 'SHARE THE BILL OF RIGHTS\nThe more names we have, the more powerful our message. Share the Water Bill of Rights with friends and family!',
+    ui_email_from: "<?php print $_COOKIE['petition_signed_as'] ?>"
+  };
+  var addthis_share = {
+    templates: {
+      twitter: "Water is a right, not a privilege! Sign the Water Bill of Rights today. {{url}} #knowyourwater via @TakePart"
+    }
+  };
+</script>
