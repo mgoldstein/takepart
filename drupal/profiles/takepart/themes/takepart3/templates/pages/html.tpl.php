8c7921d5a4ba6d64bc74afdb5d37b8fcd7656bc4
Marc Goldstein <mgoldstein@takepart.com>
2012-03-14 13:45:56 -0700

    Add FB meta tags

--- drupal/profiles/takepart/themes/takepart3/templates/pages/html.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/templates/pages/html.tpl.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @file
  * Default theme implementation to display the basic html structure of a single
@@ -41,30 +40,29 @@
  * @see template_process()
  */
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
-  "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
+    "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?> >
 
-<head profile="<?php print $grddl_profile; ?>">
-  <?php print $head; ?>
-  
- 
-  
-  <title><?php print $head_title; ?></title>
-  <?php print $styles; ?>
-  <?php print $scripts; ?>
-</head>
-<body class="<?php print $classes; ?>" <?php print $attributes;?>>
-  <div id="skip-link">
-    <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
-  </div>
-  <?php print $page_top; ?>
-  <?php print $page; ?>
-  <?php print $custom; ?>
-  <?php print $page_bottom; ?>
-  <?php 
-  if(isset($tp_sysinfo_comment_tags)):
-    echo $tp_sysinfo_comment_tags;
-  endif;  
-  ?>
-</body>
+    <head profile="<?php print $grddl_profile; ?>">
+        <?php print $head; ?>
+        <meta property="fb:admins" content="1327833247" />
+        <meta property="fb:app_id" content="247137505296280" />
+        <title><?php print $head_title; ?></title>
+        <?php print $styles; ?>
+        <?php print $scripts; ?>
+    </head>
+    <body class="<?php print $classes; ?>" <?php print $attributes; ?>>
+        <div id="skip-link">
+            <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+        </div>
+        <?php print $page_top; ?>
+        <?php print $page; ?>
+        <?php print $custom; ?>
+        <?php print $page_bottom; ?>
+        <?php
+        if (isset($tp_sysinfo_comment_tags)):
+            echo $tp_sysinfo_comment_tags;
+        endif;
+        ?>
+    </body>
 </html>
