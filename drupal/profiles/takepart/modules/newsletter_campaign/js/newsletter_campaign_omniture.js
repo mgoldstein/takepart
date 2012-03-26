f6516b4619a5bd13801d1260af064fe0a9636e41
jpedius <jp@takepart.com>
2012-03-26 11:39:51 -0700

    Fixed metrics on the Thank You page

--- drupal/profiles/takepart/modules/newsletter_campaign/js/newsletter_campaign_omniture.js
+++ drupal/profiles/takepart/modules/newsletter_campaign/js/newsletter_campaign_omniture.js
@@ -4,11 +4,18 @@
     attach: function (context, settings) {
       $('#newsletter-campaign-signup-form .form-submit', context).click(
         function(e) {
-          s.linkTrackVars="eVar28,events";
+          
+          s.linkTrackVars="eVar23,events";
           s.linkTrackEvents="event39";
-          s.eVar28='newsletter';
+          s.eVar23='TakePart Newsletter';
           s.events='event39';
-          s.tl(true, 'o', 'Newsletter Sign-Up');
+          s.tl(true, 'o', 'Newsletter Signup');
+
+          s.linkTrackVars="eVar28,events";
+          s.linkTrackEvents="event19";
+          s.eVar28='Newsletter Signup';
+          s.events='event19';
+          s.tl(true, 'o', 'Action Click');
         }
       );
     }
