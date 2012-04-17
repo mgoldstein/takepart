a9b2c05cfc299c0a3bfde231b9288537dff61790
JP McDowell <jp@takepart.com>
2012-04-17 09:13:39 -0700

    Cleaned up styling, hooked up petition view event

--- drupal/profiles/takepart/modules/petition/js/form.js
+++ drupal/profiles/takepart/modules/petition/js/form.js
@@ -1,4 +1,6 @@
 jQuery(document).ready(function () {
+
+  // hook up the rollover for the sign petition button
   jQuery("input.rollover-button").hover(
     function () { this.src = this.src.replace("_off","_on");
     },
@@ -6,6 +8,7 @@ jQuery(document).ready(function () {
     }
   );
 
+  // hook up the public display disclaimer
   jQuery("#edit-signature-public-display-und").click(
     function () {
       var note = jQuery('#petition-signature-display-disclaimer');
@@ -17,4 +20,17 @@ jQuery(document).ready(function () {
       }
     }
   );
+
+  // fire off the petition view event
+  if (Drupal.settings.petition) {
+    var latch = Drupal.settings.petition['viewed_latch'];
+    if (jQuery.cookie(latch) != null) {
+      s.events='event26';
+      s.eVar25=Drupal.settings.petition['name'];
+      s.linkTrackVars='eVar25,events';
+      s.linkTrackEvents='event26';
+      s.tl(true, 'o', 'petition view');
+      jQuery.cookie(latch, 1, {path:'/'});
+    }
+  }
 });
