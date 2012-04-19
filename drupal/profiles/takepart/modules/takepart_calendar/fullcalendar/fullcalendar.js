947b92cffd8a9394e5cdd589ceee66f4ecb862a7
Charles Toepfer <ctoepfer@takepart.com>
2012-04-19 13:31:30 -0700

    calendar fix

--- drupal/profiles/takepart/modules/takepart_calendar/fullcalendar/fullcalendar.js
+++ drupal/profiles/takepart/modules/takepart_calendar/fullcalendar/fullcalendar.js
@@ -5120,7 +5120,8 @@ function enableTextSelection(element) {
             var rowCnt = rowDivs.length;
             var tops = [];
             for (i=0; i<rowCnt; i++) {
-                tops[i] = rowDivs[i][0].offsetTop; // !!?? but this means the element needs position:relative if in a table cell!!!!
+                //tops[i] = rowDivs[i][0].offsetTop; // !!?? but this means the element needs position:relative if in a table cell!!!!
+            	tops[i] = $(rowDivs[i][0]).position().top;
             }
             return tops;
         }
