607a936957c22f126e2eab8c126739574d7c6b50
JP McDowell <jp@takepart.com>
2012-05-09 17:42:57 -0700

    more style tweaks, implemented custom message for sharing

--- drupal/profiles/takepart/modules/takepart_contests/contests/ideasforgood/theme/vote.tpl.php
+++ drupal/profiles/takepart/modules/takepart_contests/contests/ideasforgood/theme/vote.tpl.php
@@ -5,13 +5,20 @@
  *
  * Variables
  * - $ideasforgood_finalist: the finalist entity
- * - $content: contains the fields on the finalist entity
+ * - $content: contains the following
+ *     ideasforgood_finalist_image: the finalist entity image field
+ *     ideasforgood_finalist_first_name: the finalist entity first name field
+ *     ideasforgood_finalist_last_name: the finalist entity last name field
+ *     ideasforgood_finalist_idea_title: the finalist entity idea title field
+ *     ideasforgood_finalist_idea_body: the finalist entity idea body field
+ * - $ideasforgood_finalist_image: the image field values
+ * - $ideasforgood_finalist_first_name: the first name field values
+ * - $ideasforgood_finalist_last_name: the last name field values
+ * - $ideasforgood_finalist_idea_title: the idea title field values
+ * - $ideasforgood_finalist_idea_body: the idea body field values
+ *
+ * Additional $content values
+ *   vote_form: the form for casting a vote for a finalist
  */
 ?>
-<p>Do you want to confirm your vote for
-  <?php print $ideasforgood_finalist_first_name[0]['safe_value']; ?>
-  <?php print $ideasforgood_finalist_last_name[0]['safe_value']; ?>
-</p>
 <?php print render($content['vote_form']); ?>
-<img src="/<?php print $content['cancel_button']; ?>" alt="Cancel" class="ideasforgood-cancel-button">
-<div style="clear: both"></div>
