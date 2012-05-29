f8214a54f4db835e5418cb1c972e7723a2adb284
Marc Goldstein <mgoldstein@takepart.com>
2012-05-29 15:14:41 -0700

    BugzID: #24155 - remove inline font call

--- drupal/profiles/takepart/themes/takepart3/templates/pages/html--bsd--header.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/templates/pages/html--bsd--header.tpl.php
@@ -7,12 +7,6 @@
 
 <div id="page-wrapper">
 <?php print $styles; ?>
-<style type="text/css">
-@font-face {
-    font-family: NewsGothicBoldCondensed;
-    src: url('http://www.takepart.com/profiles/takepart/themes/takepart3/includes/ufonts.com_trade-gothic-lt-com-bold-condensed-no.-20.ttf') format('truetype');
-}    
-</style>
 <?php print $scripts; ?>
 <?php print $custom; ?>
 
