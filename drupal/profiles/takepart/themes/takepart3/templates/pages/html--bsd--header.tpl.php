66e693448d1c4f9aec570cbb93493fb3dfcbb08e
Marc Goldstein <mgoldstein@takepart.com>
2012-05-29 10:48:04 -0700

    BugzID: #24155 - add page wrappers for BSD templates

--- drupal/profiles/takepart/themes/takepart3/templates/pages/html--bsd--header.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/templates/pages/html--bsd--header.tpl.php
@@ -4,6 +4,11 @@
  * Render TakePart header for inclusion in BSD wrappers.
  */
 ?>
+
+<div id="page-wrapper">
 <?php print $styles; ?>
 <?php print $scripts; ?>
 <?php print $custom; ?>
+
+<div id='page' class='page clearfix'>
+<div class='main-content'>
