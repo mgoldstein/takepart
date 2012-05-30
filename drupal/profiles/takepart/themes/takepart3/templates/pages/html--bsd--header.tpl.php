52ba44068d100b83edb342bef4f97dad3fe5871a
Marc Goldstein <mgoldstein@takepart.com>
2012-05-29 18:29:23 -0700

    BugzID: #24155 - conditional CSS for IE

--- drupal/profiles/takepart/themes/takepart3/templates/pages/html--bsd--header.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/templates/pages/html--bsd--header.tpl.php
@@ -5,9 +5,19 @@
  */
 ?>
 
-<div id="page-wrapper">
 <?php print $styles; ?>
 <?php print $scripts; ?>
+<!--[if IE]>
+<style type="test/css" media="all">
+#join-login-top {
+text-align: right;
+display: block;
+width: 200px;
+}
+</style>
+<![endif]-->
+
+<div id="page-wrapper">
 <?php print $custom; ?>
 
 <div id='page' class='page clearfix'>
