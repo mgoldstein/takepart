5240f9b19ed635b965e9049c3a7350104d1fa616
Marc Goldstein <mgoldstein@takepart.com>
2012-06-11 16:30:42 -0700

    BugzID: #24118 - update RSS link as Drupal l() call

--- drupal/profiles/takepart/themes/takepart3/templates/pages/follow_us_links.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/templates/pages/follow_us_links.tpl.php
@@ -3,5 +3,5 @@
       <li class="twitter"><a target="_blank" href="http://www.twitter.com/takepart">twitter</a></li>
       <li class="pinterest"><a href="http://pinterest.com/takepart/" target="_blank"><img src="http://passets-cdn.pinterest.com/images/about/buttons/small-p-button.png" width="16" height="16" alt="Follow Me on Pinterest" /></a></li>
       <li class="youtube"><a target="_blank" href="http://www.youtube.com/takepart">youtube</a></li>
-      <li class="rss"><a href="/rss">rss</a></li>
+      <li class="rss"><?php print l("rss", "node/19814", array('attributes' => array('title' => 'RSS'), 'absolute' => TRUE ) ); ?></li>
 </ul>
