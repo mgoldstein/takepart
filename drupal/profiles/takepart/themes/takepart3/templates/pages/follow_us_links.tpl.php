f7c4fc7c87b38ce67310fe047873a614d91f966b
Marc Goldstein <mgoldstein@takepart.com>
2012-06-11 16:25:49 -0700

    BugzID: #24118 - add pinterest button to header and footer with styling

--- drupal/profiles/takepart/themes/takepart3/templates/pages/follow_us_links.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/templates/pages/follow_us_links.tpl.php
@@ -1,7 +1,7 @@
 <ul id="top-follow">
       <li class="fb"><a target="_blank" href="http://www.facebook.com/takepart">facebook</a></li>
       <li class="twitter"><a target="_blank" href="http://www.twitter.com/takepart">twitter</a></li>
-      <li class="pinterest"><a href="http://pinterest.com/takepart/"><img src="http://passets-cdn.pinterest.com/images/about/buttons/small-p-button.png" width="16" height="16" alt="Follow Me on Pinterest" /></a></li>
+      <li class="pinterest"><a href="http://pinterest.com/takepart/" target="_blank"><img src="http://passets-cdn.pinterest.com/images/about/buttons/small-p-button.png" width="16" height="16" alt="Follow Me on Pinterest" /></a></li>
       <li class="youtube"><a target="_blank" href="http://www.youtube.com/takepart">youtube</a></li>
       <li class="rss"><a href="/rss">rss</a></li>
 </ul>
