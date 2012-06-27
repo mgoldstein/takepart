3bcdbc1a0aa06ec4b674cdcd68b364a68995c3ad
Phil Glatz <phil@electronovelty.com>
2012-06-27 16:55:41 -0700

    BugzID: #24668 - add title to embedded

--- drupal/profiles/takepart/modules/takepart_vidpop/js/takepart_vidpop.js
+++ drupal/profiles/takepart/modules/takepart_vidpop/js/takepart_vidpop.js
@@ -2,12 +2,19 @@
 
 
 (function ($) {
+  Drupal.behaviors.vidpopLoad = {
+    attach: function (context, settings) {
+      $(".vidpop-preview-overlay-headline").html(Drupal.settings.vidpop.promo_headline);
+    }
+  }
+
   Drupal.behaviors.scVidpopClick = {
     attach: function (context, settings) {
       for( x in Drupal.settings.media_youtube) {
         Drupal.settings.media_youtube[x]['options']['autoplay'] = 1;
       }
 
+      // add click tracker for banner ad
       $('.vidpop-preview', context).click(function(){
         s.linkTrackVars="events";
         s.linkTrackEvents="event41";
