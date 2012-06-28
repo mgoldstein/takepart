3b177f4c97531de5f1bd3c403fd146b26d997070
Charles Toepfer <ctoepfer@takepart.com>
2012-06-27 18:30:04 -0700

    fixed query param check for fb share refresh

--- drupal/profiles/takepart/modules/takepart_galleries/js/takepart_galleries.js
+++ drupal/profiles/takepart/modules/takepart_galleries/js/takepart_galleries.js
@@ -194,8 +194,8 @@ function fastmatch_fb_iframe_refresh(q) {
 		} else {
 			token = "";
 		}
-		if((pair[0] == 'href') && (pair[0] == '?href')) {
-			nq = nq + token + pair[0] + '=' + encodeURIComponent(window.location);
+		if((pair[0] == 'href') || (pair[0] == '?href')) {
+			nq = nq + token + pair[0] + '=' + encodeURIComponent(window.location.href);
 		} else {
 			nq = nq + token + pair[0] + '=' + pair[1];
 		}
