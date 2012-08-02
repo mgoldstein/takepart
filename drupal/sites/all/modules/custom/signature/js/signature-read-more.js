548cad2785c5f35c36c7ec7c40bf64cb53cca61c
JP McDowell <jp@takepart.com>
2012-08-02 14:04:05 -0700

    BugzID #21817 - Added metrics, removed read more from embedded view

--- drupal/sites/all/modules/custom/signature/js/signature-read-more.js
+++ drupal/sites/all/modules/custom/signature/js/signature-read-more.js
@@ -1,15 +1,19 @@
 (function ($) {
   Drupal.behaviors.petitionReadMore = {
     attach: function (context, settings) {
-      $('div.node-petition-action').once('petition-read-more-processed', function (index) {
+
+      $('div.node-petition-action').not('div.embedaction-content > div').once(
+        'petition-read-more', function (index) {
         var nid = $(this).attr('nid');
-        var readMore = $('<span id="read-more-' + nid + '"> <a href="#">Read more.</a></span>');
+        var readMore = $('<span id="read-more-' + nid 
+          + '"> <a href="#">Read more.</a></span>');
         $('div.field-name-body div.field-item', this).append(readMore);
         var tabs = $('div.group-petition-tabs', this);
         $('a', readMore).click({tabs: tabs}, function (event) {
           var tabs = event.data['tabs'];
           event.preventDefault();
-          $('.group-about.field-group-htab', tabs).data('horizontalTab').tabShow();
+          $('.group-about.field-group-htab', tabs).data(
+            'horizontalTab').tabShow();
           $('html,body').animate({scrollTop:$(tabs).offset().top}, 500);
         });
       });
