a1266d574a13bdb8c2c67406185d3c9f3c43f00b
JP McDowell <jp@takepart.com>
2012-09-12 11:33:54 -0700

    Fogbugz #34509 - Added additional newslwetter opt-ins to the signatures, updated fields descriptions to be more useful

--- drupal/sites/all/modules/custom/signature/js/signature-vertical-tabs.js
+++ drupal/sites/all/modules/custom/signature/js/signature-vertical-tabs.js
@@ -9,6 +9,21 @@
           return Drupal.t('No Signature');
         }
       });
+      $('fieldset.edit-signature-newsletters', context).drupalSetSummary(
+        function(context) {
+          var count = 0;
+          $('.signature-newsletter-label').each(function (index) {
+            if ($(this).val().length > 0) { count += 1; }
+          });
+          if (count == 0) {
+            return Drupal.t('No newsletter opt-ins');
+          }
+          else if (count == 1) {
+            return Drupal.t('1 newsletter opt-in');
+          }
+          return Drupal.t(count + ' newsletter opt-ins');
+        }
+      );
     }
   };
 })(jQuery);
