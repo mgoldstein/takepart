beb37afc2a501d0b84efbb9fbca483190cc55619
Dustin Boersma <dustin.boersma@gmail.com>
2012-09-26 01:35:22 +0000

    Fogbugz #31425 - more styling fixes

--- drupal/profiles/takepart/themes/takepart3/js/pledges.js
+++ drupal/profiles/takepart/themes/takepart3/js/pledges.js
@@ -1,21 +1,28 @@
 (function($) {
   Drupal.behaviors.pledges = {
     attach: function(context) {
+      
+      // TODO: MOVE THIS TO PETITION.JS WHENEVER IT'S MADE
+      $('.node-petition-action').once('petition-js', function(){
+        $('.field-name-field-petition-sponsor .field-item', '.node-petition-action').not(':last').append(', '); // add commas to sponsors
+      });
+    
       // $(document).ready and after every ajax call
       var container = $('.node-pledge-action');
-      container.once('node-pledge-action', _init);
+      container.once('pledges-js', _init);
       
       // $(document).ready only
       function _init(){
         // variables
         var element = $(this);
-        var wycdLongContainter = $('.node-pledge-action .field-name-field-pledge-action-long');
-        var wycdShortContainer = $('.node-pledge-action .field-name-field-pledge-action-short');
+        var wycdLongContainter = $('.field-name-field-pledge-action-long', container);
+        var wycdShortContainer = $('.field-name-field-pledge-action-short', container);
         var wycdMoreLabel = 'Read more.';
         var wycdLessLabel = 'Read less.';
                       
         // on load
-        var wycdLink = $('<a href="javascript:void(0)">' + wycdMoreLabel + '</a>').addClass('wycdLink').insertAfter(wycdShortContainer);
+        var wycdLink = $('<a href="javascript:void(0)">' + wycdMoreLabel + '</a>').addClass('wycdLink').insertAfter(wycdShortContainer); // add wycdLink
+        $('.field-name-field-petition-sponsor .field-item', container).not(':last').append(', '); // add commas to sponsors
         
         // bind actions
         element.bind('click', { self: self }, _click);
