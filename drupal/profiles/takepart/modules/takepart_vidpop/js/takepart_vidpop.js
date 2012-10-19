54f3519284b955e505d2b5bdd65e1337b2d5f183
Phil Glatz <phil@electronovelty.com>
2012-10-19 10:43:29 -0700

    BugzID: #32846 - updated js from 35793

--- drupal/profiles/takepart/modules/takepart_vidpop/js/takepart_vidpop.js
+++ drupal/profiles/takepart/modules/takepart_vidpop/js/takepart_vidpop.js
@@ -4,82 +4,94 @@
 //
 // triggered by call in template.php, preprocess node
 // doesn't work here as a behavior, like the others
+var vp_embeddedvideotitle;
+var vp_embeddedvideotype;
+
+var vp_titles = new Array();   // list of titles
+var vp_types  = new Array();   // list of node types
+var vp_lookup = new Array();   // 0-based index lookup of title/types index
+
 function vidpop_loaded() {
-  // debug: alert('trap 40 c');
-  s.linkTrackVars="eVar40, prop40, eVar41, prop41, events";
-  s.linkTrackEvents="event40";
-  s.events='event40';
-  s.prop40=Drupal.settings.takepart_vidpop.embeddedvideotitle;
-  s.eVar40=Drupal.settings.takepart_vidpop.embeddedvideotitle;
-  s.prop41=Drupal.settings.takepart_vidpop.hostpagetitle;
-  s.eVar41=Drupal.settings.takepart_vidpop.hostpagetitle;
-  s.tl(true, 'o', 'Video Popup Click');
+  vp_embeddedvideotitle = Drupal.settings.takepart_vidpop.embeddedvideotitle;
+  vp_embeddedvideotype  = Drupal.settings.takepart_vidpop.embeddedvideotype;
+
+  // there may be more than one embedded video on a page; send codes for each one
+  // collect passed titles and types
+  // event40 is called first, and sets these for other events as globals
+  var lookup_index = 0;
+
+  jQuery.each(vp_embeddedvideotitle, function(i, val){
+    vp_titles[i] = val;
+    vp_lookup[lookup_index] = i;
+    lookup_index++;
+  });
+  jQuery.each(vp_embeddedvideotype, function(i, val){
+    vp_types[i] = val;
+  });
+
+  for (i=0; i < lookup_index; i++) {
+    var li = vp_lookup[i];
+    var modaltitle = vp_titles[li];
+    var modaltype  = vp_types[li];
+    s.linkTrackVars="eVar30, prop30, eVar40, prop40, eVar42, prop42, events";
+    s.linkTrackEvents="event40";
+    s.events='event40';
+    s.prop30=s.pageName;
+    s.eVar30=s.pageName;
+    s.prop40=modaltype+':'+modaltitle;
+    s.eVar40=modaltype+':'+modaltitle;
+    s.prop42=modaltype;
+    s.eVar42=modaltype;
+    s.tl(true, 'o', 'Video Popup Page Load');
+  }
 };
 
 
-// add click trackers for banner ads
+// add click trackers for popups and banner ads
 (function ($) {
   Drupal.behaviors.scVidpopClick = {
-        attach: function (context, settings) {
-          // On click on embedded video to launch and play modal
-          $('.vidpop-preview', context).click(function(){
-            // debug: alert('trap 41 ');
-            s.linkTrackVars="eVar40, prop40, eVar41, prop41, events";
-            s.linkTrackEvents="event41";
-            s.events='event41';
-            s.prop40=Drupal.settings.takepart_vidpop.embeddedvideotitle;
-            s.eVar40=Drupal.settings.takepart_vidpop.embeddedvideotitle;
-            s.prop41=Drupal.settings.takepart_vidpop.hostpagetitle;
-            s.eVar41=Drupal.settings.takepart_vidpop.hostpagetitle;
-            s.tl(true, 'o', 'Video Popup Click');
-          });
+    attach: function (context, settings) {
+      // On click on embedded video to launch and play modal
+      $('.vidpop-preview', context).click(function(){
+        var n = $(this).attr('class').match(/ vp-(\d+)/);
+        n = n[1];
+        var modaltitle = vp_titles[n];
+        var modaltype  = vp_types[n];
+        s.linkTrackVars="eVar30, prop30, eVar40, prop40, eVar42, prop42, events";
+        s.linkTrackEvents="event41";
+        s.events='event41';
+        s.prop30=s.pageName;
+        s.eVar30=s.pageName;
+        s.prop40=modaltype+':'+modaltitle;
+        s.eVar40=modaltype+':'+modaltitle;
+        s.prop42=modaltype;
+        s.eVar42=modaltype;
+        s.tl(true, 'o', 'Video Popup Click');
+        //alert('n:' + n);
+      });
 
-          /*
-           * not supported, because addthis is on a different domain
-           * revisit when we replace addthis
-           *
-           // On click of any of the share icons in the modal, pls fire (once per session per button)
-           $('#vidpop-details #vidpop-social', context).click(function(){
-           // debug: alert('trap 42t');
-           s.linkTrackVars="eVar40, prop40, eVar41, prop41, events";
-           s.linkTrackEvents="event42";
-           s.events='event41';
-           s.prop40=Drupal.settings.takepart_vidpop.embeddedvideotitle;
-           s.eVar40=Drupal.settings.takepart_vidpop.embeddedvideotitle;
-           s.prop41=Drupal.settings.takepart_vidpop.hostpagetitle;
-           s.eVar41=Drupal.settings.takepart_vidpop.hostpagetitle;
-           s.tl(true, 'o', 'Video Popup Click');
-           });
-           $('#vidpop-details .addthis_button_email span', context).click(function(){
-           // debug: alert('trap 42e');
-           s.linkTrackVars="eVar40, prop40, eVar41, prop41, events";
-           s.linkTrackEvents="event42";
-           s.events='event41';
-           s.prop40=Drupal.settings.takepart_vidpop.embeddedvideotitle;
-           s.eVar40=Drupal.settings.takepart_vidpop.embeddedvideotitle;
-           s.prop41=Drupal.settings.takepart_vidpop.hostpagetitle;
-           s.eVar41=Drupal.settings.takepart_vidpop.hostpagetitle;
-           s.tl(true, 'o', 'Video Popup Click');
-           });
-           */
+      // On-click of the subscribe button, pls fire (only once per session)
+      $('.vidpop-popup .subscribe', context).click(function(){
+        if( typeof vp_43_triggered == 'undefined' ) {
+          // block multiple calls
+          //alert('trap 43');
+          vp_43_triggered = 1;
 
-          // On-click of the subscribe button, pls fire (only once per session)
-          $('.vidpop-popup .subscribe', context).click(function(){
-            if( typeof vp_43_triggered == 'undefined' ) {
-              // block multiple calls
-              // debug: alert('trap 43');
-              vp_43_triggered = 1;
-              s.linkTrackVars="eVar40, prop40, eVar41, prop41, events";
-              s.linkTrackEvents="event43";
-              s.events='event43';
-              s.prop40=Drupal.settings.takepart_vidpop.embeddedvideotitle;
-              s.eVar40=Drupal.settings.takepart_vidpop.embeddedvideotitle;
-              s.prop41=Drupal.settings.takepart_vidpop.hostpagetitle;
-              s.eVar41=Drupal.settings.takepart_vidpop.hostpagetitle;
-              s.tl(true, 'o', 'Video Popup Click');
-            }
-          });
+          var n = $(this).attr('class').match(/ vp-(\d+)/);
+          n = n[1];
+          var modaltitle = vp_titles[n];
+          var modaltype  = vp_types[n];
+          s.linkTrackVars="eVar40, prop40, eVar41, prop41, events";
+          s.linkTrackEvents="event43";
+          s.events='event43';
+          s.prop40=modaltype+':'+modaltitle;
+          s.eVar40=modaltype+':'+modaltitle;
+          s.prop41=s.pageName;
+          s.eVar41=s.pageName;
+          s.tl(true, 'o', 'Video Popup Click');
         }
+      });
+    }
   }
 })(jQuery);
 
