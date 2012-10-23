458ba749c78f1481c1a83515d6f68f85ed5163cc
Phil Glatz <phil@electronovelty.com>
2012-10-23 09:51:11 -0700

    BugzID: #30502 - preliminary version for eval

--- drupal/sites/all/modules/custom/rightrail_popups/theme/rightrail-video-preview.tpl.php
+++ drupal/sites/all/modules/custom/rightrail_popups/theme/rightrail-video-preview.tpl.php
@@ -5,16 +5,28 @@
  *
  * - $p_title     block title
  * - $p_video_id  id of video
- * - $p_preview   preview image
- * - $p_body      block body
+ * - $p_popup     popup html
  *
  * @ingroup views_templates
  */
+
 ?>
 <div id="youtube_block">
   <div style="line-height: 5px;">&nbsp;</div>
   <br>
   <div style="line-height: 5px;">&nbsp;</div>
-  <img src="http://img.youtube.com/vi/<?php echo $p_video_id; ?>/0.jpg" width="275" height="155"></img>
+  <a class="colorbox-inline" href="?width=870&amp;height=420&amp;inline=true#<?php print $p_video_id; ?>"><img src="http://img.youtube.com/vi/<?php echo $p_video_id; ?>/0.jpg" width="275" height="155" /></a>
   <div style="line-height: 5px;">&nbsp;</div>
-  <strong><a href="http://www.youtube.com/embed/<?php echo $p_video_id; ?>">Read more and take action here.</a></strong></div>
+  <strong><a href="http://www.youtube.com/embed/<?php echo $p_video_id; ?>">Read more and take action here.</a></strong>
+</div>
+<?php
+if (!takepart_vidpop_mobile_browser()) {
+// add hidden popups if on non-mobile
+$formatted .= <<<EOT
+<div class="vidpop-overlay">
+  <div id="$p_video_id">$p_popup</div>
+</div>
+EOT;
+echo $formatted;
+}
+?>
