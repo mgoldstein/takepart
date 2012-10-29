0f171f9cf42fdb31bd978f697dfd4a312af27bbe
Marc Goldstein <mgoldstein@takepart.com>
2012-10-29 13:38:41 -0700

    BugzID: #38344 - analytics for TAB

--- drupal/profiles/takepart/modules/features/takepart_action/js/takepart-action.js
+++ drupal/profiles/takepart/modules/features/takepart_action/js/takepart-action.js
@@ -18,10 +18,10 @@
                         }
                     }
                     s.linkTrackVars="eVar28,eVar30,events";
-                    s.linkTrackEvents="event37, event19";
+                    s.linkTrackEvents="event37, event19, event56";
                     s.eVar28='tpaction';
                     s.eVar30='takepart:actions:' + $('h1.title').text();
-                    s.events='event37, event19';
+                    s.events='event37, event19, event56';
                     s.tl(this.href, 'o', 'Take Action Button Click');
                     $(this).addClass('action-taken');
                     $(this).children(":first").text('Action Taken');
