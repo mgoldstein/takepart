77a5469c582fabb73725c0394c1b423b7ccbf476
Marc Goldstein <mgoldstein@takepart.com>
2012-11-07 11:16:03 -0800

    BugzID: #37174 - remove script files from previous builds

--- drupal/scripts/jp-30104.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Enable the module
-module_enable(array('bluestatedigital'));
-module_enable(array('bluestatedigital_user'));
-module_enable(array('bluestatedigital_forms'));
-
-// Rebuild the menu router
-menu_rebuild();
