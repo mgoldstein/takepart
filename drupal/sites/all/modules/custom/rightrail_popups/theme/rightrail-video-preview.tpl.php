c3ea6920e8e3cef7e0ae3b2dc6349e7b105a5ecb
Phil Glatz <phil@electronovelty.com>
2012-11-14 13:40:41 -0800

    BugzID: #29946 - add rrpop class

--- drupal/sites/all/modules/custom/rightrail_popups/theme/rightrail-video-preview.tpl.php
+++ drupal/sites/all/modules/custom/rightrail_popups/theme/rightrail-video-preview.tpl.php
@@ -16,7 +16,7 @@
   <img src="http://img.youtube.com/vi/<?php echo $p_video_id; ?>/mqdefault.jpg" width="275" height="155" />
   <div style="line-height: 5px;">&nbsp;</div>
   <strong><a href="http://www.youtube.com/embed/<?php echo $p_video_id; ?>">Read more and take action here.</a></strong>
-  <a class="colorbox-inline" href="?width=870&amp;height=420&amp;inline=true#<?php print $p_video_id; ?>"><div class="rr-preview-arrow"></div></a>
+  <a class="colorbox-inline rrpop-<?php print $p_video_id; ?>" href="?width=870&amp;height=420&amp;inline=true#<?php print $p_video_id; ?>"><div class="rr-preview-arrow"></div></a>
 </div>
 <div class="vidpop-overlay">
   <div id="$p_video_id"><?php print $p_popup; ?></div>
