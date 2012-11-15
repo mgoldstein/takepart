50e30870b1d81b283a68ab5f78073da7b24f8fb0
Phil Glatz <phil@electronovelty.com>
2012-11-15 10:36:13 -0800

    BugzID: #29946 - metrics

--- drupal/sites/all/modules/custom/unipop/js/unipop.js
+++ drupal/sites/all/modules/custom/unipop/js/unipop.js
@@ -24,6 +24,41 @@
     return output;
   }
 
+function unipop_loaded() {
+  vp_embeddedvideotitle = Drupal.settings.takepart_vidpop.embeddedvideotitle;
+  vp_embeddedvideotype  = Drupal.settings.takepart_vidpop.embeddedvideotype;
+
+  // there may be more than one embedded video on a page; send codes for each one
+  // collect passed titles and types
+  // event40 is called first, and sets these for other events as globals
+  var lookup_index = 0;
+
+  jQuery.each(vp_embeddedvideotitle, function(i, val){
+    vp_titles[i] = val;
+    vp_lookup[lookup_index] = i;
+    lookup_index++;
+  });
+  jQuery.each(vp_embeddedvideotype, function(i, val){
+    vp_types[i] = val;
+  });
+
+  for (i=0; i < lookup_index; i++) {
+    var li = vp_lookup[i];
+    var modaltitle = vp_titles[li];
+    var modaltype  = vp_types[li];
+    s.linkTrackVars="eVar30, prop30, eVar40, prop40, eVar42, prop42, events";
+    s.linkTrackEvents="event40";
+    s.events='event40';
+    s.prop30=s.pageName;
+    s.eVar30=s.pageName;
+    s.prop40=modaltype+':'+modaltitle;
+    s.eVar40=modaltype+':'+modaltitle;
+    s.prop42=modaltype;
+    s.eVar42=modaltype;
+    s.tl(true, 'o', 'Video Popup Page Load');
+  }
+};
+
 
 (function ($) {
 })(jQuery);
