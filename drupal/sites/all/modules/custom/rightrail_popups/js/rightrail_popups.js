dfc9068483719afe41ece82820ea63a963f233d2
Marc Goldstein <mgoldstein@takepart.com>
2012-12-13 14:21:23 -0800

    remove debug alerts from rightrail video popup javascript

--- drupal/sites/all/modules/custom/rightrail_popups/js/rightrail_popups.js
+++ drupal/sites/all/modules/custom/rightrail_popups/js/rightrail_popups.js
@@ -30,7 +30,7 @@
         var n = $(this).attr('class').match(/ rr-(\w+)/);
         n = n[1];
 
-        alert('rr 41');
+        // alert('rr 41');
         var modaltitle = Drupal.settings.unipop.unipop_titles[n];
         var modaltype  = Drupal.settings.unipop.unipop_types[n];
         //alert('in rr41 n:' + n + ' modaltitle:' + modaltitle + ' modaltype:' + modaltype);
@@ -54,7 +54,7 @@
           // block multiple calls
           rr_43_triggered = 1;
 
-          alert('rr 43');
+          // alert('rr 43');
           var n = $(this).attr('class').match(/ rr-(\w+)/);
           n = n[1];
           var modaltitle = Drupal.settings.unipop.unipop_titles[n];
