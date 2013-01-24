c7f47bc542183fb8b7174e927cd4922bdd1fc0c2
Marc Goldstein <mgoldstein@takepart.com>
2013-01-23 16:38:29 -0800

    BugzID: #47485 - Fix links in embedded galleries

--- drupal/profiles/takepart/themes/takepart3/templates/nodes/node--embed--openpublish-photo-gallery.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/templates/nodes/node--embed--openpublish-photo-gallery.tpl.php
@@ -6,12 +6,16 @@
         <?php
         print render($content['field_photo_gallery_slideshow']);
         // print render($content['field_promo_headline']);
-        $headline = l($field_promo_headline[0]['safe_value'], $uri['path'], array('attributes' => array('class' => array('gallery-headline') ), 'html' => TRUE));
+        $headline = l($field_promo_headline[0]['safe_value'], 'node/'.$nid, array('attributes' => array('class' => array('gallery-headline') ), 'html' => TRUE));
         print render($headline);
         print render($content['field_promo_text']);
-        print l('See Full Gallery', $uri['path'], array('attributes' => array('class' => array('see-full-gallery'))));
+        print l('See Full Gallery', 'node/'.$nid, array('attributes' => array('class' => array('see-full-gallery'))));
         ?>
     </div>
 </div>
-<?php if (!empty($post_object))
-    print render($post_object) ?>
+<?php 
+if (!empty($post_object)) {
+    print render($post_object);
+}
+dpm($node);
+?>
