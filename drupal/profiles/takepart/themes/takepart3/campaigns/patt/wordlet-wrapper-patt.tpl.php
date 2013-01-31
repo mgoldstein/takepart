1f87340483fe11d5af85f47fbaf3f2a48ca93e78
gburns <gburns@takepart.com>
2013-01-30 18:02:45 -0800

    adding and using wrapper for patt

--- drupal/profiles/takepart/themes/takepart3/campaigns/patt/wordlet-wrapper-patt.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/patt/wordlet-wrapper-patt.tpl.php
@@ -1,3 +1,55 @@
-sucka head
-<?=$content?>
-sucka foot
+<div id="page-wrapper" class="campaign">
+	<?php print $header ?>
+
+	<!-- start -->
+
+	<div class="page-wrap">
+		<div class="table int">
+		
+			<div class="header">
+				<div class="logo">
+					<img src="http://stage.bltdigital.com/apatt/images/interior-logo.png" width="357" height="206" alt="Interior Logo" usemap="#logo">
+					<map name="logo" id="logo">
+					  <area shape="poly" coords="13,87,59,16,343,57,344,132,254,153,232,177,200,176,175,158,58,174,61,122" href="index.html" />
+					</map>
+				</div>
+			</div>
+			
+			<div class="nav">
+				<ul>
+					<li class="active"><span>the film</span></li>
+					<li><a href="http://stage.bltdigital.com/apatt/take-action.html"><span>take action</span></a></li>
+					<li><a href="http://stage.bltdigital.com/apatt/gallery.html"><span>gallery: snap alum</span></a></li>
+					<li><a href="http://stage.bltdigital.com/apatt/book.html"><span>the book</span></a></li>
+					<li><a href="http://stage.bltdigital.com/apatt/alliances.html"><span>alliances</span></a></li>
+					<li><a href="http://stage.bltdigital.com/apatt/news.html"><span>news</span></a></li>
+					<li><a href="http://stage.bltdigital.com/apatt/events.html"><span>events + resources</span></a></li>
+					<li><a href="http://stage.bltdigital.com/apatt/assistance.html"><span>i need food assistance</span></a></li>
+				</ul>
+			</div>
+			
+			<div class="nav-overlay">
+				<ul>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+					<li></li>
+				</ul>
+			</div>
+
+			<div class="main">
+				<?=theme($content)?>
+			</div>
+			
+		</div><!-- /.table -->
+		
+	</div><!-- /.page-wrap -->
+
+	<!-- end -->
+
+	<?php print $footer ?>
+</div>
