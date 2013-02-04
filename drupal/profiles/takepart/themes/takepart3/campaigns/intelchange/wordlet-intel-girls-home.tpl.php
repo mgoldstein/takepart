2f62b07f19cee4524d66baf860b5aa07f17c0296
gburns <gburns@takepart.com>
2013-02-04 10:44:03 -0800

    Removing image maps and adding header to intel girls

--- drupal/profiles/takepart/themes/takepart3/campaigns/intelchange/wordlet-intel-girls-home.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/intelchange/wordlet-intel-girls-home.tpl.php
@@ -1,3 +1,20 @@
+<? if ( wordlet_edit_mode() ): ?>
+    <div class="wordlet_edit_mode">
+        <p>
+            Page Title: <?=w('title')?>
+        </p>
+        <? if ( ($wlinks = wordlet_configure_links()) ): ?>
+            <p>Configure:
+                <? foreach ( $wlinks as $wlink ): ?>
+                    <span>
+                        <?=$wlink?>
+                    </span>
+                <? endforeach ?>
+            </p>
+        <? endif ?>
+    </div>
+<? endif ?>
+
 <div id="page-wrapper" class="campaign">
 <?php print $header ?>
 
--- drupal/profiles/takepart/themes/takepart3/campaigns/patt/wordlet-patt-home.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/patt/wordlet-patt-home.tpl.php
@@ -4,9 +4,6 @@
 	
 	<div class="item salad">
 		<img class="item full" src="/profiles/takepart/themes/takepart3/campaigns/patt/images/table-salad.png" width="214" height="206" alt="salad bowl with salad" usemap="#salad">
-		<map name="salad" id="salad">
-		  <area shape="circle" coords="112,101,89" href="/hunger">
-		</map>
 		<a class="tag" href="/hunger">
 			<img src="/profiles/takepart/themes/takepart3/campaigns/patt/images/table-tag-news.png" width="180" height="183" alt="News and Resources" usemap="#news">
 		</a>
@@ -14,9 +11,6 @@
 	
 	<div class="item saltpepper">
 		<img class="item full" src="/profiles/takepart/themes/takepart3/campaigns/patt/images/table-saltpepper.png" width="105" height="68" alt="filled salt and pepper shakers" usemap="#saltpepper">
-		<map name="saltpepper" id="saltpepper">
-		  <area shape="poly" coords="4,27,20,10,82,4,100,19,98,36,84,55,42,61,24,60,6,41" href="<?=wu('patt_film')?>">
-		</map>
 		<a class="tag" href="<?=wu('patt_film')?>">
 			<img src="/profiles/takepart/themes/takepart3/campaigns/patt/images/table-tag-film.png" width="148" height="142" alt="The Film">
 		</a>
@@ -24,9 +18,6 @@
 	
 	<div class="item butter">
 		<img class="item full" src="/profiles/takepart/themes/takepart3/campaigns/patt/images/table-butter-full-touch.png" width="173" height="111" alt="butter dish with stick of butter" usemap="#butter">
-		<map name="butter" id="butter">
-		  <area shape="rect" coords="12,23,164,93" href="<?=wu('patt_book')?>">
-		</map>
 		<a class="tag" href="<?=wu('patt_book')?>">
 			<img src="/profiles/takepart/themes/takepart3/campaigns/patt/images/table-tag-book.png" width="163" height="154" alt="The Book">
 		</a>
@@ -34,9 +25,6 @@
 	
 	<div class="item cola">
 		<img class="item full" src="/profiles/takepart/themes/takepart3/campaigns/patt/images/table-cola-full-touch.png" width="148" height="136" alt="glass full with cola" usemap="#cola">
-		<map name="cola" id="cola">
-		  <area shape="circle" coords="75,68,54" href="<?=wu('patt_alliances')?>">
-		</map>
 		<a class="tag" href="<?=wu('patt_alliances')?>">
 			<img src="/profiles/takepart/themes/takepart3/campaigns/patt/images/table-tag-alliances.png" width="155" height="151" alt="alliances" usemap="#alliances">
 		</a>
@@ -44,9 +32,6 @@
 	
 	<div class="item utensils">
 		<img class="item full" src="/profiles/takepart/themes/takepart3/campaigns/patt/images/table-utensils-full.png" width="250" height="299" alt="utensils on a napkin with an olive" usemap="#utensils">
-		<map name="utensils" id="utensils">
-		  <area shape="poly" coords="11,58,132,6,142,24,155,22,238,218,222,225,229,241,96,289" href="<?=wu('patt_events')?>">
-		</map>
 		<a class="tag" href="<?=wu('patt_events')?>">
 			<img src="/profiles/takepart/themes/takepart3/campaigns/patt/images/table-tag-events.png" width="162" height="161" alt="Events" usemap="#events">
 		</a>
@@ -54,9 +39,6 @@
 	
 	<div class="item bread">
 		<img class="item full" src="/profiles/takepart/themes/takepart3/campaigns/patt/images/table-bread.png" width="169" height="168" alt="bread with butter on a plate" usemap="#bread">
-		<map name="bread" id="bread">
-		  <area shape="circle" coords="91,81,68" href="<?=wu('patt_snap')?>">
-		</map>
 		<a class="tag" href="<?=wu('patt_snap')?>">
 			<img src="/profiles/takepart/themes/takepart3/campaigns/patt/images/table-tag-gallery.png" width="161" height="158" alt="Gallery">
 		</a>
@@ -64,9 +46,6 @@
 	
 	<div class="item knife">
 		<img class="item full" src="/profiles/takepart/themes/takepart3/campaigns/patt/images/table-knife.png" width="148" height="87" alt="butter knife" usemap="#knife">
-		<map name="knife" id="knife">
-		  <area shape="poly" coords="8,21,4,13,7,4,15,3,144,64,135,81,71,50,55,48,13,29" href="<?=wu('patt_assistance')?>">
-		</map>
 		<a class="tag" href="<?=wu('patt_assistance')?>">
 			<img src="/profiles/takepart/themes/takepart3/campaigns/patt/images/table-tag-assistance.png" width="144" height="142" alt="Assistance">
 		</a>
@@ -74,9 +53,6 @@
 	
 	<div class="item dinner">
 		<img class="item full" src="/profiles/takepart/themes/takepart3/campaigns/patt/images/table-dinner-full.png" width="435" height="436" alt="dinner plate with food" usemap="#dinner">
-		<map name="dinner" id="dinner">
-		   <area shape="circle" coords="229,214,194" href="<?=wu('patt_action')?>">
-		</map>
 		<a class="tag" href="<?=wu('patt_action')?>">
 			<img src="/profiles/takepart/themes/takepart3/campaigns/patt/images/table-tag-take-action.png" width="157" height="152" alt="Take Action">
 		</a>
