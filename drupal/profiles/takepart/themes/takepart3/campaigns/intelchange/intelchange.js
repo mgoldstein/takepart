fafc87ecec7defdbf15d0ee776311c3735ea1ebe
gburns <gburns@takepart.com>
2013-02-06 20:21:09 -0800

    fixing tabbing to video

--- drupal/profiles/takepart/themes/takepart3/campaigns/intelchange/intelchange.js
+++ drupal/profiles/takepart/themes/takepart3/campaigns/intelchange/intelchange.js
@@ -1,24 +1,26 @@
 (function(window, $, undefined) {
 // Setup
 
-
 // Document load
 $(function() {
 
 var intelChange = $('.smartgirls');
-var vidEmbed = $('.vid-embed', intelChange).css('position', 'relative');
-var vid = $('iframe', vidEmbed).css('position', 'absolute').css('opacity', 0);
+var vidEmbed = intelChange.find('.vid-embed').css('position', 'relative');
+var vid = vidEmbed.find('iframe')
+    .css({
+        position: 'absolute',
+        opacity: '0'
+    });
 var vidPreview = $('<img id="intel_change_play_video" />').addClass('vid-preview').css('cursor', 'pointer');
-vidPreview.attr('src', vidEmbed.attr('data-previewSrc'));
+vidPreview.attr('src', vidEmbed.data('previewSrc'));
 vidEmbed.append(vidPreview);
 
 // youtube tab show
 $(window).bind('keydown', function() {
     setTimeout(function() {
-        if (false){ //TODO: document.activeElement === "this iframe"
+        if ( document.activeElement == vid[0] ) {
             showIntelGirlsVideo();
         }
-        
     }, 100);
 });
 
@@ -26,11 +28,12 @@ $(window).bind('keydown', function() {
 var hoverframe = null;
 $('body')
     .delegate('.vid-embed iframe', 'mouseover', function() {
-      hoverframe = this;
+        hoverframe = this;
     })
     .delegate('.vid-embed iframe', 'mouseout', function() {
         hoverframe = null;  
     });
+
 $(window).bind('blur', function() {
     if ( hoverframe ) {
         showIntelGirlsVideo();
