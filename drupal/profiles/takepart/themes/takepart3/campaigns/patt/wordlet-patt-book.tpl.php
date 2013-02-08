ab67ae9dbe6a104517497eeb8402618aa45ca003
gburns <gburns@takepart.com>
2013-02-07 20:47:25 -0800

    hiding sponsor section on book page via css

--- drupal/profiles/takepart/themes/takepart3/campaigns/patt/wordlet-patt-book.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/patt/wordlet-patt-book.tpl.php
@@ -22,27 +22,4 @@
 		</div>
 	</div><!-- /.inner -->
 </div><!-- /.column-2 -->
-<div class="column column-3">
-	<div class="content">
-		<div class="email-signup">
-			<form name="email-form" action="" method="get">
-			<h3><?=w('email_signup')?></h3>
-		    <div class="input"><input type="text"  name="email"></div>
-            <div class="submit"><input type="image" value="Submit" src="/profiles/takepart/themes/takepart3/campaigns/patt/images/interior-form-btn.png"></div>
-			</form>
-		</div>
-		<div class="follow">
-			<h4><?=w('follow_us')?></h4>
-			<?=we('follow_links')?>
-			<? if ( w('follow_links') ): ?>
-				<ul>
-					<? foreach ( wl('follow_links') as $w ): ?>
-						<li><a href="<?=$w->href?>">
-							<img src="<?=$w->img_src?>" alt="<?=$w->single(false)?>" />
-						</a></li>
-					<? endforeach ?>
-				</ul>
-			<? endif ?>
-		</div>
-	</div>
-</div><!-- /.column-3 -->
+<? include_once('subtemplates/patt-right.tpl.php') ?>
