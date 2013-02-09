410427a4cc618513b37b28b44cd4179497132712
gburns <gburns@takepart.com>
2013-02-08 18:42:04 -0800

    adding snap gallery.

--- drupal/profiles/takepart/themes/takepart3/campaigns/patt/patt.js
+++ drupal/profiles/takepart/themes/takepart3/campaigns/patt/patt.js
@@ -12,8 +12,56 @@ if ( $columns.length ) {
 		offsetNode: '.page-wrap .main'
 	});
 
-	$('.page-wrap .main').height($('.page-wrap .main').outerHeight());
+	var height = $('.page-wrap .main').outerHeight();
+	$columns.each(function() {
+		var tempheight;
+		height = ( (tempheight = $(this).parent().outerHeight()) && tempheight > height ) ? tempheight : height;
+	});
+
+	$columns.parent().add('.page-wrap .main').height(height);
 }
+
+// Snap gallery
+
+var $modal = $('#grid-modal');
+var $overlay = $('#grid-overlay');
+var speed = 'fast';
+var $current_tile;
+
+var show_modal = function() {
+	$modal.find('.modal-left').html($current_tile.find('.modal-left').html());
+	$modal.find('.modal-right').html($current_tile.find('.modal-right').html());
+};
+
+$('body')
+	.delegate('.tile', 'click', function() {
+		$current_tile = $(this);
+		show_modal();
+
+		$overlay.fadeIn(speed);
+		$modal.fadeIn(speed);
+	})
+	.delegate('.close-btn', 'click', function() {
+		$modal.fadeOut(speed);
+		$overlay.fadeOut(speed);
+	})
+	.delegate('#nav-right', 'click', function() {
+		$current_tile = $current_tile.next('.tile.person, .tile.fact');
+		if ( !$current_tile.length ) {
+			$current_tile = $('.tile.person, .tile.fact').first();
+		}
+
+		show_modal();
+	})
+	.delegate('#nav-left', 'click', function() {
+		$current_tile = $current_tile.prev('.tile.person, .tile.fact');
+		if ( !$current_tile.length ) {
+			$current_tile = $('.tile.person, .tile.fact').last();
+		}
+
+		show_modal();
+	})
+	;
 
 });
 })(window, jQuery);
