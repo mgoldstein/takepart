f516ffcd87508c27829cd3613d2c3cf402c8b2c5
Dustin Boersma <dustin.boersma@gmail.com>
2013-02-11 15:46:12 -0800

    intel change thank you message styling

--- drupal/profiles/takepart/themes/takepart3/campaigns/intelchange/wordlet-intel-girls-home.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/intelchange/wordlet-intel-girls-home.tpl.php
@@ -37,12 +37,7 @@
             <div class="form_wrapper">
                 <div class="form-bg"></div>
                 <div class="form-content">
-                    <h3><?=w("form_title")?></h3>
-                    <?=w("form_blurb")?>
-
                     <?=w("signup_form")?>
-                    <span class="optional-info">&#42;<?=w("form_optional_info")?></span>
-                    <a class="terms-link" href="<?=w("terms_link")->href?>" target="_blank"><?=w("terms_link")->single?></a>
                 </div>
             </div>
         </div>
