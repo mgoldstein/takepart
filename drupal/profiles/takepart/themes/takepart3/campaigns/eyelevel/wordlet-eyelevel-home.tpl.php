fee24cf045d21011d1d4ef40b1b129a2bcc8be64
gburnstakepart <gburns@takepart.com>
2013-02-15 17:01:23 -0800

    fixing video wordlet on homepage

--- drupal/profiles/takepart/themes/takepart3/campaigns/eyelevel/wordlet-eyelevel-home.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/eyelevel/wordlet-eyelevel-home.tpl.php
@@ -1,6 +1,6 @@
 <div class="content">
-	<div class="video_intro">
-		<iframe width="560" height="315" src="http://www.youtube.com/embed/<?=$w->video?>" frameborder="0" allowfullscreen></iframe>
+	<div class="video_intro" <?=wa('video_intro')?>>
+		<iframe width="560" height="315" src="http://www.youtube.com/embed/<?=wa('video_intro')->video?>" frameborder="0" allowfullscreen></iframe>
 	</div>
 
 	<h2><?=w('headline')?></h2>
