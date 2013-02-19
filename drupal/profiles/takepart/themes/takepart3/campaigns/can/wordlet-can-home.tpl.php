ea42528aefe573031a3a00051631a40ca7d9db1c
Dustin Boersma <dustin.boersma@gmail.com>
2013-02-19 15:06:23 -0800

    Fogbugz #47246 - first round of can campaign styling

--- drupal/profiles/takepart/themes/takepart3/campaigns/can/wordlet-can-home.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/can/wordlet-can-home.tpl.php
@@ -1,10 +1,5 @@
 <div class="content">
-	<div class="primary">
-		<h2><?=w('headline')?></h2>
-		<div class="body cms">
-			<?=w('body')?>
-		</div>
-	</div>
+	<h2><?=w('headline')?></h2>
 	<div class="secondary">
 		<div class="signup_form" <?=wa('signup_form')?>>
 			<? if ( $w = w('signup_form') ): ?>
@@ -21,5 +16,8 @@
 				<? endforeach ?>
 			</ul>
 		</div>
+	</div>
+	<div class="body cms">
+		<?=w('body')?>
 	</div>
 </div>
