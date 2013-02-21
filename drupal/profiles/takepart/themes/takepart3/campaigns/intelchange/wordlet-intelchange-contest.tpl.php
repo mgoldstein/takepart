06d81a412ec3ca324313596e78f12fae16d2f8e7
gburns <gburns@takepart.com>
2013-02-21 15:06:43 -0800

    adding patt iframe, fixing header styling

--- drupal/profiles/takepart/themes/takepart3/campaigns/intelchange/wordlet-intelchange-contest.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/intelchange/wordlet-intelchange-contest.tpl.php
@@ -1,1 +1,13 @@
-Contest
+<div class="first-block">
+	<? if ( $w = w('first_block') ): ?>
+		<p class="image">
+			<img src="<?=$w->img_src?>"/>
+		</p>
+		<div class="first-content">
+			<h2><?=$w->single?></h2>
+			<div class="cms">
+				<?=$w->multi?>
+			</div>
+		</div>
+	<? endif ?>
+</div>
---
+++ drupal/profiles/takepart/themes/takepart3/campaigns/patt/wordlet-patt-nav.tpl.php
@@ -0,0 +1,29 @@
+<div id="page-wrapper" class="campaign">
+	<?=$header?>
+	<div class="page-wrap">
+		<div class="table int">
+			<div class="header">
+				<div class="logo">
+					<a href="<?=wu('patt_home')?>">
+						<? if ( $w = w('logo') ): ?>
+							<img src="<?=$w->img_src?>" width="357" height="206" alt="<?=$w->single(false)?>" <?=wa('logo')?> />
+						<? endif ?>
+					</a>
+				</div>
+				<p class="mobile" id="mobile_back">
+					<a href="<?=wu('patt_home')?>"><?=w('back')?></a>
+				</p>
+			</div>
+			
+			<div class="nav">
+				<ul <?=wa('top_nav')?>>
+					<? foreach ( wl('top_nav') as $w ): ?>
+						<li class="<?=wc($w->href_raw)?>"><a href="<?=$w->href?>">
+							<span><?=$w->single(false)?></span>
+						</a></li>
+					<? endforeach ?>
+				</ul>
+			</div>
+		</div><!-- /.table -->
+	</div><!-- /.page-wrap -->
+</div>
