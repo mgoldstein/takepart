66f8ceab5e750728a8f8476e044c0d88231f1a62
gburns <gburns@takepart.com>
2013-02-22 14:25:43 -0800

    eye level css fix for wordlets. adding active wordlet value.

--- drupal/profiles/takepart/themes/takepart3/campaigns/eyelevel/wordlet-eyelevel-video.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/eyelevel/wordlet-eyelevel-video.tpl.php
@@ -4,31 +4,35 @@
 	<div class="artists-list">
 		<ul <?=wa('artists')?>>
 			<? foreach( wl('artists') as $w ): ?>
-				<li><a href="#<?=$w->token?>">
-					<div class="portrait">
-						<img src="<?=$w->img_src?>" alt="Portrait"/>
-					</div>
-					<span class="name">
-						<?=$w->single(false)?>
-					</span>
-				</a></li>
+				<? if ( $w->active ): ?>
+					<li><a href="#<?=$w->token?>">
+						<div class="portrait">
+							<img src="<?=$w->img_src?>" alt="Portrait"/>
+						</div>
+						<span class="name">
+							<?=$w->single(false)?>
+						</span>
+					</a></li>
+				<? endif ?>
 			<? endforeach ?>
 		</ul>
 	</div>
 
 	<div class="artist-info" <?=wa('artists')?>>
 		<? foreach( wl('artists') as $w ): ?>
-			<div id="<?=$w->token?>" class='artist'>
-				<div class="info-wrapper">
-					<h3><?=$w->single(false)?></h3>
-					<div class="info cms">
-						<?=$w->multi(false)?>
+			<? if ( $w->active ): ?>
+				<div id="<?=$w->token?>" class='artist'>
+					<div class="info-wrapper">
+						<h3><?=$w->single(false)?></h3>
+						<div class="info cms">
+							<?=$w->multi(false)?>
+						</div>
+					</div>
+					<div class="video">
+						<iframe width="560" height="315" src="http://www.youtube.com/embed/<?=$w->video?>" frameborder="0" allowfullscreen></iframe>
 					</div>
 				</div>
-				<div class="video">
-					<iframe width="560" height="315" src="http://www.youtube.com/embed/<?=$w->video?>" frameborder="0" allowfullscreen></iframe>
-				</div>
-			</div>
+			<? endif ?>
 		<? endforeach ?>
 	</div>
 </div>
