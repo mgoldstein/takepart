d884b9260651dd3492bb76695ef26a3f725ef160
gburns <gburns@takepart.com>
2013-03-07 11:25:34 -0800

    removing / so subdomain.takepart.com links will stop opening in _blank

--- drupal/profiles/takepart/themes/takepart3/campaigns/campaign.js
+++ drupal/profiles/takepart/themes/takepart3/campaigns/campaign.js
@@ -7,7 +7,7 @@ $(function() {
 // Delegates
 $('body')
 	// Open non-takepart URLs in a new window
-	.delegate('a:not([href*="takepart.com/"])', 'click', function() {
+	.delegate('a:not([href*="takepart.com"])', 'click', function() {
 		if ( location.hostname != this.hostname ) {
 			$(this).attr({target: '_blank'});
 		}
