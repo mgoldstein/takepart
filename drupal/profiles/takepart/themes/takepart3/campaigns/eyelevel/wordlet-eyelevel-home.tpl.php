e1863c9e458482411ce920acbda6906223f52854
Dustin Boersma <dustin.boersma@gmail.com>
2013-03-14 13:53:16 -0700

    Fogubgz 53033 - moved paragraph above video

--- drupal/profiles/takepart/themes/takepart3/campaigns/eyelevel/wordlet-eyelevel-home.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/eyelevel/wordlet-eyelevel-home.tpl.php
@@ -1,13 +1,9 @@
 <div class="content">
+    <div class="body cms">
+        <?=w('body')?>
+    </div>
 	<div class="video_intro" <?=wa('video_intro')?>>
         <iframe allowfullscreen="" frameborder="0" height="480" src="http://www.youtube.com/embed/<?=w('video_intro')->video?>" width="853"></iframe>
 	</div>
-
 	<? include_once('subtemplates/eyelevel-artists-nav.tpl.php') ?>
-
-	<h2><?=w('headline')?></h2>
-
-	<div class="body cms">
-		<?=w('body')?>
-	</div>
 </div>
