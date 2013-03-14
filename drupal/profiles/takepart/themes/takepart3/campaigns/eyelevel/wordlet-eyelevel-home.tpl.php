f3ab701e3d806fb9a61d817551fe035719b887ac
Dustin Boersma <dustin.boersma@gmail.com>
2013-03-14 13:56:24 -0700

    Fogbugz 53034 - added header back as artist nav headline

--- drupal/profiles/takepart/themes/takepart3/campaigns/eyelevel/wordlet-eyelevel-home.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/eyelevel/wordlet-eyelevel-home.tpl.php
@@ -5,5 +5,6 @@
 	<div class="video_intro" <?=wa('video_intro')?>>
         <iframe allowfullscreen="" frameborder="0" height="480" src="http://www.youtube.com/embed/<?=w('video_intro')->video?>" width="853"></iframe>
 	</div>
-	<? include_once('subtemplates/eyelevel-artists-nav.tpl.php') ?>
+	<h2><?=w('headline')?></h2>
+    <? include_once('subtemplates/eyelevel-artists-nav.tpl.php') ?>
 </div>
