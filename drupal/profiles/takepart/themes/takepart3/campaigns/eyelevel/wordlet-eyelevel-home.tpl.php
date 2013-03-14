87241cfbf2151b2637602359fbb89d4efcf03491
Dustin Boersma <dustin.boersma@gmail.com>
2013-03-14 14:22:31 -0700

    Fogbugz 53033 - added main header back into wordlets just in case we need it

--- drupal/profiles/takepart/themes/takepart3/campaigns/eyelevel/wordlet-eyelevel-home.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/eyelevel/wordlet-eyelevel-home.tpl.php
@@ -1,10 +1,11 @@
 <div class="content">
+    <h2 class='main-headline'><?=w('main_headline')?></h2>
     <div class="body cms">
         <?=w('body')?>
     </div>
 	<div class="video_intro" <?=wa('video_intro')?>>
         <iframe allowfullscreen="" frameborder="0" height="480" src="http://www.youtube.com/embed/<?=w('video_intro')->video?>" width="853"></iframe>
 	</div>
-	<h2><?=w('headline')?></h2>
+	<h2 class='nav-headline'><?=w('headline')?></h2>
     <? include_once('subtemplates/eyelevel-artists-nav.tpl.php') ?>
 </div>
