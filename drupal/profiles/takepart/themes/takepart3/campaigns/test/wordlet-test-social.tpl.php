f22ef2781d302fa479268e6525d4249f86569fdc
gburnstakepart <gburns@takepart.com>
2013-03-20 21:17:41 -0700

    adding scripts etc to social test page

--- drupal/profiles/takepart/themes/takepart3/campaigns/test/wordlet-test-social.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/test/wordlet-test-social.tpl.php
@@ -1,6 +1,18 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+	<meta charset="utf-8">
+	<title>Social Test</title>
+	<!--[if lt IE 9]>
+		<script src="/profiles/takepart/themes/takepart3/js/html5shiv.js"></script>
+	<![endif]-->
+</head>
+<body>
+<div id="fb-root"></div>
+
 <!-- campaign wrapper start -->
 <div id="page-wrapper" class="campaign">
-	<?=$header ?>
+	<!-- ?=$header ? -->
 
 	<div class="page-wrap">
 		<div class="main">
@@ -55,15 +67,65 @@
 		</div>
 	</div>
 
-	<?=$footer ?>
+	<!-- ?=$footer ? -->
 </div>
 <!-- campaign wrapper end -->
 
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.js" defer="defer"></script>
+<script type="text/javascript" src="http://jp1.dev.takepart.com/profiles/takepart/themes/takepart3/js/analytics.js?mjzh5o" defer="defer"></script>
+<script type="text/javascript" src="http://jp1.dev.takepart.com/profiles/takepart/themes/takepart3/js/script.js?mjzh5o" defer="defer"></script>
+<script type="text/javascript" src="http://jp1.dev.takepart.com/profiles/takepart/themes/takepart3/js/jquery.lazyload.js?mjzh5o" defer="defer"></script>
+<script type="text/javascript" src="http://jp1.dev.takepart.com/profiles/takepart/themes/takepart3/js/tpsocial.jquery.js?mjzh5o" defer="defer"></script>
+<script type="text/javascript" src="http://jp1.dev.takepart.com/profiles/takepart/themes/takepart3/js/tpautoTab.jquery.js?mjzh5o" defer="defer"></script>
+<script type="text/javascript" src="http://jp1.dev.takepart.com/profiles/takepart/themes/takepart3/js/tpmodal.jquery.js?mjzh5o" defer="defer"></script>
+<script type="text/javascript" src="http://jp1.dev.takepart.com/profiles/takepart/themes/takepart3/js/tpsticky.jquery.js?mjzh5o" defer="defer"></script>
+<script type="text/javascript" src="http://jp1.dev.takepart.com/profiles/takepart/themes/takepart3/js/autosize.jquery.js?mjzh5o" defer="defer"></script>
+
+<!-- SiteCatalyst code version: H.23.4.
+Copyright 1997-2009 Omniture, Inc. More info available at http://www.omniture.com -->
+<script type="text/javascript" language="JavaScript" src="/profiles/takepart/modules/features/takepart_omniture/js/s_code_h23_core.js"></script>
+<script type="text/javascript" language="JavaScript"><!--
+s.prop19="2011/05/20";
+s.eVar21="";
+s.prop20="";
+s.prop4="Section Front";
+s.eVar18="TakePart Home";
+s.prop17="TakePart Home";
+s.eVar17="";
+s.prop16="";
+s.prop6="7";
+s.channel="node";
+s.eVar11="Logged_In";
+s.pageName="takepart:node:TakePart Home";
+s.prop1="node";
+/************* DO NOT ALTER ANYTHING BELOW THIS LINE ! **************/
+var s_code=s.t();if(s_code)document.write(s_code)//--></script>
+<script language="JavaScript" type="text/javascript"><!--
+if(navigator.appVersion.indexOf('MSIE')>=0)document.write(unescape('%3C')+'\!-'+'-')
+//--></script><noscript><a href="http://www.omniture.com" title="Web Analytics"><img src=" http://takepart.122.2o7.net/b/ss/takeparttakepartdev2/1/H.23.4--NS/0/5588120"
+height="1" width="1" border="0" alt="" /></a></noscript><!--/DO NOT REMOVE/-->
+<!-- End SiteCatalyst code version: H.23.4. -->
+
 <script type="text/javascript">
-
-// Page:
-(function(window, $, undefined) {
-
-})(window, jQuery);
-
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '247137505296280', // App ID
+    channelUrl : '//jp1.dev.takepart.com/facebook/channel', // Channel File
+    status     : true, // check login status
+    cookie     : true, // enable cookies to allow the server to access the session
+    xfbml      : true  // parse XFBML
+  });
+  // Additional initialization code here
+};
+(function(d){
+  var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+  js = d.createElement('script'); js.id = id; js.async = true;
+  js.src = "//connect.facebook.net/en_US/all.js";
+  d.getElementsByTagName('head')[0].appendChild(js);
+}(document));
 </script>
+
+<script type="text/javascript" src="//cdn.optimizely.com/js/77413453.js"></script>
+
+</body>
+</html>
