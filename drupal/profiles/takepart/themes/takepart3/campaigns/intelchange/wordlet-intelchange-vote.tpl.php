4b0d0ec52a940cabb90860efc932f945d97f3836
Dustin Boersma <dustin.boersma@gmail.com>
2013-04-03 14:48:22 -0700

    #TP-38 beginning of intel voting page markup

--- drupal/profiles/takepart/themes/takepart3/campaigns/intelchange/wordlet-intelchange-vote.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/intelchange/wordlet-intelchange-vote.tpl.php
@@ -2,13 +2,17 @@
     <div class="header-block">
         <h3><?=w('cta_header')?></h3>
         <h4><?=w('cta_subheader')?></h4>
+        <div class="vote-desc">
+            <?=w('vote_desc')?>
+        </div>
     </div>
     <? if ( $w = w('cta_image') ): ?>
     <div class="prize-image">
         <img src="<?=$w->img_src?>" alt="<?=$w->single(false)?>">
     </div>
     <? endif ?>
-    <div class="vote-desc">
-        <?=w('vote_desc')?>
+    <div class="vote" <?=wa('finalists')?>>
+        <h4><?=w('vote_header')?></h4>
+        <?= count(wl('finalists')) ?>
     </div>
 </div>
