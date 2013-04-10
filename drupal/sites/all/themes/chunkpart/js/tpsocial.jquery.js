5b89707ac8a67c5652737a87d4f60dd144bfb1c9
gburns <gburns@takepart.com>
2013-04-10 14:54:22 -0700

    adding messenger and stumbleupon

--- drupal/sites/all/themes/chunkpart/js/tpsocial.jquery.js
+++ drupal/sites/all/themes/chunkpart/js/tpsocial.jquery.js
@@ -377,6 +377,57 @@ $.tpsocial.add_service({
 	}
 });
 
+$.tpsocial.add_service({
+	name: 'live',
+	display: 'Messenger',
+	width: 1020,
+	height: 570,
+	share: function(args) {
+		var url = 'https://profile.live.com/P.mvc#!/badge?url=' + encodeURIComponent(args.url) + '&title=' + encodeURIComponent(args.title);
+
+		var windowOptions = 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes';
+		/*var left = 0;
+		var tops = Number((screen.height/2)-(args.height/2));*/
+		window.open(url, undefined, [windowOptions,"width="+args.width,"height="+args.height/*,"left="+left,"top="+tops*/].join(", "));
+
+		$window.trigger(cpre + 'share', args);
+	}
+});
+
+$.tpsocial.add_service({
+	name: 'digg',
+	display: 'Digg',
+	width: 1070,
+	height: 670,
+	share: function(args) {
+		var url = 'http://digg.com/submit?url=' + encodeURIComponent(args.url) + '&title=' + encodeURIComponent(args.title) + '&bodytext=';
+
+		var windowOptions = 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes';
+		/*var left = 0;
+		var tops = Number((screen.height/2)-(args.height/2));*/
+		window.open(url, undefined, [windowOptions,"width="+args.width,"height="+args.height/*,"left="+left,"top="+tops*/].join(", "));
+
+		$window.trigger(cpre + 'share', args);
+	}
+});
+
+$.tpsocial.add_service({
+	name: 'stumbleupon',
+	display: 'Stumbleupon',
+	width: 1070,
+	height: 670,
+	share: function(args) {
+		var url = 'http://www.stumbleupon.com/submit?url=' + encodeURIComponent(args.url) + '&title=' + encodeURIComponent(args.title);
+
+		var windowOptions = 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes';
+		/*var left = 0;
+		var tops = Number((screen.height/2)-(args.height/2));*/
+		window.open(url, undefined, [windowOptions,"width="+args.width,"height="+args.height/*,"left="+left,"top="+tops*/].join(", "));
+
+		$window.trigger(cpre + 'share', args);
+	}
+});
+
 // Email
 
 var email_args;
