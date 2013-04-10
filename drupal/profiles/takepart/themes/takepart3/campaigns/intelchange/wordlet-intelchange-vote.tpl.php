2e61ed3dd21ef0a96d862e915d29e118c60f528b
Dustin Boersma <dustin.boersma@gmail.com>
2013-04-10 12:40:10 -0700

    #TP-23 moved intel prize image into header block on voting page and removed margin top on the block

--- drupal/profiles/takepart/themes/takepart3/campaigns/intelchange/wordlet-intelchange-vote.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/intelchange/wordlet-intelchange-vote.tpl.php
@@ -5,12 +5,12 @@
         <div class="vote-desc">
             <?=w('vote_desc')?>
         </div>
+        <? if ( $w = w('cta_image') ): ?>
+        <div class="prize-image">
+            <img src="<?=$w->img_src?>" alt="<?=$w->single(false)?>">
+        </div>
+        <? endif ?>
     </div>
-    <? if ( $w = w('cta_image') ): ?>
-    <div class="prize-image">
-        <img src="<?=$w->img_src?>" alt="<?=$w->single(false)?>">
-    </div>
-    <? endif ?>
     <div class="vote" <?=wa('finalists')?>>
         <h4><?=w('vote_header')?></h4>
         <?= count(wl('finalists')) ?>
