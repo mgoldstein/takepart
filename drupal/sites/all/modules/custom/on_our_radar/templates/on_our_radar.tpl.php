a91d1bf1b5584233d1fdb2289c720beae057b1ba
gburns <gburns@takepart.com>
2013-04-12 17:25:26 -0700

    adding/updating html

--- drupal/sites/all/modules/custom/on_our_radar/templates/on_our_radar.tpl.php
+++ drupal/sites/all/modules/custom/on_our_radar/templates/on_our_radar.tpl.php
@@ -3,4 +3,16 @@
    *  Variables defined in on_our_radar.module
    */
 ?>
-<?php print_r($links); ?>
+
+<ol>
+	<? foreach ( $links as $link ): ?>
+		<? if ( $link['title'] ): ?>
+			<li>
+				<a href="<?=$link['link'] ?>">
+					<p class="topic"><?=$link['topic'] ?></p>
+					<p class="topic"><?=$link['title'] ?></p>
+				</a>
+			</li>
+		<? endif ?>
+	<? endforeach ?>
+</ol>
