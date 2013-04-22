f75627d8d65e18e9225fffd439841fbcfc7b1fb8
Charles Toepfer <ctoepfer@ctoepfer-workvm.(none)>
2013-04-22 15:15:57 -0700

    Adding templates / preprocessor for of the day.

---
+++ drupal/profiles/takepart/themes/takepart3/templates/entities/bean--of-the-day-custom.tpl.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * Theme implementation for "of the day" block.
+ *
+ * Available variables:
+ * - $custom_render
+ 
+ * @see template_preprocess_entity()
+ */
+?>
+Start Template <pre><?php print_r($custom_render); ?></pre> End Template
