afef0e462710a0a879529e005609e8204025a103
gburns <gburns@takepart.com>
2013-05-01 15:07:08 -0700

    styling slim header

--- drupal/profiles/takepart/themes/takepart3/templates/pages/header.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/templates/pages/header.tpl.php
@@ -1,13 +1,9 @@
 <header id="site-header" role="banner"><div id="site-header-inner">
   <p id="site-logo">
-    <a href="/"><img src="<?=$logo?>" alt="TakePart" role="logo" /></a>
+    <a href="/"><img src="<?=($is_multipage)?'/profiles/takepart/themes/takepart3/images/logo-sm-slim.png':$logo?>" alt="TakePart" role="logo" /></a>
   </p>
 
   <div id="site-navs">
-    <nav id="site-user-nav">
-      <?=$user_nav ?>
-    </nav>
-
     <nav id="site-main-nav">
       <?php print $top_nav ?>
     </nav>
@@ -23,6 +19,10 @@
   </div>
 
   <div id="site-more-navs">
+    <nav id="site-user-nav">
+      <?=$user_nav ?>
+    </nav>
+
     <nav id="site-search">
       <?php print drupal_render($search_takepart_form); ?>
     </nav>
@@ -33,41 +33,3 @@
     </div>
   </div>
 </div></header>
-
-
-<!-- div id="header-wrapper" class="clearfix regular-content <?=($is_multipage)?'slim-header':''?>">
-	<div id="join-login-top">
-  	<div class="login-fb clearfix">
-    	<?php print $user_nav; ?>       
-    </div> 
-  </div><!--/join-login-top- ->
-  <?php print $follow_us_links; ?><!--/top follow- ->
-  <div class="logo-wrapper">
-		<div id="logo"><?php print l("Home", "<front>", array('attributes' => array('title' => 'TakePart logo', 'absolute' => TRUE) ) )?></div>
-		<div class="header-right">
-      <div id="nav-wrap" class="clear clearfix">
-        <div id="block-menu-block-1">
-          <?php print $top_nav ?>
-        </div>
-      </div>
-      <div id="participant-pulldown">
-        <div class="pp-button"></div>
-        <div class="inner">
-          <?php print $participant_pulldown ?>
-        </div>
-      </div>
-      <div class="row2 clear clearfix">
-        <div id="hot-topics-nav">
-          <?php print $hottopic_nav ?>
-        </div><!--/hot topics nav- ->
-        <div id="dontmiss-nav">
-          <div class="dont">Don't Miss:</div>
-          <?php print $dontmiss_nav ?>
-        </div><!--/hot topics nav- ->
-        <div id="top-search">
-          <div class="tpform-item"><?php print drupal_render($search_takepart_form); ?></div>
-        </div><!--/top search- ->
-      </div>
-    </div>
-  </div>
-</div -->
