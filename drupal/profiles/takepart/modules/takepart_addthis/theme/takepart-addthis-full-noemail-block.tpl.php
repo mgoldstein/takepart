19cb7e346e07c678adb65cd0a8046f3f5a75afd8
Charles Toepfer <ctoepfer@takepart.com>
2013-05-06 18:25:36 -0700

    Added full URL to FB Shares / likes

--- drupal/profiles/takepart/modules/takepart_addthis/theme/takepart-addthis-full-noemail-block.tpl.php
+++ drupal/profiles/takepart/modules/takepart_addthis/theme/takepart-addthis-full-noemail-block.tpl.php
@@ -4,7 +4,7 @@ $node = menu_get_object();
 <!-- AddThis Button BEGIN -->
 <div class="takepart_addthis_leftpanel clearfix takepart_addthis_noemail">
   <div class="addthis_toolbox addthis_default_style">
-    <a class="addthis_button_facebook_like" fb:like:layout="button_count" fb:like:action="<?php print $variables['takepart_addthis_facebook_like_text'];?>"></a> 
+    <a class="addthis_button_facebook_like" fb:like:layout="button_count" fb:like:action="<?php print $variables['takepart_addthis_facebook_like_text'];?>" fb:like:href="<?php echo url(NULL, array('absolute' => TRUE)); ?>"></a>
     <a class="addthis_button_tweet" tw:via="<?php print $variables['takepart_addthis_tweet_via']; ?>" tw:text="<?php print $node->title; ?>" tw:url="<?php echo ((empty($_SERVER["HTTPS"])) ? 'http://' : 'https://') . $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"]; ?>" tw:counturl="<?php echo ((empty($_SERVER["HTTPS"])) ? 'http://' : 'https://') . $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"]; ?>"></a>
     <!-- <a class="addthis_button_linkedin_counter"></a> --> 
     <a class="addthis_button_linkedin"></a>
