221e829f7467750cd19d8c1ccf07942ff1287baa
Dustin Boersma <dustin.boersma@gmail.com>
2013-05-08 13:51:24 -0700

    updated intel learn more wordlets

--- drupal/profiles/takepart/themes/takepart3/campaigns/intelchange_winners/wordlet-intelchange-winners-learn.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/intelchange_winners/wordlet-intelchange-winners-learn.tpl.php
@@ -1,6 +1,6 @@
 <div class="first-block">
-	<div class="header-image" <?=wa('header_image')?>>
-        <? if ( ($w = w('header_image')) && $w->img_src ): ?>
+	<div class="header-image" <?=wa('first_block_header_image')?>>
+        <? if ( ($w = w('first_block_header_image')) && $w->img_src ): ?>
             <p>
                 <img src="<?=$w->img_src?>" alt="<?=$w->single(false)?>" />
             </p>
@@ -8,12 +8,12 @@
             Add Image
         <? endif ?>
     </div>
-    <h2 class='header-title'><?=wl('header_title')?></h2>
+    <h2 class='header-title'><?=wl('first_block_header_title')?></h2>
     <div class="header-text">
-    	<?=wl('header_text')?>
+    	<?=wl('first_block_header_text')?>
     </div>
 </div>
 <div class="second-block">
-	<h3 class="header-title"><?=wl('secondary_header_title')?></h3>
+	<h3 class="header-title"><?=wl('second_block_header_title')?></h3>
 	<div class="resources"><?=wl('resources_view')?></div>
 </div>
