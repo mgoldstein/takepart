3fcc52f7e3d24b84f84067263c0855a731d13eb8
Dustin Boersma <dustin.boersma@gmail.com>
2013-05-08 14:01:42 -0700

    updated intel learn more wordlets

--- drupal/profiles/takepart/themes/takepart3/campaigns/intelchange_winners/wordlet-intelchange-winners-learn.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/intelchange_winners/wordlet-intelchange-winners-learn.tpl.php
@@ -1,19 +1,17 @@
 <div class="first-block">
 	<div class="image-wrapper" <?=wa('first_block_image')?>>
         <? if ( ($w = w('first_block_header_image')) && $w->img_src ): ?>
-            <p>
-                <img src="<?=$w->img_src?>" alt="<?=$w->single(false)?>" />
-            </p>
+            <img src="<?=$w->img_src?>" alt="<?=$w->single(false)?>" />
         <? elseif( wordlet_edit_mode() ): ?>
             Add Image
         <? endif ?>
     </div>
     <h2 class='headline'><?=wl('first_block_headline')?></h2>
-    <div class="text cms">
+    <div class="text-block cms">
     	<?=wl('first_block_text')?>
     </div>
 </div>
 <div class="second-block">
-	<h3 class="header-title"><?=wl('second_block_header_title')?></h3>
+	<h3 class="header-title"><?=wl('second_block_headline')?></h3>
 	<div class="resources"><?=wl('resources_view')?></div>
 </div>
