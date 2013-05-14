3042cf9da9fa4fac8bb19ec735c0428aa6f6e684
Dustin Boersma <dustin.boersma@gmail.com>
2013-05-13 18:00:45 -0700

    added gallery markup to teams and styled subheaders

--- drupal/profiles/takepart/themes/takepart3/campaigns/intelchange_winners/wordlet-intelchange-winners-teams.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/intelchange_winners/wordlet-intelchange-winners-teams.tpl.php
@@ -46,17 +46,37 @@ if (isset($_GET['member'])) {
     $team_member = wf($team->token.'_team', 'token', $cur_member);
     ?>
     <div class="member" <?=wa($team->token.'_team')?>>
-        <div class="portrait">
-            <img src="<?=$team_member->img_src?>" alt="<?=$team_member->single(false)?>">
+        <div class="first-sub">
+            <div class="portrait">
+                <img src="<?=$team_member->img_src?>" alt="<?=$team_member->single(false)?>">
+            </div>
+            <h2 class='headline'>
+                <span class='member-title'><?=w($team_member->token.'_member_title_label')?> </span>
+                <span class="name"><?=$team_member->single(false)?></span>
+            </h2>
+            <div class='facts cms'><?=$team_member->multi_short(false)?></div>
+            <div class='blurb cms'><?=$team_member->multi(false)?></div>
+            <div class="video">
+                <iframe class="video-player" width="675" height="393" src="http://www.youtube.com/embed/<?=$team_member->video?>" frameborder="0" allowfullscreen></iframe>
+            </div>
         </div>
-        <h2 class='headline'>
-            <span class='member-title'><?=w($team_member->token.'_member_title_label')?> </span>
-            <span class="name"><?=$team_member->single(false)?></span>
-        </h2>
-        <div class='facts cms'><?=$team_member->multi_short(false)?></div>
-        <div class='blurb cms'><?=$team_member->multi(false)?></div>
-        <div class="video">
-            <iframe class="video-player" width="675" height="393" src="http://www.youtube.com/embed/<?=$team_member->video?>" frameborder="0" allowfullscreen></iframe>
+        <div class="second-sub">
+            <h3 class="subheadline"></h3>
+            <div class="gallery">
+                <div class="image-wrapper" <?=wa($team->token.'_'.$team_member->token.'_gallery_image')?>>
+                    <? if ( ($w = w($team->token.'_'.$team_member->token.'_gallery_image')) && $w->img_src ): ?>
+                        <img src="<?=$w->img_src?>" alt="<?=$w->single(false)?>" />
+                    <? elseif( wordlet_edit_mode() ): ?>
+                        Add Image
+                    <? endif ?>
+                </div>
+                <div class="gallery-link-wrapper">
+                    <a href="<?=w($team->token.'_'.$team_member->token.'_gallery_link')->href?>" class="gallery-link"><?=w($team->token.'_'.$team_member->token.'_gallery_link')->single(false)?></a>
+                </div>
+            </div>
+        </div>
+        <div class="third-sub">
+            <h3 class="subheadline"></h3>
         </div>
     </div>
 </div>
