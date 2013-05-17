90d7abb0cc82672f91f0dcf7d1d1742a209092f0
gburns <gburns@takepart.com>
2013-05-17 15:54:48 -0700

    Fixing up contagion to work with wordlets

--- drupal/profiles/takepart/themes/takepart3/campaigns/contagion/wordlet-wrapper-contagion.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/contagion/wordlet-wrapper-contagion.tpl.php
@@ -1,17 +1,7 @@
-<div id="page-wrapper" class="campaign">
+<div id="header-wrapper-wrapper">
 	<?=$header ?>
+</div>
 
-	<!-- start -->
-
-	<div class="page-wrap">
-		<div class="main">
-			<main id="page">
-				<?=$content?>
-			</main>
-		</div>
-	</div><!-- /.page-wrap -->
-
-	<!-- end -->
-
-	<?=$footer ?>
+<div id="wrapper">
+	<?=$content?>
 </div>
