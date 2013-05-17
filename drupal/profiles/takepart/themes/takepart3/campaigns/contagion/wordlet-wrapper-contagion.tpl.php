62e8fb590aebd62adc43bdc1284389c4f6aa43d5
gburns <gburns@takepart.com>
2013-05-17 14:16:51 -0700

    adding contagion site as a wordlet page

---
+++ drupal/profiles/takepart/themes/takepart3/campaigns/contagion/wordlet-wrapper-contagion.tpl.php
@@ -0,0 +1,17 @@
+<div id="page-wrapper" class="campaign">
+	<?=$header ?>
+
+	<!-- start -->
+
+	<div class="page-wrap">
+		<div class="main">
+			<main id="page">
+				<?=$content?>
+			</main>
+		</div>
+	</div><!-- /.page-wrap -->
+
+	<!-- end -->
+
+	<?=$footer ?>
+</div>
