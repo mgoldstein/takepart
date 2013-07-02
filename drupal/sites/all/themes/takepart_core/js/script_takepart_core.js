831a85112c0e1a98985d78f931db4389b92dd57b
gburns <gburns@takepart.com>
2013-07-02 15:08:29 -0700

    Making link open in new window

--- drupal/sites/all/themes/takepart_core/js/script_takepart_core.js
+++ drupal/sites/all/themes/takepart_core/js/script_takepart_core.js
@@ -2,20 +2,25 @@
 
 window.tp_ad_takeover = function(bgcolor, bgimage, link) {
 	var $body = $('body');
-	var $a = $('<a id="tp_ad_takeover" href="' + link + '"></a>');
-	$body.css({
-		background: bgcolor + ' url("' + bgimage + '") center top no-repeat',
-		backgroundAttachment: 'fixed'
-	});
-	$a.css({
-		position: 'fixed',
-		height: '100%',
-		width: '100%',
-		left: 0,
-		top: 0,
-		zIndex: 1
-	})
-	$body.append($a);
+	var $a = $('<a id="tp_ad_takeover" href="' + link + '" target="_blank"></a>');
+
+	$body
+		.css({
+			background: bgcolor + ' url("' + bgimage + '") center top no-repeat',
+			backgroundAttachment: 'fixed'
+		})
+		.addClass('tp_ad_takeover');
+
+	$a
+		.css({
+			position: 'fixed',
+			height: '100%',
+			width: '100%',
+			left: 0,
+			top: 0,
+			zIndex: 1
+		})
+		$body.append($a);
 };
 
 // Document Ready
