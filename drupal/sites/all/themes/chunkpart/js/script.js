f78ab3f3d47dee647aa6bc41ec0a63326bfde199
gburns <gburns@takepart.com>
2013-07-10 14:26:02 -0700

    More fucking analytics

--- drupal/sites/all/themes/chunkpart/js/script.js
+++ drupal/sites/all/themes/chunkpart/js/script.js
@@ -211,7 +211,8 @@ $(function() {
 			.delegate('#next-gallery a, .forward-to-gallery a', 'click', function() {
 				takepart.analytics.track('gallery-next-gallery-click', {
 					headline: next_gallery_headline,
-					topic: next_gallery_topic
+					topic: next_gallery_topic,
+					current_gallery_url: base_url
 				});
 			});
 
@@ -442,13 +443,11 @@ $(function() {
 		} else if ( get_curtoken() == 'first-slide' ) {
 			skip_next_pageview = true;
 			show_gallery(true);
-			//hpush($current_slide.data('token'), $current_slide.find('.headline').text());
 		} else if ( $gallery_cover.length ) {
 			hide_gallery();
 		} else {
 			skip_next_pageview = true;
 			show_gallery(true);
-			//hpush($current_slide.data('token'), $current_slide.find('.headline').text(), true);
 		}
 
 		var first_pop = true;
