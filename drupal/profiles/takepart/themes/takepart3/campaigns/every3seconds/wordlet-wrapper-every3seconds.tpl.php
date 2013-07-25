69409f6a2d1dbce7420310a324a99e4e05a4e07e
gburns <gburns@takepart.com>
2013-07-25 15:52:57 -0700

    Adding every 3 seconds campaign

---
+++ drupal/profiles/takepart/themes/takepart3/campaigns/every3seconds/wordlet-wrapper-every3seconds.tpl.php
@@ -0,0 +1,32 @@
+<div id="page-wrapper" class="campaign">
+	<?=$header ?>
+
+	<!-- start -->
+
+	<div class="page-wrap">
+		<div class="main">
+			<div class="header">
+				<? if ( $w = w('header_image') ): ?>
+					<h1 <?=wa('header_image')?>>
+						<span><?=$w->single(false)?></span>
+						<a href="<?=$w->href?>"><img src="<?=$w->img_src?>" alt="logo" /></a>
+					</h1>
+				<? endif ?>
+				<div class="menu">
+					<ul <?=wa('menu')?>>
+						<? foreach( wl('menu') as $w ): ?>
+							<li><a href="<?=$w->href?>" class="<?=ws($w->href)?>"><?=$w->single(false)?></a></li>
+						<? endforeach ?>
+					</ul>
+				</div>
+			</div>
+			<main id="page">
+				<?=$content?>
+			</main>
+		</div>
+	</div><!-- /.page-wrap -->
+
+	<!-- end -->
+
+	<?=$footer ?>
+</div>
