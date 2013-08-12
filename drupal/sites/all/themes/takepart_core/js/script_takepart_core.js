9ea68c0e2fd6dedc9e7f151e63d0e222b429a563
Dustin Boersma <dustin.boersma@gmail.com>
2013-08-12 15:56:39 -0700

    hooked up interstitial module to front end and added cookie support

--- drupal/sites/all/themes/takepart_core/js/script_takepart_core.js
+++ drupal/sites/all/themes/takepart_core/js/script_takepart_core.js
@@ -4,23 +4,58 @@
 $(function() {
 	var $body = $('body');
 
-	var $interstitial = $('#interstitial');
-	if ( $interstitial.length ) {
+	var interstitial_url = $('body').attr('data-interstitial-provider');
+
+	$.ajax({
+		dataType: "json",
+		url: interstitial_url,
+		data: {},
+		success: function(data){
+			if(!data.show){
+				//return;
+			}
+			data.show = 'interstitials/newsletter';
+			$interstitial_link = $('<a></a>').attr('href', '/' + data.show);
+			$interstitial = $('<div></div>').attr('id', 'interstitial').append($interstitial_link);
+			interstitial_init($interstitial);
+		}
+	});
+
+	function interstitial_init($interstitial){
+		var interstitial_modal_id = 'interstitial_modal_';
 		var $a = $interstitial.find('a');
 		var $iframe = $('<iframe src="' + $a.attr('href') + '"></iframe>').css({border: '0'});
-
+		var extend_pm_interstitial_cookie = function(days){
+			var date = new Date();
+			date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+			var myCookieValue = $.cookie('pm_igloo');
+			$.cookie('pm_igloo', null);
+			$.cookie('pm_igloo', myCookieValue, { expires:date, secure:true, path:'/' });
+		};
+		
 		window.resize_interstitial = function(w, h) {
 			$iframe.css({width: w, height: h});
-			$.tpmodal.showModal({id: 'interstitial_modal_'});
+			$.tpmodal.showModal({id: interstitial_modal_id});
 		};
 
 		window.dont_show_interstitial = function() {
-			$.tpmodal.hide({id: 'interstitial_modal_'});
+			$.tpmodal.set({
+				id: interstitial_modal_id,
+				values: {
+					afterClose: function() {
+						extend_pm_interstitial_cookie(365 * 5);
+					}
+				}
+			});
+			$.tpmodal.hide({id: interstitial_modal_id});
 		};
 
 		$.tpmodal.load({
-			id: 'interstitial_modal_',
-			node: $iframe
+			id: interstitial_modal_id,
+			node: $iframe,
+			afterClose: function() {
+				extend_pm_interstitial_cookie(7);
+			}
 		});
 	}
 
