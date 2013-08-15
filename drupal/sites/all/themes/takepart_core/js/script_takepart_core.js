115291ed0ab20f1ab64fd310c0f73cf0ed674c53
Dustin Boersma <dustin.boersma@gmail.com>
2013-08-15 16:53:53 -0700

    overflow hidden moved to css

--- drupal/sites/all/themes/takepart_core/js/script_takepart_core.js
+++ drupal/sites/all/themes/takepart_core/js/script_takepart_core.js
@@ -70,7 +70,6 @@ $(function() {
 
 		window.resize_interstitial = function(w, h) {
 			//$iframe.css({width: w, height: h});
-			$iframe.css({overflow: 'hidden'});
 			$.tpmodal.showModal({id: interstitial_modal_id});
 		};
 
