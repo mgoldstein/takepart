79177f5be708d912bbcaa1e866de9b94ae22c62d
Dustin Boersma <dustin.boersma@gmail.com>
2013-08-19 18:07:56 -0700

    styling fix for newsletter form label

--- drupal/profiles/takepart/themes/takepart3/campaigns/interstitials/newsletter/newsletter.js
+++ drupal/profiles/takepart/themes/takepart3/campaigns/interstitials/newsletter/newsletter.js
@@ -1,6 +1,15 @@
 (function (window, $, undefined) {
 
 $(function() {
+	console.log('asdf');
+	$('.form-item-email input').bind('blur', function(){
+		var newCss = {'z-index': ''};
+		if($(this).val()){
+			newCss['z-index'] = 3;
+		}
+		console.log(newCss);
+		$(this).css(newCss);
+	});
 
 	$('#dont a').bind('click', function(e) {
 		e.preventDefault();
