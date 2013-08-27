c53e66d30aa6c4fa40c85d26f811ec288855a388
Samrod Shenassa <sshenassa@takepart.com>
2013-08-27 11:58:22 -0700

    WIP - pending getting the right fonts and the CBS & Participant logos, mostly done styling the TEACH header.

---
+++ drupal/profiles/takepart/themes/takepart3/campaigns/teach/wordlet-wrapper-teach.tpl.php
@@ -0,0 +1,39 @@
+<?
+	$w = w('header_image')
+?>
+<div id="page-wrapper" class="campaign">
+	<?=$header ?>
+
+	<!-- start -->
+
+	<div class="page-wrap">
+		<div class="main">
+			<div class="header">
+				<div class="slug"><?=w('header_slug')?></div>
+				<h1><?=w('header_title')?></h1>
+				<div class="credit"><?=w('header_credit')?></div>
+				<div class="burst"><?=w('header_burst')?></div>
+				<div class="date"><?=w('header_date')?></div>
+				<img class="hero" src="/profiles/takepart/themes/takepart3/campaigns/teach/img/teachers-hero.png" alt="" />
+				<div class="fine">
+					<?=w('header_fine')?>
+					<div class="right"><?=w('header_photo_credit')?></div>
+				</div>
+				<div class="menu">
+					<ul <?=wa('menu') ?>>
+						<? foreach( wl('menu') as $w ): ?>
+							<li><a href="<?=$w->href?>" class="<?=ws($w->href)?>"><?=$w->single(false)?></a></li>
+						<? endforeach ?>
+					</ul>
+				</div>
+			</div>
+			<main id="page">
+				<?=$content?>
+			</main>
+		</div>
+	</div><!-- /.page-wrap -->
+
+	<!-- end -->
+
+	<?=$footer ?>
+</div>
