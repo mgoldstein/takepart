0909b0be5bda3ac38a57496c99d551f36f127089
Dustin Boersma <dustin.boersma@gmail.com>
2013-08-27 18:04:14 -0700

    added file path to include on home page

--- drupal/profiles/takepart/themes/takepart3/campaigns/teach/wordlet-teach-home.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/teach/wordlet-teach-home.tpl.php
@@ -1,5 +1,5 @@
 <div class="content">
 	<h2 class="content-header"><span><?=w('page_title')?></span></h2>
 	<div class="video"></div>
-    <? include_once('templates/teach-social-block.tpl.php') ?>
+    <? include_once(dirname(__FILE__).'/templates/teach-social-block.tpl.php') ?>
 </div>
