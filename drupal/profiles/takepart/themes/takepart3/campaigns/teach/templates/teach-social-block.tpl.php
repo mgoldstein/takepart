c32d415100527552210c8a5f2a4453eb312e4459
Samrod Shenassa <sshenassa@takepart.com>
2013-08-27 19:48:34 -0700

    WIP fixed bad filename in social include.

---
+++ drupal/profiles/takepart/themes/takepart3/campaigns/teach/templates/teach-social-block.tpl.php
@@ -0,0 +1,27 @@
+<section class="social-menu">
+  <h1 class="sect-headline"><?=w('social_headline')?></h1>
+  <section class="text">
+    <h2 class="sect-headline"><?=w('social_text_headline')?></h2>
+    <div class="cms">
+        <?=w('social_text_body')?>
+    </div>
+  </section>
+  <section class="connected">
+    <h2 class="sect-headline"><?=w('social_connected_headline')?></h2>
+    <div class="cms">
+        <?=w('social_connected_form')?>
+    </div>
+  </section>
+  <section class="discussion">
+    <h2 class="sect-headline"><?=w('social_discussion_headline')?></h2>
+    <div class="cms">
+        <?=w('social_discussion_body')?>
+    </div>
+  </section>
+  <section class="follow">
+    <h2 class="sect-headline"><?=w('social_discussion_headline')?></h2>
+    <div class="cms">
+        <?=w('social_follow_body')?>
+    </div>
+  </section>
+</section>
