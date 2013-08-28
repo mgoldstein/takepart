927c2bd992acd43bf79ba773304b6df58fc5ef54
Dustin Boersma <dustin.boersma@gmail.com>
2013-08-27 18:01:38 -0700

    added social block to home page

--- drupal/profiles/takepart/themes/takepart3/campaigns/teach/wordlet-teach-home.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/teach/wordlet-teach-home.tpl.php
@@ -1,4 +1,5 @@
 <div class="content">
 	<h2 class="content-header"><span><?=w('page_title')?></span></h2>
 	<div class="video"></div>
+    <? include_once('templates/teach-social-block.tpl.php') ?>
 </div>
