6f2d3d924fe2ea84b78749245f3c8cec9ac140c8
Dustin Boersma <dustin.boersma@gmail.com>
2013-08-27 20:55:17 -0700

    made header more semantic

--- drupal/profiles/takepart/themes/takepart3/campaigns/teach/wordlet-wrapper-teach.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/teach/wordlet-wrapper-teach.tpl.php
@@ -8,17 +8,17 @@
 
 	<div class="page-wrap">
 		<div class="main">
-			<div class="header">
-				<div class="slug"><?=w('header_slug')?></div>
+			<header class="header">
+				<span class="slug"><?=w('header_slug')?></span>
 				<h1 class="page-header"><?=w('header_title')?></h1>
-				<div class="credit"><?=w('header_credit')?></div>
-				<div class="burst"><?=w('header_burst')?></div>
-				<div class="date"><?=w('header_date')?></div>
+				<span class="credit"><?=w('header_credit')?></span>
+				<span class="burst"><?=w('header_burst')?></span>
+				<span class="date"><?=w('header_date')?></span>
 				<img class="hero" src="/profiles/takepart/themes/takepart3/campaigns/teach/img/teachers-hero.png" alt="" />
-				<div class="fine">
+				<p class="fine">
 					<?=w('header_fine')?>
 					<div class="right"><?=w('header_photo_credit')?></div>
-				</div>
+				</p>
 				<div class="menu">
 					<ul <?=wa('menu') ?>>
 						<? foreach( wl('menu') as $w ): ?>
@@ -26,7 +26,7 @@
 						<? endforeach ?>
 					</ul>
 				</div>
-			</div>
+			</header>
 			<main id="page">
 				<?=$content?>
 			</main>
