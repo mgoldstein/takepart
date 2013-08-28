d3d5aa616bc4050bb10ad87d79c92fe84db03a6b
Dustin Boersma <dustin.boersma@gmail.com>
2013-08-27 18:16:25 -0700

    testing shit

--- drupal/profiles/takepart/themes/takepart3/campaigns/teach/wordlet-teach-home.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/teach/wordlet-teach-home.tpl.php
@@ -1,9 +1,6 @@
 <div class="content">
-	<h2 class="content-header"><span><?=w('page_title')?></span></h2>
+	<? include('templates/teach-social-block.tpl.php') ?>
+    <h2 class="content-header"><span><?=w('page_title')?></span></h2>
 	<div class="video"></div>
-
-    DIE IN A FUCKING FIRE -jp
-
-
-    <? include('templates/teach-social-block.tpl.php') ?>
+    
 </div>
