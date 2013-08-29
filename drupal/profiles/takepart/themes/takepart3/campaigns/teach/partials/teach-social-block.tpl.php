d1dda019568644dd56c63c7e81808405b0ab958e
Samrod Shenassa <sshenassa@takepart.com>
2013-08-28 17:05:58 -0700

    TP 759 - added social bullet icons.

--- drupal/profiles/takepart/themes/takepart3/campaigns/teach/partials/teach-social-block.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/teach/partials/teach-social-block.tpl.php
@@ -1,11 +1,11 @@
   <h1 class="sect-headline"><?=w('social_headline')?></h1>
-  <section class="text">
+  <section class="item text">
     <h2 class="sect-headline"><?=w('social_text_headline')?></h2>
     <div class="cms">
         <?=w('social_text_body')?>
     </div>
   </section>
-  <section class="connected">
+  <section class="item connected">
     <h2 class="sect-headline"><?=w('social_connected_headline')?></h2>
     <div class="cms">
         <?=w('social_connected_body')?>
@@ -14,7 +14,7 @@
         <?=w('social_connected_form')?>
     </div>
   </section>
-  <section class="discussion">
+  <section class="item discussion">
     <h2 class="sect-headline"><?=w('social_discussion_headline')?></h2>
     <div class="cms">
         <?=w('social_discussion_body')?>
