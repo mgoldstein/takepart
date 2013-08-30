b727237c3d00629f00ade2bbbba15f0b97f7dfe1
Matthew Wrather <matt@wrathercreative.com>
2013-08-29 21:01:27 -0700

    add href to main alliance logo

--- drupal/profiles/takepart/themes/takepart3/campaigns/teach/wordlet-teach-alliances.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/teach/wordlet-teach-alliances.tpl.php
@@ -3,7 +3,10 @@
         <h1 class="content-headline"><?=w('page_headline')?></h1>
         <h2 class="content-subheadline"><?=w('page_subheadline')?></h2>
         <div class="main-image-wrapper" <?=wa('main_image')?>>
-	    <img src="<?=w('main_image')->img_src?>" alt="<?=w('main_image')->single_no_markup?>">
+        <? $w = w('main_image'); ?>
+        <? $markup = '<img src="' . $w->img_src . '" alt="' . $w->single_no_markup . '">'; ?>
+        <? if ($w->href) $markup = '<a href="' . $w->href . '">' . $markup . '</a>'; ?>
+        <?=$markup;?>
         </div>
 	<div class="content-main cms">
             <?=w('content_main')?>
@@ -14,8 +17,8 @@
 		<? $zebra = 'alliance-' .($row_count % 2 == 0) ? 'even' : 'odd'; ?>
 		<? $zebra .= ($row_count % 4 == 0) ? ' alliance-nth-child-4n' : ''; ?>
 		<div class="alliance alliance-<?=$row_count?> <?=$zebra?>">
-		    <? $markup = '<img src="' . $w->img_src . '" alt="' . $w->single_no_markup . '">' ;?>
-		    <? if ($w->href) $markup = '<a href="' . $w->href . '">' . $markup . '</a>';?>
+		    <? $markup = '<img src="' . $w->img_src . '" alt="' . $w->single_no_markup . '">'; ?>
+		    <? if ($w->href) $markup = '<a href="' . $w->href . '">' . $markup . '</a>'; ?>
 		    <?=$markup?>
                 </div>
             <? endforeach ?>
