cc52e8aab62ff998406aa83b0b425640583935e2
Samrod Shenassa <sshenassa@takepart.com>
2013-08-29 17:16:39 -0700

    TP 759 - continued working on responsive landing page, added white FB share icon.

--- drupal/profiles/takepart/themes/takepart3/campaigns/teach/wordlet-teach-home.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/teach/wordlet-teach-home.tpl.php
@@ -1,10 +1,10 @@
 <div class="content">
 	<h2 class="content-header"><span><?=w('page_title')?></span></h2>
+	<h4 class="call-to-share"><?=w('social_follow_body')?></h4>
   <div class="main-video-wrapper" <?=wa('video')?>>
       <script src="<?=w('video')?>"></script>
   </div>
 	<section class="social-menu">
-		<h4 class="link_on_white"><?=w('social_follow_body')?></h4>
 		<? include('partials/teach-social-block.tpl.php') ?>
 	</section>
 	<section class="page-body-content cms">
