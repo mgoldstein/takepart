484189d760d9af918d32ad8bcdb8f623ecc44691
Samrod Shenassa <sshenassa@takepart.com>
2013-09-09 18:24:23 -0700

    TP-834 - converted to wordlets, exported wordlets to QA, and added page curl background.

---
+++ drupal/profiles/takepart/themes/takepart3/campaigns/teach/partials/teach-watch-promo.tpl.php
@@ -0,0 +1,15 @@
+<section class="teach-watch-promo">
+  <div class="teach-watch-promo-content">
+    <h2 class="sect-headline"><?=w('teach-promo-header')?></h2>
+    <ul>
+      <? foreach( wl('teach-promo-links') as $w ): ?>
+      <li>
+        <a href="<?=$w->href?>">
+          <img class="media-logo" src="<?=$w->img_src?>" alt="<?=$w->token?>" />
+          <span><?=$w->single(false)?></span>
+        </a>
+      </li>
+      <? endforeach ?>
+    </ul>
+  </div>
+</section>
