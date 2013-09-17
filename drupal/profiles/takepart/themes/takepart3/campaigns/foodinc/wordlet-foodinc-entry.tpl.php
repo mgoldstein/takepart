a6714cf9a4ee05cdae9c4ef20129834f7d2a1675
Scott Knight <sknight17@gmail.com>
2013-09-17 14:55:57 -0700

    added entry title images and wordlet

--- drupal/profiles/takepart/themes/takepart3/campaigns/foodinc/wordlet-foodinc-entry.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/foodinc/wordlet-foodinc-entry.tpl.php
@@ -1,3 +1,12 @@
 <div class="content">
-
+<?php foreach (wl('howto_section') as $ws): ?>
+  <h1><?= w($ws->token . '_heading') ?><span><?= w($ws->token . '_tag') ?></span></h1>
+  <?php if ($wis = wl($ws->token . '_items')): ?>
+  <ul <?= wa($ws->token . '_items') ?>>
+    <?php foreach ($wis as $wi): ?>
+      <li><img src="<?= $wi->img_src ?>" /><?= $wi->multi ?></li>
+    <?php endforeach ?>
+  </ul>
+  <?php endif ?>
+<?php endforeach ?>
 </div>
