6f49eeb3fe874546ccd58d3d72a2046c335a5fd2
Scott Knight <sknight17@gmail.com>
2013-09-17 00:06:36 -0700

    themed howto page

--- drupal/profiles/takepart/themes/takepart3/campaigns/foodinc/wordlet-foodinc-howto.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/foodinc/wordlet-foodinc-howto.tpl.php
@@ -3,52 +3,77 @@
   <section class="page-body-content cms">
 
     <!-- Intro  -->
-    <h4>HOW TO ENTER</H4>
-    <?php print w('foodinc_howto_intro')->single; ?>
-    <?php print w('foodinc_howto_intro')->multi; ?>
-    <?php if(w('foodinc_howto_intro')->video != NULL): ?>
-      <div class="video">
-        <?php if(substr(w('foodinc_howto_intro')->video, 0, 7 ) === "http://"): ?>
-          <?php $youtube_id = substr(w('foodinc_howto_intro')->video, strrpos(w('foodinc_howto_intro')->video, '=') + 1);; ?>
-          <iframe width="520" height="315" src="//www.youtube.com/embed/<?php print $youtube_id; ?>" frameborder="0" allowfullscreen></iframe>
-        <?php else: ?>
-          <script type="text/javascript" src="http://video.takepart.com/players/<?php print w('foodinc_howto_intro')->video; ?>.js"></script>
-        <?php endif; ?>
-      </div>
-    <?php else: ?>
-      <img src="<?php print w('foodinc_howto_intro')->img_src; ?>" alt="food inc awards">
-    <?php endif; ?>
+    <div class="page-title"><h3>HOW TO ENTER</h3></div>
+    <div class="intro">
+      <div class="title"><?php print w('foodinc_howto_intro')->single; ?></div>
+      <div class="description"><?php print w('foodinc_howto_intro')->multi; ?></div>
+      <?php if(w('foodinc_howto_intro')->video != NULL): ?>
+        <div class="video">
+          <?php if(substr(w('foodinc_howto_intro')->video, 0, 7 ) === "http://"): ?>
+            <?php $youtube_id = substr(w('foodinc_howto_intro')->video, strrpos(w('foodinc_howto_intro')->video, '=') + 1);; ?>
+            <iframe width="635" height="360" src="//www.youtube.com/embed/<?php print $youtube_id; ?>" frameborder="0" allowfullscreen></iframe>
+          <?php else: ?>
+            <script type="text/javascript" src="http://video.takepart.com/players/<?php print w('foodinc_howto_intro')->video; ?>.js"></script>
+          <?php endif; ?>
+        </div>
+      <?php else: ?>
+        <img src="<?php print w('foodinc_howto_intro')->img_src; ?>" alt="food inc awards">
+      <?php endif; ?>
+    </div>
 
     <!-- CATEGORIES  -->
-    <?php foreach( wl('foodinc_howto_categories') as $key => $w ): ?>
-      <div class="row row-id-<?php print $key; ?> <?php print ($key % 2 == 0 ? 'row-odd' : 'row-even'); ?>">
-        <div class="title"><?php print $w->single; ?></div>
-        <div class="description"><?php print $w->multi; ?></div>
-      </div>
-    <? endforeach ?>
+    <div class="categories">
+      <?php foreach( wl('foodinc_howto_categories') as $key => $w ): ?>
+        <div class="row row-id-<?php print $key; ?> <?php print ($key % 2 == 0 ? 'row-odd' : 'row-even'); ?>">
+          <div class="title"><?php print $w->single; ?></div>
+          <div class="description"><?php print $w->multi; ?></div>
+        </div>
+      <? endforeach ?>
+    </div>
 
     <!-- ADDITIONAL INFO  -->
-    <?php foreach( wl('foodinc_howto_additional_info') as $key => $w ): ?>
-      <div class="row row-id-<?php print $key; ?> <?php print ($key % 2 == 0 ? 'row-odd' : 'row-even'); ?>">
-        <div class="title"><?php print $w->single; ?></div>
-        <div class="description"><?php print $w->multi; ?></div>
-        <?php if(w('foodinc_howto_intro')->video != NULL): ?>
-          <div class="video">
-            <?php if(substr(w('foodinc_howto_additional_info')->video, 0, 7 ) === "http://"): ?>
-              <?php $youtube_id = substr(w('foodinc_howto_additional_info')->video, strrpos(w('foodinc_howto_additional_info')->video, '=') + 1);; ?>
-              <iframe width="520" height="315" src="//www.youtube.com/embed/<?php print $youtube_id; ?>" frameborder="0" allowfullscreen></iframe>
-            <?php else: ?>
-              <script type="text/javascript" src="http://video.takepart.com/players/<?php print w('foodinc_howto_additional_info')->video; ?>.js"></script>
-            <?php endif; ?>
-          </div>
-        <?php else: ?>
-          <img src="<?php print w('foodinc_howto_additional_info')->img_src; ?>" alt="<?php print w('foodinc_howto_additional_info')->single; ?>">
-        <?php endif; ?>
-      </div>
-    <? endforeach ?>
+    <div class="additional-info">
+      <?php foreach( wl('foodinc_howto_additional_info') as $key => $w ): ?>
+        <div class="row row-id-<?php print $key; ?> <?php print ($key % 2 == 0 ? 'row-odd' : 'row-even'); ?>">
+          <div class="title"><?php print $w->single; ?></div>
+          <div class="description"><?php print $w->multi; ?></div>
+          <?php if(w('foodinc_howto_intro')->video != NULL): ?>
+            <div class="video">
+              <?php if(substr($w->video, 0, 7 ) === "http://"): ?>
+                <?php $youtube_id = substr($w->video, strrpos($w->video, '=') + 1);; ?>
+                <iframe width="635" height="360" src="//www.youtube.com/embed/<?php print $youtube_id; ?>" frameborder="0" allowfullscreen></iframe>
+              <?php else: ?>
+                <script type="text/javascript" src="http://video.takepart.com/players/<?php print w('foodinc_howto_additional_info')->video; ?>.js"></script>
+              <?php endif; ?>
+            </div>
+          <?php else: ?>
+            <img src="<?php print w('foodinc_howto_additional_info')->img_src; ?>" alt="<?php print w('foodinc_howto_additional_info')->single; ?>">
+          <?php endif; ?>
+        </div>
+      <? endforeach ?>
+    </div>
   </section>
     <section class="right-rail">
-      <?php print w('foodinc_howto_instruction_block'); ?>
+      <div class="instruction-block">
+        <div class="top"></div>
+        <div class="mid">
+          <?php foreach( wl('foodinc_howto_instruction_steps') as $key => $w ): ?>
+            <?php $number = $key + 1; ?>
+            <div class="item"><div class="number"><?php print $number; ?></div><span><?php print $w->single; ?></span></div>
+          <?php endforeach; ?>
+          <div class="button"><?php print l(w('foodinc_howto_instruction_button')->single, w('foodinc_howto_instruction_button')->href); ?></div>
+          <div class="extra"><?php print w('foodinc_howto_instruction_block')->multi; ?></div>
+        </div>
+        <div class="bottom"></div>
+      </div>
     <div class="ad"><?php print (w('foodinc_howto_adblock') != NULL ? render(w('foodinc_howto_adblock')) : ''); ?></div>
   </section>
 </div>
+
+
+
+
+
+
+
+
