bcd6322ad5be5dc14f77544199c2e73d8e8b1e37
JP McDowell <jp@takepart.com>
2013-09-17 19:14:54 -0700

    Add template for Food, Inc. Awards entry form

---
+++ drupal/profiles/takepart/themes/takepart3/campaigns/foodinc/foodinc-awards-entry-form.tpl.php
@@ -0,0 +1,93 @@
+<?php $form = $variables['element']; ?>
+
+<!-- BEGIN -->
+<?php print render($form['about_you_heading']); ?>
+<!-- END -->
+
+<!-- BEGIN -->
+<?php print render($form['about_you_description']); ?>
+<!-- END -->
+
+<!-- BEGIN -->
+<?php print render($form['first_name']); ?>
+<!-- END -->
+
+<!-- BEGIN -->
+<?php print render($form['last_name']); ?>
+<!-- END -->
+
+<!-- BEGIN -->
+<?php print render($form['email']); ?>
+<!-- END -->
+
+<!-- BEGIN -->
+<?php print render($form['dob_month']); ?>
+<!-- END -->
+
+<!-- BEGIN -->
+<?php print render($form['dob_year']); ?>
+<!-- END -->
+
+<!-- BEGIN -->
+<?php print render($form['city']); ?>
+<!-- END -->
+
+<!-- BEGIN -->
+<?php print render($form['state']); ?>
+<!-- END -->
+
+<!-- BEGIN -->
+<?php print render($form['which_award_label']); ?>
+<!-- END -->
+
+<!-- BEGIN -->
+<?php print render($form['lifestyle_award']); ?>
+<!-- END -->
+
+<!-- BEGIN -->
+<?php print render($form['lifestyle_award_description']); ?>
+<!-- END -->
+
+<!-- BEGIN -->
+<?php print render($form['pioneer_award']); ?>
+<!-- END -->
+
+<!-- BEGIN -->
+<?php print render($form['pioneer_award_description']); ?>
+<!-- END -->
+
+<!-- BEGIN -->
+<?php print render($form['your_video_heading']); ?>
+<!-- END -->
+
+<!-- BEGIN -->
+<?php print render($form['your_video_description']); ?>
+<!-- END -->
+
+<!-- BEGIN -->
+<?php print render($form['video_link']); ?>
+<!-- END -->
+
+<!-- BEGIN -->
+<?php print render($form['submission_heading']); ?>
+<!-- END -->
+
+<!-- BEGIN -->
+<?php print render($form['submission_description']); ?>
+<!-- END -->
+
+<!-- BEGIN -->
+<?php print render($form['newsletter_opt_in']); ?>
+<!-- END -->
+
+<!-- BEGIN -->
+<?php print render($form['contest_rules_opt_in']); ?>
+<!-- END -->
+
+<!-- BEGIN -->
+<?php print render($form['terms_of_service']); ?>
+<!-- END -->
+
+<!-- BEGIN -->
+<?php print render($form['submit']); ?>
+<!-- END -->
--- drupal/profiles/takepart/themes/takepart3/campaigns/foodinc/wordlet-foodinc-entry.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/foodinc/wordlet-foodinc-entry.tpl.php
@@ -1,12 +1,1 @@
-<div class="content">
-<?php foreach (wl('howto_section') as $ws): ?>
-  <h1><?= w($ws->token . '_heading') ?><span><?= w($ws->token . '_tag') ?></span></h1>
-  <?php if ($wis = wl($ws->token . '_items')): ?>
-  <ul <?= wa($ws->token . '_items') ?>>
-    <?php foreach ($wis as $wi): ?>
-      <li><img src="<?= $wi->img_src ?>" /><?= $wi->multi ?></li>
-    <?php endforeach ?>
-  </ul>
-  <?php endif ?>
-<?php endforeach ?>
-</div>
+<?= w('entry_form') ?>
