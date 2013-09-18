0b4d383dd8db154b24b35fd25b9ef5f0ea0779f4
Samrod Shenassa <sshenassa@takepart.com>
2013-09-17 19:20:53 -0700

    TP-841 Add Food Inc Awards entry page intro text.

--- drupal/profiles/takepart/themes/takepart3/campaigns/foodinc/wordlet-foodinc-entry.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/foodinc/wordlet-foodinc-entry.tpl.php
@@ -1,1 +1,20 @@
-<?= w('entry_form') ?>
+<div class="content" id="foodinc-entryform">
+  <h2><?=w('entry_form_title')?></h2>
+  <section class="left-rail">
+    <? foreach (wl('howto_section') as $ws): ?>
+      <section>
+        <h2><?= $ws->single ?><span><?= $ws->img_src ?></span></h2>
+        <? if ($wis = wl($ws->token . '_items')): ?>
+          <ul <?= wa($ws->token . '_items') ?>>
+            <? foreach ($wis as $wi): ?>
+              <li class="<?= $wi->single ?>"><?= $wi->multi ?></li>
+            <? endforeach ?>
+          </ul>
+        <? endif ?>
+      </section>
+    <? endforeach ?>
+  </section>
+  <section class="entry-form-container right-rail">
+    <?= w('entry_form') ?>
+  </section>
+</div>
