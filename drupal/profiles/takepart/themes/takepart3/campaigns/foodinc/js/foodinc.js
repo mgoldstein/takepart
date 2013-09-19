5b3ca9f537aaca71387f40bf14f2a763e69aca52
Marc Goldstein <mgoldstein@takepart.com>
2013-09-18 17:13:47 -0700

    Add metrics to social share on Food Inc awards

--- drupal/profiles/takepart/themes/takepart3/campaigns/foodinc/js/foodinc.js
+++ drupal/profiles/takepart/themes/takepart3/campaigns/foodinc/js/foodinc.js
@@ -36,7 +36,45 @@
     };
   }
 
-})(jQuery, Drupal, this, this.document);
+})
+
+(function ($) {
+    $('a.tp-social-facebook').click(
+        function(e) {
+            var s=s_gi('takepartprod');
+            s.linkTrackVars='eVar27,events';
+            s.linkTrackEvents='event25';
+            s.events='event25';
+            s.eVar27="Facebook share";
+            s.tl(true, 'o', 'Food Inc Awards Social Sharing');
+        }
+        );
+            
+            $('a.tp-social-twitter').click(
+        function(e) {
+            var s=s_gi('takepartprod');
+            s.linkTrackVars='eVar27,events';
+            s.linkTrackEvents='event25';
+            s.events='event25';
+            s.eVar27="Twitter Tweet";
+            s.tl(true, 'o', 'Food Inc Awards Social Sharing');
+        }
+        );
+            
+            $('a.tp-social-googleplus').click(
+        function(e) {
+            var s=s_gi('takepartprod');
+            s.linkTrackVars='eVar27,events';
+            s.linkTrackEvents='event25';
+            s.events='event25';
+            s.eVar27="GooglePlus share";
+            s.tl(true, 'o', 'Food Inc Awards Social Sharing');
+        }
+        );
+            
+})
+
+(jQuery, Drupal, this, this.document);
 
 
 
