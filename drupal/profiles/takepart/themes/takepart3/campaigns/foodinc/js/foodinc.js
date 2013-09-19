101a405de1d312854429b75625036d9c0034466b
Scott Knight <sknight17@gmail.com>
2013-09-19 14:19:30 -0700

    conflict with epic branch

--- drupal/profiles/takepart/themes/takepart3/campaigns/foodinc/js/foodinc.js
+++ drupal/profiles/takepart/themes/takepart3/campaigns/foodinc/js/foodinc.js
@@ -44,49 +44,6 @@
 
 
 (function ($) {
-    $('a.tp-social-facebook').click(
-        function(e) {
-            var s=s_gi('takepartprod');
-            s.linkTrackVars='eVar27,events';
-            s.linkTrackEvents='event25';
-            s.events='event25';
-            s.eVar27="Facebook share";
-            s.tl(true, 'o', 'Food Inc Awards Social Sharing');
-        }
-      }
-
-      tpSocial:
-      {
-        attach: function(context) {
-          var tp_social_config = {
-            url_append: '?cmpid=organic-share-{{name}}',
-            services: [
-              {name: 'facebook'},
-              {
-                name: 'twitter',
-                text: '{{title}}',
-                via: 'TakePart'
-              },
-              {name: 'googleplus'},
-              {name: 'email'}
-            ]
-          };
-          $('.tp-social:not(.tp-social-skip)').tpsocial(tp_social_config);
-        }
-      },
-
-      FBook:
-      {
-        attach: function(context) {
-          FB.Canvas.setSize({ width: 640, height: 1000 });
-        }
-      }
-    }
-  })
-})(jQuery, Drupal, this, this.document);
-
-
-(function ($) {
   $('a.tp-social-facebook').click(
       function(e) {
           var s=s_gi('takepartprod');
