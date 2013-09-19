9ec62d593d9a8f265b82e41475cdcffaaeff87a2
Scott Knight <sknight17@gmail.com>
2013-09-19 12:00:24 -0700

    fixed TP-889

--- drupal/profiles/takepart/themes/takepart3/campaigns/foodinc/js/foodinc.js
+++ drupal/profiles/takepart/themes/takepart3/campaigns/foodinc/js/foodinc.js
@@ -4,20 +4,20 @@
   });
 
   $.extend(true, Drupal, {behaviors: {
-      expandJudges:
-      {
-        attach: function (context) {
-          $(".show-more").click(function () {
-              if($(this).prev().hasClass("show-more-height")) {
-                  $(this).text("See Less");
-              } else {
-                  $(this).text("See More");
-              }
+      // expandJudges:
+      // {
+      //   attach: function (context) {
+      //     $(".show-more").click(function () {
+      //         if($(this).prev().hasClass("show-more-height")) {
+      //             $(this).text("See Less");
+      //         } else {
+      //             $(this).text("See More");
+      //         }
 
-              $(this).prev().toggleClass("show-more-height");
-          });
-        }
-      },
+      //         $(this).prev().toggleClass("show-more-height");
+      //     });
+      //   }
+      // },
 
       tpSocial:
       {
