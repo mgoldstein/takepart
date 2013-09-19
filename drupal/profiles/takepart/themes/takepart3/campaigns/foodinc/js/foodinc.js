f8abf066dde42d1c0a01c01c362fee9a7c2c0fcb
Samrod Shenassa <sshenassa@takepart.com>
2013-09-18 19:29:33 -0700

    TP-840 styled Month, Year and State dropdowns using Gerald's tpselect jQ plugin.

--- drupal/profiles/takepart/themes/takepart3/campaigns/foodinc/js/foodinc.js
+++ drupal/profiles/takepart/themes/takepart3/campaigns/foodinc/js/foodinc.js
@@ -1,60 +1,54 @@
 (function ($, Drupal, window, document, undefined) {
-  Drupal.behaviors.expandJudges = {
-    attach: function (context) {
-      $(".show-more").click(function () {
-          if($(this).prev().hasClass("show-more-height")) {
-              $(this).text("See Less");
-          } else {
-              $(this).text("See More");
-          }
+  $(document).ready(function() {
+    $('#foodinc-entryform select').tpselect();
+  });
 
-          $(this).prev().toggleClass("show-more-height");
-      });
+  $.extend(true, Drupal, {behaviors: {
+      expandJudges:
+      {
+        attach: function (context) {
+          $(".show-more").click(function () {
+              if($(this).prev().hasClass("show-more-height")) {
+                  $(this).text("See Less");
+              } else {
+                  $(this).text("See More");
+              }
+
+              $(this).prev().toggleClass("show-more-height");
+          });
+        }
+      },
+
+      tpSocial:
+      {
+        attach: function(context) {
+          var tp_social_config = {
+            url_append: '?cmpid=organic-share-{{name}}',
+            services: [
+              {name: 'facebook'},
+              {
+                name: 'twitter',
+                text: '{{title}}',
+                via: 'TakePart'
+              },
+              {name: 'googleplus'},
+              {name: 'email'}
+            ]
+          };
+          $('.tp-social:not(.tp-social-skip)').tpsocial(tp_social_config);
+        }
+      },
+
+      FBook:
+      {
+        attach: function(context) {
+          FB.Canvas.setSize({ width: 640, height: 1000 });
+        }
+      }
     }
-  }
-  Drupal.behaviors.tpSocial={
-    attach: function(context) {
-      var tp_social_config = {
-        url_append: '?cmpid=organic-share-{{name}}',
-        services: [
-          {name: 'facebook'},
-          {
-            name: 'twitter',
-            text: '{{title}}',
-            via: 'TakePart'
-          },
-          {name: 'googleplus'},
-          {name: 'email'}
-        ]
-      };
-      $('.tp-social:not(.tp-social-skip)').tpsocial(tp_social_config);
-    }
-  }
-  Drupal.behaviors.FBook={
-    attach: function(context) {
-      FB.Canvas.setSize({ width: 640, height: 1000 });
-    }
-  }
-  }
+  });
 
-})(jQuery, Drupal, this, this.document);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
+)(jQuery, Drupal, this, this.document);
 
 
