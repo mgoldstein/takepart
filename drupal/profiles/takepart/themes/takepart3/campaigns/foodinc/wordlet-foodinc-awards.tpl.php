3a93676f9978d01d4653eec1eef8bcb03b1738b2
Scott Knight <sknight17@gmail.com>
2013-09-19 18:05:18 -0700

    fixed tp-749 analytics

--- drupal/profiles/takepart/themes/takepart3/campaigns/foodinc/wordlet-foodinc-awards.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/foodinc/wordlet-foodinc-awards.tpl.php
@@ -68,7 +68,17 @@
     </div>
   </section>
   <section class="right-rail">
-    <div class="ad"><?php print (w('foodinc_awards_adblock') != NULL ? render(w('foodinc_awards_adblock')) : ''); ?></div>
+    <div class="ad">
+            <!-- place in the <body> to display the 300x250 ad -->
+      <!-- TP3_ROS_RR_ATF_300x250 -->
+      <div id='div-gpt-ad-1379616725962-0' style='width:300px; height:250px;'>
+      <script type='text/javascript'>
+      googletag.cmd.push(function()
+      { googletag.display('div-gpt-ad-1379616725962-0'); }
+      );
+      </script>
+      </div>
+    </div>
     <div class="newsletter">
       <div class="title"><?php print w('foodinc_awards_stayconnected')->single; ?></div>
       <div class="form-wrapper-first">
