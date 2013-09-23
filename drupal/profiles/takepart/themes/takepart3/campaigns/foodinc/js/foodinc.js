069e734f991d0454c691390a9fb3c8663f49d9a5
Scott Knight <sknight17@gmail.com>
2013-09-23 13:48:30 -0700

    custom social messages

--- drupal/profiles/takepart/themes/takepart3/campaigns/foodinc/js/foodinc.js
+++ drupal/profiles/takepart/themes/takepart3/campaigns/foodinc/js/foodinc.js
@@ -83,9 +83,11 @@
               services: [
                 {
                   name: 'facebook',
-                  media: social_image,
+                  image: social_image,
                   description: social_text,
-                  text: social_title
+                  title: social_title,
+                  url: social_url,
+                  caption: social_caption
                 },
                 {
                   name: 'twitter',
