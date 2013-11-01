b5828656cfa6dd4a8aba2df4dd06f5c7dd18191a
Scott Knight <sknight17@gmail.com>
2013-11-01 16:40:51 -0700

    themed megamenu

--- drupal/sites/all/modules/custom/tp_megamenu/tp-megamenu-megamenu.tpl.php
+++
@@ -1,9 +0,0 @@
-<div class="parent-menu">
-  <?php print $parent_menu; ?>
-  <span></span>
-</div>
-<div class="child-menus">
-  <?php foreach($somethings as $key => $something): ?>
-
-  <?php endforeach; ?>
-</div>
