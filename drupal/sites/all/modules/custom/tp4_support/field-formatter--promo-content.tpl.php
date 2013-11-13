162cd6a7e40f2e97cea7611922c9406033fef34a
Scott Knight <sknight17@gmail.com>
2013-11-13 15:13:54 -0800

    themed topics on topic page

---
+++ drupal/sites/all/modules/custom/tp4_support/field-formatter--promo-content.tpl.php
@@ -0,0 +1,42 @@
+<ul class="promo-content">
+  <?php
+    foreach($items as $item){
+      $field_collection = entity_load('field_collection_item', array($item['value']));
+      $key = $item['value'];
+      $node = node_load($field_collection[$key]->field_promo_content['und'][0]['target_id']);
+      $path = drupal_get_path_alias('node/'. $node->nid);
+
+      //Promo Headline
+      $promo_headline = '';
+      if(isset($field_collection[$key]->field_promo_headline['und'][0]['value'])){
+        $promo_headline = $field_collection[$key]->field_promo_headline['und'][0]['value'];
+      }
+      elseif(isset($node->field_promo_headline['und'][0]['value'])){
+        $promo_headline = $node->field_promo_headline['und'][0]['value'];
+      }
+      else{
+        $promo_headline = $node->title;
+      }
+
+      //Promo Image
+      $promo_image = '';
+      if(isset($field_collection[$key]->field_promo_thumbnail['und'][0]['uri'])){
+        $thumb_uri = $field_collection[$key]->field_promo_thumbnail['und'][0]['uri'];
+        $promo_image = '<img src="'. image_style_url('topic_feature', $thumb_uri). '">';
+      }
+      else{
+        $thumb = file_load($node->field_thumbnail['und'][0]['fid']);
+        $promo_image = '<img src="'. image_style_url('topic_feature', $thumb->uri). '">';
+      }
+
+      //Output
+      $output = '<li>';
+      $output .= '<div class="promo-image">'. l($promo_image, $path, array('html' => true)). '</div>';
+      $output .= '<div class="promo-headline">'. l($promo_headline, $path, array('html' => true)). '</div>';
+      $output .= '</li>';
+
+      print $output;
+    }
+  ?>
+</ul>
+<div class="clearfix"></div>
