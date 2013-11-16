e6dedcb1677b5c828a8cee6953205b8d8af6cd36
Matthew Wrather <matt@wrathercreative.com>
2013-11-15 17:42:21 -0800

    Add and style sidebar blocks to article page

---
+++ drupal/sites/all/themes/tp4/templates/bean--of-the-day-custom.tpl.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @file
+ * Theme implementation for "of the day" block.
+ *
+ * Available variables:
+ * - $custom_render
+ * @see template_preprocess_entity()
+ */
+?>
+
+<ul>
+  <? foreach ( $custom_render as $item ): ?>
+    <li class="of-the-day-item">
+      <a href="<?=$item['url'] ?>">
+	<img src="<?=$item['thumbnail']; ?>" alt="<?=$item['thumbnail_alt']; ?> image" height="100" width="100" />
+	<h4 class="subhead"><?=$item['typename']; ?></h4>
+	<h2 class="node-title"><?= $item['title'][0]['safe_value']; ?></h2>
+      </a>
+    </li>
+  <? endforeach ?>
+</ul>
