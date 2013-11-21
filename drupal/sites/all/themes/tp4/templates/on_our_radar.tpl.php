2ad1e48eeacf9c9119fc7dd001402033e6bf4a1f
Matthew Wrather <matt@wrathercreative.com>
2013-11-20 16:49:04 -0800

    Comment Out Taboola Widgets.

--- drupal/sites/all/themes/tp4/templates/on_our_radar.tpl.php
+++ drupal/sites/all/themes/tp4/templates/on_our_radar.tpl.php
@@ -8,17 +8,9 @@
 <aside class="<?php print $classes; ?>"<?php print $attributes; ?>>
 
   <?php print render($title_prefix); ?>
-  <h3<?php print $title_attributes; ?>>From The Web</h3>
+  <h3<?php print $title_attributes; ?>>On Our Radar</h3>
   <?php print render($title_suffix); ?>
 
-<div id='taboola-below-main-column'></div>
-<script type="text/javascript">
-window._taboola = window._taboola || [];
-_taboola.push({mode:'thumbs-1r', container:'taboola-below-main-column', placement:'below-main-column'});
-</script>
-
-
-    <h3>On Our Radar</h3>
     <ol>
       <?php foreach ( $links as $link ) : ?>
 	<?php if ( $link['title'] ) : ?>
