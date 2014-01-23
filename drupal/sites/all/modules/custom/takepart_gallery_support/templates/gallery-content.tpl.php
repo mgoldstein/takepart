4b000d306573555e96a14f1c337c76f3903a6fa3
Matthew Wrather <matt@wrathercreative.com>
2014-01-22 19:51:25 -0800

    style gallery next/prev links for mobile

--- drupal/sites/all/modules/custom/takepart_gallery_support/templates/gallery-content.tpl.php
+++ drupal/sites/all/modules/custom/takepart_gallery_support/templates/gallery-content.tpl.php
@@ -23,46 +23,48 @@
     </aside>
   </header>
 
-  <div id="slides" class="swipe">
-    <div class="swipe-wrap">
-    <?php foreach($slides as $delta => $slide) : ?>
-      <div<?php print drupal_attributes($slide['attributes']) ?>>
-        <figure>
-          <div class="slide-image-wrapper">
-            <?php print $slide['image']; ?>
-          </div>
-          <figcaption class="slide-caption">
-            <h2 class="slide-caption-headline"><?php print $slide['title']; ?></h2>
-            <div class="slide-caption-content"><?php print $slide['caption']; ?></div>
-          </figcaption>
-        </figure>
-      </div>
-    <?php endforeach; ?>
-    <?php if ($next_gallery) : ?>
-      <div id="next-gallery"<?php print drupal_attributes($next_gallery['attributes']); ?>>
-        <figure>
-          <a href="<?php print $next_gallery['href']; ?>">
+  <div class="slides-wrapper">
+    <div id="slides" class="swipe">
+      <div class="swipe-wrap">
+      <?php foreach($slides as $delta => $slide) : ?>
+        <div<?php print drupal_attributes($slide['attributes']) ?>>
+          <figure>
             <div class="slide-image-wrapper">
-              <?php print $next_gallery['image']; ?>
-              <div class="gallery-cover-content">
-                <div class="gallery-cover-branding"><?php print t('Up Next'); ?></div>
-                <div class="gallery-cover-title"><?php print $next_gallery['title']; ?></div>
-                <div class="gallery-cover-enter"><?php print t('Enter Photo Gallery'); ?></div>
-              </div>
+              <?php print $slide['image']; ?>
             </div>
-          </a>
-          <figcaption class="slide-caption">
-          <h2 class="slide-caption-headline"><?php print $next_gallery['title']; ?></h2>
-          <div class="slide-caption-content"><?php print $next_gallery['caption']; ?></div>
-          </figcaption>
-        </figure>
+            <figcaption class="slide-caption">
+              <h2 class="slide-caption-headline"><?php print $slide['title']; ?></h2>
+              <div class="slide-caption-content"><?php print $slide['caption']; ?></div>
+            </figcaption>
+          </figure>
+        </div>
+      <?php endforeach; ?>
+      <?php if ($next_gallery) : ?>
+        <div id="next-gallery"<?php print drupal_attributes($next_gallery['attributes']); ?>>
+          <figure>
+            <a href="<?php print $next_gallery['href']; ?>">
+              <div class="slide-image-wrapper">
+                <?php print $next_gallery['image']; ?>
+                <div class="gallery-cover-content">
+                  <div class="gallery-cover-branding"><?php print t('Up Next'); ?></div>
+                  <div class="gallery-cover-title"><?php print $next_gallery['title']; ?></div>
+                  <div class="gallery-cover-enter"><?php print t('Enter Photo Gallery'); ?></div>
+                </div>
+              </div>
+            </a>
+            <figcaption class="slide-caption">
+            <h2 class="slide-caption-headline"><?php print $next_gallery['title']; ?></h2>
+            <div class="slide-caption-content"><?php print $next_gallery['caption']; ?></div>
+            </figcaption>
+          </figure>
+        </div>
+      <?php endif; ?>
       </div>
-    <?php endif; ?>
     </div>
+    <nav id="gallery-nav" class="gallery-nav">
+      <a href="#" id="previous-slide" class="gallery-nav-link previous" title="Previous Slide"></a>
+      <span class="pagination"><span id="current-slide">0</span> of <span id="total-slides">0</span></span>
+      <a href="#" id="next-slide" class="gallery-nav-link next" title="Next Slide"></a>
+    </nav>
   </div>
-  <nav id="gallery-nav" class="gallery-nav">
-    <a href="#" id="previous-slide" class="gallery-nav-link previous" title="Previous Slide"></a>
-    <span class="pagination"><span id="current-slide">0</span> of <span id="total-slides">0</span></span>
-    <a href="#" id="next-slide" class="gallery-nav-link next" title="Next Slide"></a>
-  </nav>
 </article>
