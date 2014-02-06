245da2fca9b6ff8a924b7fdea5f5de1cb6f2d379
Scott Knight <sknight17@gmail.com>
2014-02-06 10:41:14 -0800

    interstitial social styling

--- drupal/profiles/takepart/themes/takepart3/campaigns/interstitials/social_follows/wordlet-interstitial-social-follows.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/interstitials/social_follows/wordlet-interstitial-social-follows.tpl.php
@@ -20,6 +20,9 @@
 			</a>
 		<? endforeach ?>
 	</div>
+	<div class="fb-like" data-href="https://www.facebook.com/takepart" data-colorscheme="light" data-layout="button" data-action="like" data-show-faces="false" data-share="false"></div>
+	<a href="//twitter.com/takepart" class="twitter-follow-button" data-show-count="false" data-size="large" data-show-screen-name="false" data-lang="en">Follow</a>
+	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 		<p id="dont">
 			<a href="#"><?=w('do_not_show') ?></a>
 		</p>
