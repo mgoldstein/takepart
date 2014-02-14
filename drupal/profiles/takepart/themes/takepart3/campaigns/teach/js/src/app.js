9627f2d8652745eafed175ff266dd3b513b05d2b
Matthew Wrather <matt@wrathercreative.com>
2014-02-13 17:41:04 -0800

    WIP on form validation and coppa compliance

--- drupal/profiles/takepart/themes/takepart3/campaigns/teach/js/src/app.js
+++ drupal/profiles/takepart/themes/takepart3/campaigns/teach/js/src/app.js
@@ -1,10 +1,71 @@
 (function($, window, document, undefined){
 
+  var touchEnabled = 'ontouchstart' in window || window.DocumentTouch && document instanceof DocumentTouch;
+
+  var coppaCookieName = "pm_sys_user_birthdate",
+      coppaCookieExpires = 1, // days to keep the cookie
+      msDay = 24 * 60 * 60 * 1000, // one day in milliseconds
+      ageRequirement = Date.now() - 13 * 365 * msDay; // 13 years in milliseconds
+
+  // show coppa error message and delete form from page
+  var showCoppaErrorMessage = function() {
+      $('#sys-form-content').slideUp().remove();
+      $('#sys-coppa-content').slideDown();
+  };
+
+  var sysFormSubmit = function(form) {
+    var $form = $(form),
+        formData = {};
+
+    // let's get all the data
+    $form.find("input, textarea, select").not('[type="checkbox"]').each(function() {
+      formData[this.id] = $(this).val();
+    });
+    formData.email_subscribe = $form.find('#email_subscribe').is(':checked');
+    formData.terms_agree = $form.find('#terms_agree').is(':checked');
+    console.log(formData); // TODO remove after debugging
+
+    var userBirthdate = formData.user_year + '-' + formData.user_month + '-' + formData.user_day;
+
+    if (Date.parse(userBirthdate) > ageRequirement) {
+
+      // set coppa cookie
+      var expires = new Date();
+      expires.setTime(expires.getTime() + coppaCookieExpires * msDay);
+      document.cookie = escape(coppaCookieName) + "=" + escape(userBirthdate) + "; expires=" +  expires.toGMTString() + '; path=/';
+
+      showCoppaErrorMessage();
+
+      return false;
+    }
+
+    // we've passed the age check. Lets have a beer.
+
+    alert('TODO: successful form submit goes here.');
+    $('#sys-form-content').slideUp().remove();
+    $('#sys-thanks-content').slideDown();
+  };
 
   // not using Drupal.behaviors because this JS has nothing to do with drupal
   $(document).ready(function() {
-    // populate character count divs from maxlength
-    $('input[maxlength], textarea[maxlength]').not('.story-year').each(function() {
+
+    // coppa check
+    var birthDate = null;
+    $.each(document.cookie.split(";"), function() {
+      if (this.trim().indexOf(escape(coppaCookieName)) === 0) {
+        birthDate = unescape(this.substring(escape(coppaCookieName).length + 1, this.length));
+      }
+    });
+    if (birthDate && (Date.parse(birthDate) > ageRequirement)) {
+      showCoppaErrorMessage();
+      return false;
+    }
+
+    // we've passed the age test. have a beer.
+    var $form = $('#sys-form');
+
+    // populate character count divs from maxlength properties
+    $form.find('input[maxlength], textarea[maxlength]').each(function() {
       var $this = $(this),
           maxlength = $this.attr('maxlength');
 
@@ -32,6 +93,11 @@
       });
     });
 
+    // style select boxes on non-touch-enabled devices
+    if (!touchEnabled) {
+        $form.find('select').customSelect();
+    }
+
     // Preview
     $('#sys-preview').on('click', function(e) {
       e.preventDefault();
@@ -41,13 +107,13 @@
       $.tpmodal.show({node: $modal[0]});
     });
 
-    $('#sys-submit').on('click', function(e) {
-      e.preventDefault();
-
-      if ($('#sys-form').valid()) {
-        alert('yes!');
+    $form.validate({
+      submitHandler: sysFormSubmit,
+      messages: {
+        terms_agree: {
+          required: "You must agree to to submit your story."
+        }
       }
-
     });
 
   }); // $(document).ready() callback
--- drupal/profiles/takepart/themes/takepart3/campaigns/teach/partials/teach-sys-form.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/teach/partials/teach-sys-form.tpl.php
@@ -4,7 +4,27 @@
     <div class="field-wrapper left"><label for="first_name">First Name</label><input type="text" name="first_name" id="first_name" placeholder="First Name*" required></div>
     <div class="field-wrapper right"><label for="last_name">Last Name</label><input type="text" name="last_name" id="last_name" placeholder="Last Name*" required></div>
     <div class="field-wrapper"><label for="email">Email</label><input type="email" name="email" id="email" placeholder="Email*" required></div>
-    <div>COPPA stuff TBD</div>
+    <div class="field-wrapper">
+      <label for="user_year" class="visible"><span class="vertically-center">Your Birthdate</span></label>
+      <select name="user_month" id="user_month" required>
+        <option value="">Month*</option>
+        <?php foreach (range(1,12) as $month) : ?>
+        <option value="<?php print $month; ?>"><?php print date("F", mktime(0,0,0,$month,10)); ?></option>
+        <?php endforeach; ?>
+      </select>
+      <select name="user_day" id="user_day" required>
+        <option value="">Day*</option>
+        <?php foreach (range(1,31) as $day) : ?>
+        <option value="<?php print $day; ?>"><?php print $day; ?></option>
+        <?php endforeach; ?>
+      </select>
+      <select name="user_year" id="user_year" required>
+        <option value="">Year*</option>
+        <?php foreach (array_reverse(range(1900, date(Y))) as $year) : ?>
+        <option value="<?php print $year; ?>"><?php print $year; ?></option>
+        <?php endforeach; ?>
+      </select>
+    </div>
   </fieldset>
   <fieldset>
     <legend>Your Teacher</legend>
@@ -15,7 +35,16 @@
   <fieldset>
     <legend>Your Story</legend>
     <div class="field-wrapper"><label for="story_title">Story Title</label><input type="text" name="story_title" id="story_title" placeholder="Story Title*" maxlength="100" required></div>
-    <div class="field-wrapper field-wrapper-story-year clearfix"><label class="visible"><span class="vertically-center">Approximate Year This Story Happened</span><input type="text" maxlength="4" id="story_year" name="story_year" class="story-year pull-right" required></label></div>
+    <div class="field-wrapper field-wrapper-story-year clearfix">
+      <label class="visible"><span class="vertically-center">Approximate Year This Story Happened</span>
+        <select name="story_year" id="story_year" class="pull-right" required>
+          <option value="">Year</option>
+          <?php foreach (array_reverse(range(1950, date(Y))) as $year) : ?>
+          <option value="<?php print $year; ?>"><?php print $year; ?></option>
+          <?php endforeach; ?>
+        </select>
+      </label>
+    </div>
     <div class="field-wrapper"><label for="story_body">Your Story</label><textarea name="story_body" id="story_body" cols="30" rows="10" placeholder="Tell us your story" maxlength="400" required></textarea></div>
   </fieldset>
   <fieldset>
@@ -34,8 +63,8 @@
     <div class="field-wrapper field-wrapper-terms-agree"><input type="checkbox" id="terms_agree" name="terms_agree" required><label for="terms_agree" class="visible">I have read and agree to the <a href="#">content submission agreement</a></label></div>
   </fieldset>
   <p class="clearfix">
-    <a href="#" id="sys-preview" class="sys-button left">Preview Story</a>
-    <a href="#" id="sys-submit" class="sys-button sys-button-submit right">Submit Story</a>
+    <input type="button" id="sys-preview" class="sys-button left" value="Preview Story" />
+    <input type="submit" id="sys-submit" class="sys-button sys-button-submit right" value="Submit Story" />
   </p>
   <p class="fine-print clearfix">By clicking "Submit Story" above, you agree to TakePart's <?php print l('Terms of Use', 'terms-of-use', array('attributes' => array('target' => '_blank'))); ?> and <?php print l('Privacy Policy', 'privacy-policy', array('attributes' => array('target' => '_blank'))); ?></p>
 </form>
