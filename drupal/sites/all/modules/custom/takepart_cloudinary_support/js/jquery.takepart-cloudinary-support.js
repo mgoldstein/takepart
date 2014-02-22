2e8dc02d07dff88803b30050c40cb35fe1814061
Matthew Wrather <matt@wrathercreative.com>
2014-02-21 16:49:48 -0800

    Updates to Cloudinary Module config

--- drupal/sites/all/modules/custom/takepart_cloudinary_support/js/jquery.takepart-cloudinary-support.js
+++ drupal/sites/all/modules/custom/takepart_cloudinary_support/js/jquery.takepart-cloudinary-support.js
@@ -1,10 +1,15 @@
 (function($, Drupal, undefined) {
   Drupal.behaviors.takepartCloudinarySupport = {
     attach: function() {
+
       // configure cloudinary
       $.cloudinary.config(Drupal.settings.cloudinary_support.config);
-      // add data to form inputs
-      $("input.cloudinary-fileupload[type=file]").data('form-data', Drupal.settings.cloudinary_support.file_field_data);
+
+      // // add data to form inputs
+      $("input.cloudinary-fileupload[type=file]").cloudinary_fileupload({
+	formData: Drupal.settings.cloudinary_support.file_field_data
+      });
+
     }
   };
 })(jQuery, Drupal);
