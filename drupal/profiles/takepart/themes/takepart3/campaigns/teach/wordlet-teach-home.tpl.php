e69b6eef982de699d8c6affde05ad153ee90e245
Matthew Wrather <matt@wrathercreative.com>
2014-02-24 16:52:58 -0800

    wip: template and basic styling for teach home

--- drupal/profiles/takepart/themes/takepart3/campaigns/teach/wordlet-teach-home.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/teach/wordlet-teach-home.tpl.php
@@ -1,15 +1,18 @@
-<div class="content">
-	<h4 class="call-to-share"><?=w('social_follow_body')?></h4>
-	<h2 class="content-header"><span><?=w('page_title')?></span></h2>
-  <div class="main-video-wrapper" <?=wa('video')?>>
-      <script src="<?=w('video')?>"></script>
+<div class="page-body-content">
+  <div class="row" <?php print wa('home_sections'); ?> >
+    <?php foreach (wl('home_sections') as $w) : ?>
+    <div class="col-1-3 home-section">
+      <div class="image-wrapper"><img src="<?php print $w->img_src; ?>"></div>
+      <h2 class="headline"><?php print $w->single; ?></h2>
+      <div class="body"><?php print $w->multi; ?></div>
+    </div>
+    <?php endforeach; ?>
   </div>
-	<section class="social-menu">
-		<? include('partials/teach-social-block.tpl.php') ?>
-	</section>
-	<section class="page-body-content cms">
-		<?= w('page_body_content') ?>
-    <? include('partials/teach-watch-promo.tpl.php') ?>
-	</section>
+  <section class="featured-articles">
+    <h2><?php print w('featured_articles_headline'); ?></h2>
+    <div><?php print w('featured_stories_block');?></div>
+  </section>
+  <section class="social-menu">
+    <? include('partials/teach-social-block.tpl.php') ?>
+  </section>
 </div>
-<br clear="all" />
