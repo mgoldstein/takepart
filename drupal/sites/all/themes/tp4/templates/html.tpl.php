52bacb705e6b7f0a1375c3b6c16758126f8823f1
Marc Goldstein <mgoldstein@takepart.com>
2014-03-04 12:45:56 -0800

    Try placing twitter at the bottom of the meta tag order

--- drupal/sites/all/themes/tp4/templates/html.tpl.php
+++ drupal/sites/all/themes/tp4/templates/html.tpl.php
@@ -6,19 +6,22 @@
  * Complete documentation for this file is available online.
  * @see https://drupal.org/node/1728208
  */
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <!--[if IEMobile 7]><html class="iem7" <?php print $html_attributes; ?>><![endif]-->
 <!--[if lte IE 6]><html class="lt-ie9 lt-ie8 lt-ie7" <?php print $html_attributes; ?>><![endif]-->
 <!--[if (IE 7)&(!IEMobile)]><html class="lt-ie9 lt-ie8" <?php print $html_attributes; ?>><![endif]-->
 <!--[if IE 8]><html class="lt-ie9" <?php print $html_attributes; ?>><![endif]-->
-<!--[if (gte IE 9)|(gt IEMobile 7)]><!--><html <?php print $html_attributes /* . $rdf_namespaces */; ?>><!--<![endif]-->
+<!--[if (gte IE 9)|(gt IEMobile 7)]><!-->
+<html <?php print $html_attributes . $rdf_namespaces; ?>>
+<!--<![endif]-->
 <head>
   <title><?php print $head_title; ?></title>
   <?php print $head; ?>
   <?php if ($default_mobile_metatags): ?>
-    <meta name="MobileOptimized" content="width">
-    <meta name="HandheldFriendly" content="true">
-    <meta name="viewport" content="width=device-width">
+  <meta name="MobileOptimized" content="width">
+  <meta name="HandheldFriendly" content="true">
+  <meta name="viewport" content="width=device-width">
   <?php endif; ?>
   <meta http-equiv="cleartype" content="on">
   <?php print $styles; ?>
