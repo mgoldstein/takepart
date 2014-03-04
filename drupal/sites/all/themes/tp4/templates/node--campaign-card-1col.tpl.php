dbeace00bfd6d77eadd0502b100895bacf3660c0
Scott Knight <sknight17@gmail.com>
2014-03-04 11:30:02 -0800

    template updates

--- drupal/sites/all/themes/tp4/templates/node--campaign-card-1col.tpl.php
+++ drupal/sites/all/themes/tp4/templates/node--campaign-card-1col.tpl.php
@@ -7,7 +7,7 @@
  * @see https://drupal.org/node/1728164
  */
 ?>
-<div class="card campaign-1col" style="background-image: url('<?php print $variables['card_background']; ?>')">
+<div class="card campaign-1col <?php print implode(' ', $variables['classes_array']); ?>" style="background-image: url('<?php print $variables['card_background']; ?>')">
   <article class="card-inner">
     <h1 class="card-title"><?php print $title; ?></h1>
     <div class="instructional">
