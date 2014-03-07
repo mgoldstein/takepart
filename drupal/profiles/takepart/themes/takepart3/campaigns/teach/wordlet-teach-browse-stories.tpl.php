f3e600b35891a21582ece4f90908abb4eb378393
Matthew Wrather <matt@wrathercreative.com>
2014-03-07 15:42:30 -0800

    template/style browse page

--- drupal/profiles/takepart/themes/takepart3/campaigns/teach/wordlet-teach-browse-stories.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/teach/wordlet-teach-browse-stories.tpl.php
@@ -1,3 +1,11 @@
-<div class="content teach-story-content">
-    TEACHER STORIES
+<div class="content teach-story-content teach-browse-stories-content">
+  <h1 class="sys-headline"><span><?php print w('browse_stories_headline'); ?></span></h1>
+  <div class="sys-intro-body"><?php print w('browse_stories_intro'); ?></div>
+  <?php include('partials/teach-sys-cta.tpl.php'); ?>
+  <section id="app">
+    <p>APP GOES HERE</p>
+  </section>
+  <section class="social-menu">
+  <?php include('partials/teach-social-block.tpl.php'); ?>
+  </section>
 </div>
