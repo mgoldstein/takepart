ff56cf7bc60619de70273e85399cddfe238acc47
Matthew Wrather <matt@wrathercreative.com>
2014-03-07 12:57:19 -0800

    Refinements to teacher landing page
    * 3 stats instead of 4 at the bottom of the left col
    * Prepare for ajax population of schools, stats, and stories

--- drupal/profiles/takepart/themes/takepart3/campaigns/teach/js/src/landing.js
+++ drupal/profiles/takepart/themes/takepart3/campaigns/teach/js/src/landing.js
@@ -34,18 +34,50 @@
   // this code has nothing to do with Drupal
   $(document).ready(function(){
 
+    var $body = $('body');
+
+    // delegate clicks on stories/schools
+    $body
+      .on('click', '[data-storyid]', function() {
+        alert('@todo - navigate to story ID ' + $(this).data('storyid'));
+      })
+      .on('click', '[data-schoolid]', function() {
+        alert('@todo - navigate to school ID ' + $(this).data('schoolid'));
+      })
+    ;
+
+    // set up most stories lists 
     var $mostStoriesNav = $('#most-stories-nav');
     var $mostStoriesLists = $('.most-stories-list');
+    var $mostStoriesLatest = $mostStoriesLists.filter('#most-stories-latest');
+    var $mostStoriesOverall = $mostStoriesLists.filter('#most-stories-overall');
+
     $mostStoriesLists.hide().filter(':first').show();
     $mostStoriesNav.find('a:first').addClass('active');
+
+    var listItem = tmpl('most_stories_list_template');
+    // @todo ajax call
+    var data = [{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}];    
+    $.each(data, function() {
+      $(listItem(this)).appendTo($mostStoriesLatest);
+      $(listItem(this)).appendTo($mostStoriesOverall);
+    });
+
     $mostStoriesNav.on('click', 'a', function(e){
       e.preventDefault();
       $mostStoriesNav.find('a').removeClass('active').filter(this).addClass('active');
       $mostStoriesLists.hide().filter(e.target.hash).show();
     });
 
+    // populate the story stats
+    var $storyStats = $('.teacher-story-stat');
+    // @todo ajax call
+    $storyStats.find('.campaign-stat').html("54,321");
+
+    // populate the featured stories in polaroids
     $('.featured-stories').find('.featured-story').each(function() {
       var $this = $(this);
+      // @todo ajax call
       $this.html(tmpl('featured_story_template', {}));
     });
   });
