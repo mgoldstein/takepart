9087aee0116eae56774d4605f807684ca7389002
Matthew Wrather <matt@wrathercreative.com>
2014-03-11 12:03:01 -0700

    Upgrade grunt-contrib-uglify and add source maps

--- drupal/profiles/takepart/themes/takepart3/campaigns/teach/Gruntfile.js
+++ drupal/profiles/takepart/themes/takepart3/campaigns/teach/Gruntfile.js
@@ -46,7 +46,8 @@ module.exports = function(grunt) {
     },
     uglify: {
       options: {
-        banner: '<%= banner %>'
+        banner: '<%= banner %>',
+        sourceMap: true
       },
       dist: {
         files: {
