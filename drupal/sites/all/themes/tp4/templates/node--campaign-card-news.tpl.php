4b84ca2268e97f4fecd9b4521426d9d50feef6be
Scott Knight <sknight17@gmail.com>
2014-03-19 12:10:52 -0700

    card title tweaks

--- drupal/sites/all/themes/tp4/templates/node--campaign-card-news.tpl.php
+++ drupal/sites/all/themes/tp4/templates/node--campaign-card-news.tpl.php
@@ -12,16 +12,16 @@
  */
 ?>
 <?php
-  // We hide the comments and links now so that we can render them later.
-  hide($content['comments']);
-  hide($content['links']);
-  // print drupal_render(node_view_multiple($variables['output'], 'default'));
-  foreach($variables['output'] as $key => $item){
-    $file = file_load($item->field_article_main_image['und'][0]['fid']);
-    $image = '<img src="'. file_create_url($file->uri). '">';
-    $headline = '<div class="headline">'. $item->field_promo_headline['und'][0]['value']. '</div>';
-    print '<div class="news-column">'. $image. $headline. $item->type. $item->nid. '</div>';
-  }
+  // // We hide the comments and links now so that we can render them later.
+  // hide($content['comments']);
+  // hide($content['links']);
+  // // print drupal_render(node_view_multiple($variables['output'], 'default'));
+  // foreach($variables['output'] as $key => $item){
+  //   $file = file_load($item->field_article_main_image['und'][0]['fid']);
+  //   $image = '<img src="'. file_create_url($file->uri). '">';
+  //   $headline = '<div class="headline">'. $item->field_promo_headline['und'][0]['value']. '</div>';
+  //   print '<div class="news-column">'. $image. $headline. $item->type. $item->nid. '</div>';
+  // }
 ?>
 
 
