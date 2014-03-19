a90bf16873949e12da292e448b70691656d3ee19
Matthew Wrather <matt@wrathercreative.com>
2014-03-18 18:11:37 -0700

    Add State Name Hash. That's some good hash.

--- drupal/profiles/takepart/themes/takepart3/campaigns/teach/js/teach.js
+++ drupal/profiles/takepart/themes/takepart3/campaigns/teach/js/teach.js
@@ -83,6 +83,8 @@ TEACH.tmpl = function tmpl(str, data) {
     return data ? fn(data) : fn;
 };
 
+TEACH.stateNames = {AL:"Alabama",AK:"Alaska",AS:"America Samoa",AZ:"Arizona",AR:"Arkansas",CA:"California",CO:"Colorado",CT:"Connecticut",DE:"Delaware",DC:"District of Columbia",FL:"Florida",GA:"Georgia",GU:"Guam",HI:"Hawaii",ID:"Idaho",IL:"Illinois",IN:"Indiana",IA:"Iowa",KS:"Kansas",KY:"Kentucky",LA:"Louisiana",ME:"Maine",MD:"Maryland",MA:"Massachusetts",MI:"Michigan",MN:"Minnesota",MS:"Mississippi",MO:"Missouri",MT:"Montana",NE:"Nebraska",NV:"Nevada",NH:"New Hampshire",NJ:"New Jersey",NM:"New Mexico",NY:"New York",NC:"North Carolina",ND:"North Dakota",OH:"Ohio",OK:"Oklahoma",OR:"Oregon",PW:"Palau",PA:"Pennsylvania",PR:"Puerto Rico",RI:"Rhode Island",SC:"South Carolina",SD:"South Dakota",TN:"Tennessee",TX:"Texas",UT:"Utah",VT:"Vermont",VI:"Virgin Island",VA:"Virginia",WA:"Washington",WV:"West Virginia",WI:"Wisconsin",WY:"Wyoming"};
+
 // augment String.prototype with some things
 String.prototype.htmlEntities = function() {
     return this.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
