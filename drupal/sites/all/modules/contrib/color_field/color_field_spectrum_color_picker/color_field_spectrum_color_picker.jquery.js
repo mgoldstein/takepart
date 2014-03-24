965658fd9b27344e08c714c2ee0d48faff8145bf
Scott Knight <sknight17@gmail.com>
2014-03-24 14:18:14 -0700

    hacked the color_field module: TODO: convert to cusotm module

--- drupal/sites/all/modules/contrib/color_field/color_field_spectrum_color_picker/color_field_spectrum_color_picker.jquery.js
+++ drupal/sites/all/modules/contrib/color_field/color_field_spectrum_color_picker/color_field_spectrum_color_picker.jquery.js
@@ -7,7 +7,7 @@
     attach: function (context) {
       $.each(Drupal.settings.color_field_spectrum, function (selector) {
         $('#' + this.id).spectrum({
-          preferredFormat: "hex",
+          preferredFormat: "rgb",
           showInput: this.show_input,
           showAlpha: true,
           showInitial: true,
