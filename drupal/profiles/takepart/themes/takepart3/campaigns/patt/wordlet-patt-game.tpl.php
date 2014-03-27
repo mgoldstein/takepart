92b6ad8585afe0c3ec9eb99e01ebcc1d9d9d36be
Scott Knight <sknight17@gmail.com>
2014-03-27 14:34:44 -0700

    hotfix

--- drupal/profiles/takepart/themes/takepart3/campaigns/patt/wordlet-patt-game.tpl.php
+++ drupal/profiles/takepart/themes/takepart3/campaigns/patt/wordlet-patt-game.tpl.php
@@ -7,13 +7,13 @@
   }
   $browser = browscap_get_browser();
 ?>
-<div class="game" <?=wa('game_iframe')?>>
+<div class="game" <?=w('game_iframe')?>>
   <iframe src="<?php print $iframe; ?>"></iframe>
 </div>
 
 <script language=javascript>
 <!--
-if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i) || (navigator.userAgent.match(/iPad/i) || (navigator.userAgent.match(/Android/i))) {
+if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i)) || (navigator.userAgent.match(/iPad/i)) || (navigator.userAgent.match(/Android/i))) {
    location.replace("<?=wa('game_iframe')?>");
 }
 -->
