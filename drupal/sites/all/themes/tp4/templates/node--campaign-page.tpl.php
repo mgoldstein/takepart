8a6d5c7d3dcd04644046a8ec2a205382d3a5f849
Scott Knight <sknight17@gmail.com>
2014-04-21 13:48:37 -0700

    removed unused templates and added node--campaign-page for facebook comments

--- drupal/sites/all/themes/tp4/templates/node--campaign-page.tpl.php
+++ drupal/sites/all/themes/tp4/templates/node--campaign-page.tpl.php
@@ -8,7 +8,6 @@
  */
 ?>
 <article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-
   <?php
     // We hide the comments and links now so that we can render them later.
     hide($content['comments']);
@@ -17,5 +16,9 @@
     // print render($content['links']);
     // print render($content['comments']);
   ?>
-
+  <div id="article-comments">
+    <h3 class="top-border">Comments <span>(<fb:comments-count href="<?php print $url_production; ?>"></fb:comments-count>)</span></h3>
+    <fb:comments href="<?php print $url_production; ?>" numposts="15"></fb:comments>
+  </div>
 </article>
+
