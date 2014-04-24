ceb5a5d3fc06b12829af6429e7b0832c11b23ab7
Matthew Wrather <matt@wrathercreative.com>
2014-04-24 15:23:48 -0700

    add flashcards branding

--- drupal/sites/all/themes/tp4/templates/node--flashcard--full.tpl.php
+++ drupal/sites/all/themes/tp4/templates/node--flashcard--full.tpl.php
@@ -8,10 +8,9 @@
  */
 ?>
 <article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
-
   <header class="article-header">
     <?php print render($title_prefix); ?>
-    <div class="flashcard-branding"><a href="#"><img alt="TakePart Flashcards" src="http://placehold.it/1000x75&text=Flashcard+Branding"></a></div>
+    <div class="flashcard-branding"><a href="/flashcards"><span>fl</span>ashcards</a></div>
     <?php print render($content['field_flashcard_page_headline']); ?>
     <?php print render($content['field_article_subhead']); ?>
     <?php print render($title_suffix); ?>
