3e52b12ca84fc033de710d89d21171064abcb576
August Pascual <apascual@stauffer.com>
2014-04-30 13:32:31 -0700

    TP-1869 Slim text card type

--- drupal/sites/all/themes/tp4/templates/node--campaign-card-1col.tpl.php
+++ drupal/sites/all/themes/tp4/templates/node--campaign-card-1col.tpl.php
@@ -9,7 +9,7 @@
 ?>
 <div class="card campaign-1col <?php print implode(' ', $variables['classes_array']); ?>" style="background-image: url('<?php print $variables['card_background']; ?>'); <?php print implode(' ', $variables['styles']); ?>">
   <?php print render($title_prefix); ?>
-  <article class="card-inner">
+  <article class="card-inner<?php if(isset($variables['slim_text'])){ print " ".$variables['slim_text']; }?>">
   <?php print render($title_suffix);  // contextual links ?>
     <?php if(isset($node->field_campaign_card_title['und'][0]['value']) == true): ?>
       <div class="title-wrapper"><h1 class="card-title <?php print (isset($variables['instructional']) == true ? 'has-instructional' : ''); ?>"><?php print $node->field_campaign_card_title['und'][0]['value']; ?></h1></div>
