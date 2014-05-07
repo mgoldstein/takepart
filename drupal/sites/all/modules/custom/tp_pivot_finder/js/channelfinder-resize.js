7196db8e48c240b42639d3dd2412d15c93ca6a58
Scott Knight <sknight17@gmail.com>
2014-05-07 15:56:43 -0700

    added tp_pivot_finder module

---
+++ drupal/sites/all/modules/custom/tp_pivot_finder/js/channelfinder-resize.js
@@ -0,0 +1,7 @@
+function  kpiCFheightV2(ht) {
+    //toggle the div visibility
+    var  frameFoo = document.getElementById('kpiFrameV2');
+    var  divFoo = document.getElementById('kpiDivV2');
+    divFoo.style.height = ht;
+    frameFoo.style.height = ht;
+}
