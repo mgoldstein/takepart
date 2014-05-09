1562ea99978136bcea5ecad8a286befdd7a62916
Scott Knight <sknight17@gmail.com>
2014-05-08 18:25:15 -0700

    TP-1874

--- drupal/sites/all/themes/tp4/templates/block--bean--tpl-live-long.tpl.php
+++ drupal/sites/all/themes/tp4/templates/block--bean--tpl-live-long.tpl.php
@@ -1,40 +1,20 @@
 <?php
 /**
  * @file
- * Default theme implementation for beans.
+ * Returns the HTML for a block.
  *
- * Available variables:
- * - $content: An array of comment items. Use render($content) to print them all, or
- *   print a subset such as render($content['field_example']). Use
- *   hide($content['field_example']) to temporarily suppress the printing of a
- *   given element.
- * - $title: The (sanitized) entity label.
- * - $url: Direct url of the current entity if specified.
- * - $page: Flag for the full page state.
- * - $classes: String of classes that can be used to style contextually through
- *   CSS. It can be manipulated through the variable $classes_array from
- *   preprocess functions. By default the following classes are available, where
- *   the parts enclosed by {} are replaced by the appropriate values:
- *   - entity-{ENTITY_TYPE}
- *   - {ENTITY_TYPE}-{BUNDLE}
- *
- * Other variables:
- * - $classes_array: Array of html class attribute values. It is flattened
- *   into a string within the variable $classes.
- *
- * @see template_preprocess()
- * @see template_preprocess_entity()
- * @see template_process()
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728246
  */
 ?>
-<div class="<?php print 'tpl-block '. $classes; ?> clearfix"<?php print $attributes; ?>>
-  <h2 class="block__title"> TakePart Live </h2>
-  <div class="content"<?php print $content_attributes; ?>>
-    <?php
-      print render($content);
-    ?>
-  </div>
-  <div class="find-pivot">
-    <?php print l('', '//find.pivot.tv'); ?>
-  </div>
+<div id="<?php print $block_html_id; ?>" class="tpl-block <?php print $classes; ?>"<?php print $attributes; ?>>
+  <?php
+  $output = '';
+  $output .= render($title_prefix);
+  $output .= '<h2 class="block__title"> TakePart Live </h2>';
+  $output .= render($title_suffix);
+  print $output;
+  print $content;
+  ?>
+
 </div>
