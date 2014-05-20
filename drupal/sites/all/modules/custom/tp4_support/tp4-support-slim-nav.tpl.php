ef2c917559d8f64f9e8765729397b36e4f1aae2d
Matthew Wrather <matt@wrathercreative.com>
2014-05-19 19:15:53 -0700

    retheme slimnav

--- drupal/sites/all/modules/custom/tp4_support/tp4-support-slim-nav.tpl.php
+++ drupal/sites/all/modules/custom/tp4_support/tp4-support-slim-nav.tpl.php
@@ -1,50 +1,21 @@
-<div class="menu-toggle">
-</div>
+<div class="slim-nav-inner clearfix">
+<div class="menu-toggle"></div>
 <div class="left">
   <div class="logo">
-    <?php $logo = '<img src="'. $logo. '">'; ?>
-    <?php 
-    global $base_url;
-    if (arg(0) === 'iframes') {
-        $link_url = str_replace('https://', 'http://', $base_url);
-    } else {
-        $link_url = $base_url;
-    }
-    print l($logo, $link_url, array('html' => true)); 
-    ?>
+    <?php print $logo; ?>
   </div>
   <nav id="main-menu">
-    <?php 
-    if (arg(0) === 'iframes') {
-      print str_replace('https://', 'http://', $slimnav); 
-    } else {
-      print $slimnav;
-    }
-    ?>
+    <?php print $slimnav; ?>
   </nav>
 </div>
 <div class="right">
   <div class="follow-us">
-    <?php print drupal_render(menu_tree('menu-social-header-follow')); ?>
+    <h2 class="follow-headline"><?php print t("Follow"); ?></h2>
+    <?php print render($social_menu); ?>
   </div>
   <div class="user-menu">
-    <ul>
-      <?php 
-      $sign = implode($user_links);
-      if (arg(0) === 'iframes') {
-          print str_replace('https://', 'http://', $sign);
-      }
-      else {
-          print $sign;
-      }
-      ?>
-    </ul>
+    <?php print render($user_links); ?>
   </div>
-  <?php if(arg(0) != 'iframes'): ?>
-  <div class="search">
-    <div class="search-toggle"></div>
-    <?php print drupal_render(module_invoke('search_api_page', 'block_view', '2')); ?>
-  </div>
-  <?php endif; ?>
+  <?php print render($search); ?>
 </div>
-<div class="clearfix"></div>
+</div>
