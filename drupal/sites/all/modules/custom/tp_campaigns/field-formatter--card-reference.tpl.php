5c6512e95ac7ded3cd9702315d565fa285f75344
Scott Knight <sknight17@gmail.com>
2014-05-19 18:53:28 -0700

    cleaned up card references to better be able to add custom styling classes

--- drupal/sites/all/modules/custom/tp_campaigns/field-formatter--card-reference.tpl.php
+++ drupal/sites/all/modules/custom/tp_campaigns/field-formatter--card-reference.tpl.php
@@ -1,67 +1,12 @@
-
-<?php
-  // Does the Tray title exist?
-  if(isset($variables['entity']->field_campaign_tray_title['und'][0]['value']) == TRUE) {
-    $tray_title = $variables['entity']->field_campaign_tray_title['und'][0]['value'];
-  }
-
-?>
-<div class="swipe-wrap">
-<?php
-  foreach ($items as $key => $item) {
-    $node = node_load($item['target_id']);
-
-    //Set the background of the card
-    //If Tray contains a background image, set all other slides to transparent.
-    $background_image = $node->field_campaign_background['und'][0]['uri'];
-    $background_image = file_create_url($background_image);
-
-    //If a tray has a background we make the card transparent and set the background on the slider level.
-    $card_styles = '';
-    if(isset($variables['entity']->field_campaign_background) ==  true && $variables['entity']->field_campaign_background != NULL){
-      $card_styles = 'style="background-image: none; background-color: transparent; background-size: 980px;"';
-
-    }
-    else{
-      // $card_styles = 'style="background-image: url(\''. $background_image. '\');"';
-    }
-
-
-    //Card classes in the CSS control background properties.
-    $card_classes = array();
-    if(isset($tray_title) == true){
-      $card_classes[] = 'has-tray-title';
-    }
-    if(isset($tray_background) == true){
-      $card_classes[] = 'has-tray-background';
-    }
-    if(isset($node->field_campaign_card_title['und'][0]['value']) == true){
-      $card_classes[] = 'has-card-title';
-    }
-
-    $card_classes[] = $node->type;
-
-    print '<div class="card-wrapper '. implode(' ', $card_classes). '" '. $card_styles. '>';
-    print drupal_render(node_view($node, 'full', NULL));
-    print '</div>';
-  }
-?>
+<div class="swipe-wrap  <?php $tray_vars['tray_classes']; ?>">
+  <?php print implode('', $tray_vars['cards']); ?>
 </div>
-<?php
-    if(isset($variables['entity']->field_campaign_anchor_tag['und'][0]['value']) == true){
-      $title = strtolower($variables['entity']->field_campaign_anchor_tag['und'][0]['value']);
-      $title = preg_replace("/[^a-z0-9_\s-]/", "", $title);
-      $title = preg_replace("/[\s-]+/", " ", $title);
-      $title = preg_replace("/[\s_]/", "-", $title);
-    }
-?>
+<a class="card-anchor" id="<?php print $tray_vars['anchor_tag']; ?>"></a>
 
-<?php print '<a class="card-anchor" id="'. $title. '"></a>'; ?>
+
 <?php // Print arrows is multiple cards exist in the card ?>
-
-
-
-<?php if(isset($variables['entity']->field_campaign_card_reference[1]) == true || isset($variables['entity']->field_campaign_card_reference['und'][1]) == true): ?>
+<?php $reference = field_get_items('node', $variables['entity'], 'field_campaign_card_reference'); ?>
+<?php if(!empty($reference) && count($reference) > 1): ?>
   <div class="tray-header">
 
     <nav class="slider-pagination">
@@ -77,14 +22,10 @@
       <div class="mobile-arrow arrow-right"></div>
     </nav>
 
-    <?php if(isset($tray_title) == true): ?>
-      <h1 class="card-title"><?php print $tray_title; ?></h1>
+    <?php if(isset($tray_vars['tray_title'])): ?>
+      <h1 class="card-title"><?php print $tray_vars['tray_title']; ?></h1>
     <?php endif; ?>
   </div>
-<?php endif; ?>
-
-<?php // Print arrows is multiple cards exist in the card ?>
-<?php if(isset($variables['entity']->field_campaign_card_reference['und'][1]) == true): ?>
   <nav class="slider-nav">
     <div class="slider-inner">
       <div<?php print drupal_attributes(array(
@@ -97,21 +38,8 @@
         ><div class="arrow-inner"></div></div>
     </div>
   </nav>
-<?php elseif(isset($tray_title) == true): ?>
-	
-<?php $node = node_load($variables['entity']->field_campaign_card_reference['und'][0]['target_id']); ?>
-
-<?php if($node->field_slim_card_text['und'][0]['value'] == 1){} // Dont display card title if slim text
-else { ?>
+<?php elseif(isset($tray_vars['tray_title']) == true): ?>
   <div class="tray-header">
-    <h1 class="card-title"><?php print $tray_title; ?></h1>
+    <h1 class="card-title"><?php print $tray_vars['tray_title']; ?></h1>
   </div>
-<?php } ?>
-  
-  
 <?php endif; ?>
-
-
-
-
-
