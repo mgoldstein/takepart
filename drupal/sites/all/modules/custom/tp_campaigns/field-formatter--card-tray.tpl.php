b60f7746ef79fa9c262c69c9dd98db0dba120a06
Scott Knight <sknight17@gmail.com>
2014-05-20 12:22:40 -0700

    changed style field to be a text list instead of an integer

--- drupal/sites/all/modules/custom/tp_campaigns/field-formatter--card-tray.tpl.php
+++ drupal/sites/all/modules/custom/tp_campaigns/field-formatter--card-tray.tpl.php
@@ -4,20 +4,21 @@
     //If a tray has a background we make the card transparent and set the background on the slider level.
     $slider_styles = array();
     $slider_class = array('swipe', 'on-first-slide');
+    if($items = field_get_items('node', $node, 'field_campaign_style')){
+      $slider_class[] = drupal_render(field_view_value('node', $node, 'field_campaign_style', $items[0]));
+    }
 
-    if(isset($node->field_campaign_bgw_image['und'][0]['value']) == TRUE) {
-      $bg_width_img = $node->field_campaign_bgw_image['und'][0]['value'];
-    }
-    if(isset($node->field_campaign_bg_color['und'][0]['rgb']) == TRUE) {
-      $bg_color = $node->field_campaign_bg_color['und'][0]['rgb'];
-      $slider_styles[] = 'background-color: '. $bg_color. ';';
-    }
-    if(isset($bg_width_img) == true){
+    if($items = field_get_items('node', $node, 'field_campaign_bgw_image')){
+      $bg_width_img = drupal_render(field_view_value('node', $node, 'field_campaign_bgw_image', $items[0]));
       $slider_class[] = ($bg_width_img == 0 ? 'tray-img-width-full' : 'tray-img-width-1000');
     }
+    if($items = field_get_items('node', $node, 'field_campaign_bg_color')){
+      $bg_color = $items[0]['rgb'];
+      $slider_styles[] = 'background-color: '. $bg_color. ';';
+    }
 
-    if(isset($node->field_campaign_background) ==  true && $node->field_campaign_background != NULL){
-      $background_image = $node->field_campaign_background['und'][0]['uri'];
+    if($items = field_get_items('node', $node, 'field_campaign_background')){
+      $background_image = $items[0]['uri'];
       $background_image = file_create_url($background_image);
       $slider_styles[] = 'background-image: url(\''. $background_image. '\');';
       $slider_class[] = 'has-tray-background';
@@ -25,7 +26,9 @@
     else{
       $slider_class[] = '';
     }
-    if(isset($node->field_campaign_card_reference['und'][1]) == true){
+    $references = field_get_items('node', $node, 'field_campaign_card_reference');
+
+    if(!empty($references) && count($references) > 1){
       $slider_class[] = 'has-multiple-cards';
       $slider_class[] = 'slider';
     }
