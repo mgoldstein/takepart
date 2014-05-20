c261a1fbd0d2ce86138fa8f7ea3ae01c04e47041
Matthew Wrather <matt@wrathercreative.com>
2014-05-20 13:48:23 -0700

    Refine theming of slim-nav template files

--- drupal/sites/all/themes/tp4/templates/tp4-support-slim-nav.tpl.php
+++ drupal/sites/all/themes/tp4/templates/tp4-support-slim-nav.tpl.php
@@ -1,3 +1,4 @@
+<div class="slim-nav-inner clearfix">
 <div class="menu-toggle"></div>
 <div class="left">
   <div class="logo">
@@ -9,6 +10,7 @@
 </div>
 <div class="right">
   <div class="follow-us">
+    <h2 class="follow-headline"><?php print t("Follow"); ?></h2>
     <?php print render($social_menu); ?>
   </div>
   <div class="user-menu">
@@ -16,4 +18,4 @@
   </div>
   <?php print render($search); ?>
 </div>
-<div class="clearfix"></div>
+</div>
