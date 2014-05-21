e3b4df5fd66cce951de2fa7e4c8c0c6de1f245b3
Matthew Wrather <matt@wrathercreative.com>
2014-05-20 18:29:34 -0700

    add header to snap drawer

---
+++ drupal/sites/all/modules/custom/tp4_support/tp4-support-mobile-menu-header.tpl.php
@@ -0,0 +1,7 @@
+<div class="user-menu">
+  <?php print render($user_links); ?>
+</div>
+<div class="follow-us">
+  <h2><?php print t('Follow'); ?></h2>
+  <?php print render($social_menu); ?>
+</div>
