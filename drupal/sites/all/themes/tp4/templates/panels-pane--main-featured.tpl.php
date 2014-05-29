b67bddaf257cced6f44c8decd6674cddb7e0bb69
Marc Goldstein <mgoldstein@takepart.com>
2014-05-29 13:22:43 -0700

    Logic for different subhead field names

--- drupal/sites/all/themes/tp4/templates/panels-pane--main-featured.tpl.php
+++ drupal/sites/all/themes/tp4/templates/panels-pane--main-featured.tpl.php
@@ -19,55 +19,58 @@
 $headline = !empty($content['#node']->field_promo_headline) ? $content['#node']->field_promo_headline['und'][0]['safe_value'] : $title;
 ?>
 <?php if ($pane_prefix): ?>
-  <?php print $pane_prefix; ?>
+    <?php print $pane_prefix; ?>
 <?php endif; ?>
 <div class="<?php print $classes; ?>" <?php print $id; ?>>
-  <?php if ($admin_links): ?>
-    <?php print $admin_links; ?>
-  <?php endif; ?>
+    <?php if ($admin_links): ?>
+        <?php print $admin_links; ?>
+    <?php endif; ?>
 
     <header class="entry-header">
-      <?php if (!empty($variables['content']['field_video'])) {
-        print render($variables['content']['field_video']);
-      }
-      else {
-        print render($variables['content']['field_thumbnail']);
-      } ?>
-      <?php print render($title_prefix); ?>
-      <?php 
+        <?php
+        if (!empty($variables['content']['field_video'])) {
+            print render($variables['content']['field_video']);
+        } else {
+            print render($variables['content']['field_thumbnail']);
+        }
+        ?>
+        <?php print render($title_prefix); ?>
+        <?php
         if ($headline) {
-          if (!empty($title_link)) {
-            $title_heading = '<a href="' . $title_link . '">' . $headline . '</a>';
-          }
-          else {
-            $title_heading = $headline;
-          }
-        print '<h2' . $title_attributes . '>' . $title_heading . '</h2>';
-        } 
-      ?>
-      <?php print render($title_suffix); ?>
-      <?php if ($unpublished): ?>
-        <mark class="unpublished"><?php print t('Unpublished'); ?></mark>
-      <?php endif; ?>
+            if (!empty($title_link)) {
+                $title_heading = '<a href="' . $title_link . '">' . $headline . '</a>';
+            } else {
+                $title_heading = $headline;
+            }
+            print '<h2' . $title_attributes . '>' . $title_heading . '</h2>';
+        }
+        ?>
+        <?php print render($title_suffix); ?>
+        <?php if ($unpublished): ?>
+            <mark class="unpublished"><?php print t('Unpublished'); ?></mark>
+            <?php endif; ?>
     </header>
 
     <?php
-      hide($variables['content']['comments']);
-      hide($variables['content']['links']);
-      $tab_title_override = variable_get('main_feature_tab_title_override', NULL);
-      if($tab_title_override != NULL){
+    hide($variables['content']['comments']);
+    hide($variables['content']['links']);
+    $tab_title_override = variable_get('main_feature_tab_title_override', NULL);
+    if ($tab_title_override != NULL) {
         $variables['content']['field_tab_action_override'][0]['#title'] = $tab_title_override;
-      }
-      
-      if (!empty($title_link)) {
+    }
+
+    if (!empty($title_link) && !empty($variables['content']['field_article_subhead'][0]['#markup'])) {
         $variables['content']['field_article_subhead'][0]['#markup'] .= ' <span id="mf-more-link-bullet"> </span><a id="mf-more-link" href="' . $title_link . '">more</a>';
-      }
+    } elseif (!empty($title_link) && !empty($variables['content']['field_subhead'][0]['#markup'])) {
+        $variables['content']['field_subhead'][0]['#markup'] .= ' <span id="mf-more-link-bullet"> </span><a id="mf-more-link" href="' . $title_link . '">more</a>';
+    }
     ?>
     <?php
-      // We hide the comments and links now so that we can render them later.
-      hide($variables['content']['comments']);
-      hide($variables['content']['links']);
-      print render($variables['content']['field_author']);
-      print render($variables['content']);
+    // We hide the comments and links now so that we can render them later.
+    hide($variables['content']['comments']);
+    hide($variables['content']['links']);
+    print render($variables['content']['field_author']);
+    print render($variables['content']);
+    dd($variables['content']);
     ?>
 </div>
