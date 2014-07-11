d3401802e0b79ddfd3dd12e201e950be1d2a8e54
jpedius <jp@takepart.com>
2014-07-11 16:53:55 -0700

    TPS-265: Add strategies

---
+++ app/src/TakePart/DigitalData/Strategies/Content.php
@@ -0,0 +1,44 @@
+<?php
+namespace TakePart\DigitalData\Strategies;
+
+class Content extends DefaultPage {
+
+  public static function claim($path, $page) {
+    // Claim node pages.
+    return preg_match('/^node\/[0-9]+/', $path);
+  }
+
+  protected function categoryForContentType($type) {
+    $categories = array(
+      'openpublish_article' => 'Article',
+      'campaign_page' => 'Campaign',
+      'page' => 'Page',
+    );
+    return array_key_exists($type, $categories) ? $categories[$type] : $type;
+  }
+
+  public function build($page) {
+
+    $node = menu_get_object();
+
+    $data = parent::build($page);
+
+    $pageID = "node/{$node->nid}";
+
+    $data += array(
+      'pageInstanceID' => ENVIRONMENT . ': ' . $pageID,
+      'page' => array(
+        'pageInfo' => array(
+          'pageID' => $pageID,
+          'pageName' => $node->title,
+          'destinationURL' => url($pageID, array('absolute' => TRUE)),
+        ),
+        'category' => array(
+          'primaryCategory' => $this->categoryForContentType($node->type),
+        ),
+      ),
+    );
+
+    return $data;
+  }
+}
