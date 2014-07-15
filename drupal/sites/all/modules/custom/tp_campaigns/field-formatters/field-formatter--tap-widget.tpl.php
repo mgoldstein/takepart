14d1b0f0d262c7c25989a20f2c8cbb26b79bcd11
Marc Goldstein <mgoldstein@takepart.com>
2014-07-15 13:54:25 -0700

    TP-2096 - TAP card spacing

--- drupal/sites/all/modules/custom/tp_campaigns/field-formatters/field-formatter--tap-widget.tpl.php
+++ drupal/sites/all/modules/custom/tp_campaigns/field-formatters/field-formatter--tap-widget.tpl.php
@@ -1,5 +1,4 @@
 <p class="takepart-take-action-widget-wysiwyg-wrapper">&nbsp;</p>
 <div class="takepart-take-action-widget" data-action-id="<?php print $tap_id; ?>" data-action-title="" data-form-style="<?php print $widget_exposed; ?>">
-	&nbsp;
 </div>
 <p class="takepart-take-action-widget-wysiwyg-wrapper">&nbsp;</p>
