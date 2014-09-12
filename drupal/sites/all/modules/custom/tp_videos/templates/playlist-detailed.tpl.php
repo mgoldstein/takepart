365f426386409a3d9d75c2c8c4a7cfb2a7d349d3
Scott Knight <sknight17@gmail.com>
2014-09-12 16:41:34 -0700

    added javascript to handle video selection and updated layout

--- drupal/sites/all/modules/custom/tp_videos/templates/playlist-detailed.tpl.php
+++ drupal/sites/all/modules/custom/tp_videos/templates/playlist-detailed.tpl.php
@@ -3,11 +3,18 @@
  * Template for the Playlist DETAILED View Mode
  */
 ?>
-DETAILED VIEW MODE
-<div class="video">
-	<div class="video-placeholder"></div>
-	<div class="description"><p>This is where the description will go.  This is static for now</p></div>
-</div>
-<nav>
+<div class="playlist" data-playlist-type="detailed">
+	<div class="current-video">
+		<div class="video-placeholder"></div>
+		<div class="video-description">
+			<?php foreach($variables['videos'] as $key => $video): ?>
+				<div class="description-item" data-video-description="<?php print $key; ?>">
+					<h2 class="title"><?php print $video->title; ?></h2>
+					<?php $subhead = field_get_items('node', $video, 'field_article_subhead'); ?>
+					<?php print drupal_render(field_view_field('node', $video, 'field_article_subhead', $subhead)); ?>
+				</div>
+			<?php endforeach; ?>
+		</div>
+	</div>
 	<?php print $variables['navigation']; ?>
-</nav>
+</div>
--- drupal/sites/all/modules/custom/tp_videos/templates/playlist-hidelist.tpl.php
+++ drupal/sites/all/modules/custom/tp_videos/templates/playlist-hidelist.tpl.php
@@ -3,7 +3,8 @@
  * Template for the Playlist hidelist View Mode
  */
 ?>
-HIDELIST VIEW MODE
-<div class="video">
-	<div class="video-placeholder"></div>
+<div class="playlist" data-playlist-type="hidelist">
+	<div class="video">
+		<div class="video-placeholder"></div>
+	</div>
 </div>
