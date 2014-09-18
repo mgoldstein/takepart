71c66d6e0a0a32cd0b4798fd4b6f93614d458629
jpedius <jp@takepart.com>
2014-09-18 10:10:59 -0700

    TP-2114: Render video inline replacement field directly with additional overrides

--- drupal/sites/all/modules/custom/tp_video_player/classes/TakePartVideoPlayerNodeReferenceFieldBuilder.php
+++ drupal/sites/all/modules/custom/tp_video_player/classes/TakePartVideoPlayerNodeReferenceFieldBuilder.php
@@ -14,11 +14,71 @@ class TakePartVideoPlayerNodeReferenceFieldBuilder {
     $this->items = $items;
   }
 
+  private function getVideoNode() {
+    $items = field_get_items($this->entity_type, $this->entity, 'field_ic_video', $this->langcode);
+    if ($items !== FALSE && count($items) > 0) {
+      $item = reset($items);
+      return node_load($item['nid']);
+    }
+    return NULL;
+  }
+
   public function settings($global_default) {
+
+    $controller = new TakePartVideoPlayerOverrideController();
+
+    // Load the global defaults for the entity view mode.
+    $defaults = $controller->loadByName($global_default);
+
+    $configurations = array($defaults);
+
+    $node = $this->getVideoNode();
+    if (!is_null($node)) {
+      $node_configuration = $controller->loadOverrideForEntity('node',
+        $node->nid, 'full_page');
+      if (!is_null($node_configuration)) {
+        $configurations[] = $node_configuration;
+      }
+    }
+
+    // Get the configuration overrides for full page nodes.
+    $override = $controller->loadOverrideForEntity($this->entity_type,
+      entity_id($this->entity_type, $this->entity), 'inline_content');
+    if (!is_null($override)) {
+      $configurations[] = $override;
+    }
+
+    // Merge the defaults with the overrides.
+    $configuration = $controller->merge($configurations);
+
+    // The configuration can contain tokens, wrap it in a token resolver object.
+    $resolved_configuration =  new TakePartTokenizedObject($configuration,
+      array($this->entity_type => $this->entity),
+      array('language' => $this->langcode));
+
+    $items = field_get_items('node', $node, 'field_video');
+    if ($items !== FALSE && count($items) > 0) {
+      $settings_builder = new TakePartVideoPlayerSettingsBuilder($resolved_configuration);
+      return $settings_builder->build(reset($items));
+    }
     return array();
   }
 
   public function allowedRegions() {
-    return array();
+    $regions = array();
+    $node = $this->getVideoNode();
+    if (!empty($node)) {
+      $items = field_get_items('node', $node, 'field_allowed_regions');
+      if ($items !== FALSE && count($items) > 0) {
+        foreach ($items as $item) {
+          $value = str_replace(array(','), ' ', $item['value']);
+          $chunks = explode(' ', $value);
+          $trimmed = array_map('trim', $chunks);
+          $list = array_filter($trimmed, 'strlen');
+          $regions += array_map('strtolower', $list);
+        }
+      }
+    }
+    return $regions;
   }
 }
