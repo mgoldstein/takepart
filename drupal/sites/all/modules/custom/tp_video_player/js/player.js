dcdb0d9a3d36e6da986c30091c94bb98c5c811a0
jpedius <jp@takepart.com>
2014-09-22 13:25:03 -0700

    TP-2156: Limit player field formatter to file fields, move allowed regions to settings hash

--- drupal/sites/all/modules/custom/tp_video_player/js/player.js
+++ drupal/sites/all/modules/custom/tp_video_player/js/player.js
@@ -8,33 +8,8 @@
         if (!window.s || !window.s.Media) {
           delete settings['sitecatalyst'];
         }
-        var attribute = $(element).attr('data-allowed-regions');
-        var regions = attribute ? attribute.split(',') : [];
-        if (regions.length > 0) {
-          var blockVideo = function() {
-            $(element).addClass('blocked').removeClass('loading');
-          };
-          var handleResponse = function(response) {
-            if (!response.country || !response.country.iso_code) {
-              $(element).addClass('blocked').removeClass('loading');
-              return false;
-            }
-            var code = response.country.iso_code.toLowerCase();
-            if ($.inArray(code, regions) < 0) {
-              $(element).addClass('blocked').removeClass('loading');
-            }
-            else {
-              $(element).removeClass('loading');
-              jwplayer.key = Drupal.settings.tp_video_player.key;
-              jwplayer(element).setup(settings);
-            }
-          };
-          geoip2.country(handleResponse, blockVideo);
-        }
-        else {
-          jwplayer.key = Drupal.settings.tp_video_player.key;
-          jwplayer(element).setup(settings);
-        }
+        jwplayer.key = Drupal.settings.tp_video_player.key;
+        jwplayer(element).setup(settings);
       });
     }
   };
