1a1583bbe4030407fdbdfb5fbf9501850c795be8
Scott Knight <sknight17@gmail.com>
2014-09-24 13:30:22 -0700

    added title on-off switch for playlists

--- drupal/sites/all/modules/custom/tp_videos/templates/playlist-wrapper.tpl.php
+++ drupal/sites/all/modules/custom/tp_videos/templates/playlist-wrapper.tpl.php
@@ -1,4 +1,6 @@
 <div class="playlist-wrapper">
-	<h2 class="playlist-title"><?php print $title; ?></h2>
+	<?php if($title != null): ?> </php>
+		<h2 class="playlist-title"><?php print $title; ?></h2>
+	<?php endif; ?>
   <?php print render($playlist); ?>
 </div>
