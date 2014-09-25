18506edef5cb2081e3a0302632edd1dcb4d15e78
Scott Knight <sknight17@gmail.com>
2014-09-24 17:29:08 -0700

    support for multiple playlists on a page

--- drupal/sites/all/modules/custom/tp_videos/js/video-playlist.js
+++ drupal/sites/all/modules/custom/tp_videos/js/video-playlist.js
@@ -15,16 +15,18 @@
 
           window['currentVideo_' + index] = 0;
           updateVideo(window['currentVideo_' + index], $(this));
-          var playlist = $(this);
 
-          jwplayer(index).onComplete(function(event) {
+          var playlist = $(this);
+          var element = playlist.find('.jwplayer').attr('id');
+
+          jwplayer(element).onComplete(function(event) {
 
             window.videoTransition; //hack for jwplayer firing twice onPlay and onComplete
             if(null == window.videoTransition){
               window['currentVideo_' + index] = window['currentVideo_' + index] + 1;
               var file = Drupal.settings.tp_video_player.settings['tp-video-player'].playlist[0].sources[window['currentVideo_' + index]].file;
-              jwplayer(index).load([{file: file}]);
-              jwplayer(index).play();
+              jwplayer(element).load([{file: file}]);
+              jwplayer(element).play();
 
               playlist.find('.video-description .description-item').removeClass('active');
               playlist.find('ul.video-playlist .video-item').removeClass('active');
@@ -33,19 +35,20 @@
             }
 
           });
-          jwplayer(index).onPlay(function(event){
+
+          jwplayer(element).onPlay(function(event){
             delete window.videoTransition;
 
             /* Analytics */
-            var autoplay = jwplayer(index).config.autostart;
+            var autoplay = jwplayer(element).config.autostart;
             if(autoplay == true){
               autoplay = 'Auto-play';
             }else{
               autoplay = 'Manual';
             }
             takepart.analytics.track('playlist-play', {
-              playerName: jwplayer(index).config.primary,
-              listName: jwplayer(index).config.title,
+              playerName: jwplayer(element).config.primary,
+              listName: jwplayer(element).config.title,
               playConfig: autoplay
             });
           });
@@ -55,13 +58,15 @@
         /* Navigation */
         $('.video-item').click(function(){
           var playlist = $(this).parents('.playlist');
+          var element = playlist.find('.jwplayer').attr('id');
           var index = playlist.attr('playlist-id');
           window['currentVideo_' + index] = $(this).data('video-number');
           playlist.find('.video-description .description-item').removeClass('active');
           playlist.find('ul.video-playlist .video-item').removeClass('active');
           var file = Drupal.settings.tp_video_player.settings['tp-video-player'].playlist[index].sources[window['currentVideo_' + index]].file;
-          jwplayer().load([{file: file}]);
-          jwplayer().play();
+
+          jwplayer(element).load([{file: file}]);
+          jwplayer(element).play();
 
           updateVideo(window['currentVideo_' + index], playlist);
 
