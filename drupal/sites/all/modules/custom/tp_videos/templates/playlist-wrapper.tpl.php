e92a8d9f7218cba3f26083678eff498f0bddf3aa
Scott Knight <sknight17@gmail.com>
2014-10-01 11:45:45 -0700

    added controls back for inline replacements

--- drupal/sites/all/modules/custom/tp_videos/templates/playlist-wrapper.tpl.php
+++ drupal/sites/all/modules/custom/tp_videos/templates/playlist-wrapper.tpl.php
@@ -1,6 +1,8 @@
-<div class="playlist-wrapper">
-	<?php if($title != null): ?>
-		<h2 class="playlist-title"><?php print $title; ?></h2>
-	<?php endif; ?>
-  <?php print render($playlist); ?>
+<div class="playlist-wrapper-wrapper">
+	<div class="playlist-wrapper">
+		<?php if($title != null): ?>
+			<h2 class="playlist-title"><?php print $title; ?></h2>
+		<?php endif; ?>
+	  <?php print render($playlist); ?>
+	</div>
 </div>
