d621c96518253c707cee653f4b689e5da24dcc1d
Scott Knight <sknight17@gmail.com>
2014-10-01 18:11:50 -0700

    Revert "commit"
    This reverts commit 3bb9dc2182058a9eb641472976368d5e7b822189.

--- drupal/sites/all/modules/custom/tp_video_player/js/player.js
+++ drupal/sites/all/modules/custom/tp_video_player/js/player.js
@@ -30,60 +30,6 @@
           settings['primary'] = 'flash';
         }
 
-        //moved code from pm-jwplayer over to new player.js
-        var regions = Drupal.settings.tp_video_player.settings[element_id].allowed_regions[0];
-        if (regions.length > 0 && !$('body').hasClass('node-type-video-playlist')) {
-          var blockVideo = function() {
-            $(element).addClass('blocked').removeClass('loading');
-          };
-          var handleResponse = function(response) {
-            if (!response.country || !response.country.iso_code) {
-              $(element).addClass('blocked').removeClass('loading');
-              return false;
-            }
-            var code = response.country.iso_code.toLowerCase();
-            if ($.inArray(code, regions) < 0) {
-              $(element).addClass('blocked').removeClass('loading');
-            }
-            else {
-              $(element).removeClass('loading');
-              jwplayer.key = Drupal.settings.tp_video_player.key;
-              jwplayer(element).setup(settings);
-            }
-          };
-          geoip2.country(handleResponse, blockVideo);
-        }
-        else {
-          jwplayer.key = Drupal.settings.tp_video_player.key;
-          var allowed = Drupal.settings.tp_video_player.settings[element_id]['allowed_regions'][0];
-    
-          //calls the country to get the code
-          geoip2.country(function(response) {
-            window['tp_client_location'] = response.country.iso_code;
-            tp_video_blocked(element_id, 0);
-            
-            //unsets first ad if blocked
-            if ($('#' + element_id).hasClass('blocked')) {
-              delete settings.playlist[0]['adschedule'];
-            }
-          });
-
-          jwplayer(element).setup(settings);
-          
-          //this is used for the playlist to hide the items
-          jwplayer(element_id).onReady(function() {
-            var playlist_index = jwplayer(element_id).getPlaylistIndex();
-            tp_video_blocked(element_id, playlist_index);
-          });
-          
-          //stops the player if it has blocked class
-          jwplayer(element_id).onPlay(function() {
-            if ($('#' + element_id).hasClass('blocked')) {
-              jwplayer(element_id).stop();
-            }
-          });
-        }
-
         var playlist = $(this);
         jwplayer(element_id).onComplete(function(event) {
 
@@ -137,6 +83,60 @@
           else if(playlistType == 'basic'){
             playlist.find('ul.video-playlist .video-item[data-video-number="' + current_video + '"]').addClass('active');
           }
+        }
+
+        //moved code from pm-jwplayer over to new player.js
+        var regions = Drupal.settings.tp_video_player.settings[element_id].allowed_regions[0];
+        if (regions.length > 0 && !$('body').hasClass('node-type-video-playlist')) {
+          var blockVideo = function() {
+            $(element).addClass('blocked').removeClass('loading');
+          };
+          var handleResponse = function(response) {
+            if (!response.country || !response.country.iso_code) {
+              $(element).addClass('blocked').removeClass('loading');
+              return false;
+            }
+            var code = response.country.iso_code.toLowerCase();
+            if ($.inArray(code, regions) < 0) {
+              $(element).addClass('blocked').removeClass('loading');
+            }
+            else {
+              $(element).removeClass('loading');
+              jwplayer.key = Drupal.settings.tp_video_player.key;
+              jwplayer(element).setup(settings);
+            }
+          };
+          geoip2.country(handleResponse, blockVideo);
+        }
+        else {
+          jwplayer.key = Drupal.settings.tp_video_player.key;
+          var allowed = Drupal.settings.tp_video_player.settings[element_id]['allowed_regions'][0];
+    
+          //calls the country to get the code
+          geoip2.country(function(response) {
+            window['tp_client_location'] = response.country.iso_code;
+            tp_video_blocked(element_id, 0);
+            
+            //unsets first ad if blocked
+            if ($('#' + element_id).hasClass('blocked')) {
+              delete settings.playlist[0]['adschedule'];
+            }
+          });
+
+          jwplayer(element).setup(settings);
+          
+          //this is used for the playlist to hide the items
+          jwplayer(element_id).onReady(function() {
+            var playlist_index = jwplayer(element_id).getPlaylistIndex();
+            tp_video_blocked(element_id, playlist_index);
+          });
+          
+          //stops the player if it has blocked class
+          jwplayer(element_id).onPlay(function() {
+            if ($('#' + element_id).hasClass('blocked')) {
+              jwplayer(element_id).stop();
+            }
+          });
         }
       });
     }
