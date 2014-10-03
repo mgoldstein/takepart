2f3f11f37387543580249992ef753cf3d0417a0a
Scott Knight <sknight17@gmail.com>
2014-10-02 17:27:15 -0700

    deleted lines in video-playlist again

--- drupal/sites/all/modules/custom/tp_videos/js/video-playlist.js
+++ drupal/sites/all/modules/custom/tp_videos/js/video-playlist.js
@@ -12,53 +12,6 @@
           window['currentVideo_' + index] = 0;
           updateVideo(window['currentVideo_' + index], $(this));
 
-          var playlist = $(this);
-          var element = playlist.find('.jwplayer').attr('id');
-
-          jwplayer(element).onComplete(function(event) {
-          
-          
-            window.videoTransition; //hack for jwplayer firing twice onPlay and onComplete
-            if(null == window.videoTransition){
-              window['currentVideo_' + index] = window['currentVideo_' + index] + 1;
-              jwplayer(element).playlistItem(window['currentVideo_' + index]);
-              playlist.find('.video-description .description-item').removeClass('active');
-              playlist.find('ul.video-playlist .video-item').removeClass('active');
-              updateVideo(window['currentVideo_' + index], playlist);
-              window.videoTransition = 'not null';
-            }
-
-            /* Move slider over if it needs to be */
-            var slides;
-            if(window['slider_' + index + '_view_mode'] == 'large'){
-              slides = 4;
-            }else{
-              slides = 3;
-            }
-            var ratio = (window['currentVideo_' + index])/slides;
-
-            if(ratio % 1 === 0){
-              window['slider_' + index].goToNextSlide();
-            }
-          });
-
-          jwplayer(element).onPlay(function(event){
-            delete window.videoTransition;
-
-            /* Analytics */
-            var autoplay = jwplayer(element).config.autostart;
-            if(autoplay == true){
-              autoplay = 'Auto-play';
-            }else{
-              autoplay = 'Manual';
-            }
-            takepart.analytics.track('playlist-play', {
-              playerName: jwplayer(element).config.primary,
-              listName: jwplayer(element).config.title,
-              playConfig: autoplay
-            });
-          });
-
         });
 
         /* Navigation */
