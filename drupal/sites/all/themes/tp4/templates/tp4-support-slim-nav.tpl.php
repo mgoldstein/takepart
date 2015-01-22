349f52176c7bbe3b1150a543621a68d2d81c7d00
Tim Heiderich <theiderich@takepart.com>
2015-01-21 19:45:30 -0800

    TP-2774 Made the header actually go all the way across the page. Re-added 'Follow' label.

--- drupal/sites/all/themes/tp4/templates/tp4-support-slim-nav.tpl.php
+++ drupal/sites/all/themes/tp4/templates/tp4-support-slim-nav.tpl.php
@@ -1,19 +1,21 @@
-<div class="menu-toggle"></div>
-<div class="left">
-  <div class="logo">
-    <?php print $logo; ?>
-  </div>
-  <nav id="main-menu">
-    <?php print $slimnav; ?>
-  </nav>
+<div class="slim-nav-inner clearfix">
+	<div class="menu-toggle"></div>
+	<div class="left">
+	  <div class="logo">
+	    <?php print $logo; ?>
+	  </div>
+	  <nav id="main-menu">
+	    <?php print $slimnav; ?>
+	  </nav>
+	</div>
+	<div class="right">
+	  <div class="follow-us">
+		<h2 class="follow-headline">Follow</h2>
+	    <?php print render($social_menu); ?>
+	  </div>
+	  <div class="user-menu">
+	    <div class="tpsLogin"></div>
+	  </div>
+	  <?php print render($search); ?>
+	</div>
 </div>
-<div class="right">
-  <div class="follow-us">
-    <?php print render($social_menu); ?>
-  </div>
-  <div class="user-menu">
-    <div class="tpsLogin"></div>
-  </div>
-  <?php print render($search); ?>
-</div>
-<div class="clearfix"></div>
