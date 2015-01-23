983168079f3e410ea3b78e7cbf30a15f80442cb3
Marc Goldstein <mgoldstein@takepart.com>
2015-01-23 15:03:22 -0800

    TP-2733 - remove sitecatalyst from iframes/footer

--- drupal/sites/all/themes/tp4/template.php
+++ drupal/sites/all/themes/tp4/template.php
@@ -74,7 +74,8 @@ function tp4_preprocess_html(&$variables, $hook) {
     // add jquery cookie library to tp4 pages
     drupal_add_library('system', 'jquery.cookie', true);
 
-    if (preg_match('/^\/entity_iframe/', $_SERVER['REQUEST_URI']) ) {
+    if (preg_match('/^\/entity_iframe/', $_SERVER['REQUEST_URI']) 
+            ||  preg_match('/^\/iframes/', $_SERVER['REQUEST_URI'])) {
         unset($variables['page']['page_bottom']['omniture']);
         unset($variables['page']['page_bottom']['quantcast']);
     }
