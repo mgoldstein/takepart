d77e588fc86827fdc0835188c35569e17ef4bef7
Tim Heiderich <theiderich@takepart.com>
2015-02-12 17:13:33 -0800

    TP-2902 Changed <?= tags to <?php tags in interstitial templates.

--- drupal/sites/all/modules/custom/pm_interstitial/theme/pm_interstitial_social_follows.tpl.php
+++ drupal/sites/all/modules/custom/pm_interstitial/theme/pm_interstitial_social_follows.tpl.php
@@ -1,23 +1,23 @@
 <header class="header">
 	<div>
-		<?= $image ?>
+		<?php $image ?>
 	</div>
 
 	<h1 class="headline">
-		<?= t($header) ?>
+		<?php t($header) ?>
 	</h1>
 </header>
 
 <div class="content">
 	<div class="body">
-		<?= t($body) ?>
+		<?php t($body) ?>
 	</div>
 
 	<div class="socials">
-	<?= $facebook."\n".$twitter; ?>
+	<?php $facebook."\n".$twitter; ?>
 	</div>
 
 	<p id="dont">
-		<a href="#"><?= t($do_not_show) ?></a>
+		<a href="#"><?php t($do_not_show) ?></a>
 	</p>
 </div>
