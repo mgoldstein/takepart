83c0670ab7d8f9e06aea25c6c7a78a060408a5a9
Marc Goldstein <mgoldstein@takepart.com>
2015-03-05 16:17:35 -0800

    TP-2644 - Remove uninstalled modules from code base

--- drupal/profiles/takepart/modules/takepart_addthis/js/exaddthis.js
+++
@@ -1,7 +0,0 @@
-var addthis_config = {"data_track_clickback":true};
-var pn = document.title;
-var shortname = pn.replace("TakePart - News, Culture, Videos and Photos That Make the World Better", "");
-var addthis_share = {
-  // templates: { twitter: 'check this out: {{title}}  {{url}} via @TakePart'}
-  templates: { twitter: '' + shortname + ' {{url}} via @TakePart'}
-}
