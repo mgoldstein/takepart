bed84f429e43b6644e79e751c1a8389bcf18f98c
Scott Knight <sknight17@gmail.com>
2015-03-18 11:31:08 -0700

    TP-2994: moved html template out of tp4

--- drupal/sites/all/themes/base/templates/html.tpl.php
+++ drupal/sites/all/themes/base/templates/html.tpl.php
@@ -8,13 +8,7 @@
  */
 ?>
 <!DOCTYPE html>
-<!--[if IEMobile 7]><html class="iem7" <?php print $html_attributes; ?>><![endif]-->
-<!--[if lte IE 6]><html class="lt-ie9 lt-ie8 lt-ie7" <?php print $html_attributes; ?>><![endif]-->
-<!--[if (IE 7)&(!IEMobile)]><html class="lt-ie9 lt-ie8" <?php print $html_attributes; ?>><![endif]-->
-<!--[if IE 8]><html class="lt-ie9" <?php print $html_attributes; ?>><![endif]-->
-<!--[if (gte IE 9)|(gt IEMobile 7)]><!-->
 <html <?php print $html_attributes; ?> prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#">
-<!--<![endif]-->
 <head>
   <title><?php print $head_title; ?></title>
   <?php if (!empty($tp_digital_data)): ?>
@@ -32,19 +26,6 @@
   <meta http-equiv="cleartype" content="on">
   <?php print $styles; ?>
   <?php print $scripts; ?>
-  <?php if ($add_html5_shim and !$add_respond_js): ?>
-    <!--[if lt IE 9]>
-    <script src="<?php print $base_path . $path_to_zen; ?>/js/html5.js"></script>
-    <![endif]-->
-  <?php elseif ($add_html5_shim and $add_respond_js): ?>
-    <!--[if lt IE 9]>
-    <script src="<?php print $base_path . $path_to_zen; ?>/js/html5-respond.js"></script>
-    <![endif]-->
-  <?php elseif ($add_respond_js): ?>
-    <!--[if lt IE 9]>
-    <script src="<?php print $base_path . $path_to_zen; ?>/js/respond.js"></script>
-    <![endif]-->
-  <?php endif; ?>
   <script src="<?php print variable_get('digital_data_wrapper_js'); ?>"></script>
   <script src="<?php print variable_get('centralized_login_widget_js'); ?>"></script>
 </head>
