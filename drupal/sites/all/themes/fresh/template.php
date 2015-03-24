439178989f898be2a37f112b7f9225e58b857e98
Scott Knight <sknight17@gmail.com>
2015-03-24 12:59:27 -0700

    TP-2994: mobile menu, unstyled

--- drupal/sites/all/themes/fresh/template.php
+++ drupal/sites/all/themes/fresh/template.php
@@ -9,7 +9,7 @@
 
 
 /**
- * Function implements hook_page_build
+ * Implements hook_page_build
  */
 function fresh_page_alter(&$page){
   $node = menu_get_object();
@@ -17,4 +17,20 @@ function fresh_page_alter(&$page){
     $path = drupal_get_path('theme', 'fresh'). '/css/article.css';
     $page['content']['#attached']['css'][] = $path;
   }
+}
+
+
+/**
+ * Implements hook_preprocess_page
+ */
+function fresh_preprocess_page(&$variables) {
+
+  /* Statically add mobile menu on every page */
+  $mobile_menu = theme('tp4_support_mobile_menu_header');
+  $variables['page']['left_drawer']['social']['#markup'] = '<div id="block-tp4-support-tp4-mobile-menu-header">'. $mobile_menu. '</div>';
+
+  /* Statically add mobile menu */
+  $menu = drupal_render(menu_tree_output(menu_tree_all_data('menu-megamenu')));
+  $variables['page']['left_drawer']['menu']['#markup'] = '<div id="block-menu-menu-megamenu">'. $menu. '</div>';
+
 }
