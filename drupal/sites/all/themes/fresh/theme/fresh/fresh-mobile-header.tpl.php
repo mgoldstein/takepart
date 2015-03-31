c8d7cf6bf93300b1e760fe5ae911f60de87485dd
Scott Knight <sknight17@gmail.com>
2015-03-31 08:52:55 -0700

    TP-2993: unstyled header

---
+++ drupal/sites/all/themes/fresh/theme/fresh/fresh-mobile-header.tpl.php
@@ -0,0 +1,22 @@
+<div id="mobile-header" class="header">
+
+  <div class="menu-toggle"></div>
+  <div class="logo">
+    <a href="http://www.takepart.com" class="tplinkpos" name="logo"><img src="//www.takepart.com/sites/all/themes/tp4/images/takepart_logo.png"></a>
+  </div>
+
+  <div class="search">
+    <div class="search-toggle"></div>
+    <?php /* render form */ ?>
+    <form action="/" method="post" id="search-api-page-search-form-site-search" accept-charset="UTF-8">
+      <div>
+        <div class="form-item form-type-textfield form-item-keys-2">
+          <input placeholder="Search" type="text" id="edit-keys-2" name="keys_2" value="" size="15" maxlength="128" class="form-text">
+        </div>
+        <input type="hidden" name="id" value="2">
+        <input type="submit" id="edit-submit-2" name="op" value="Search" class="form-submit"><input type="hidden" name="form_build_id" value="form-xC6KRTt0F2wXTvq-VPToxxvLMkSmUnAfFyFBT4PPMkI">
+        <input type="hidden" name="form_id" value="search_api_page_search_form_site_search">
+      </div>
+    </form>
+  </div>
+</div>
