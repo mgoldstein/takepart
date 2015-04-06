2e3eecf9f3287faf609e43dde2b3d65857300077
Roger <rtanyongkul@stauffer.com>
2015-04-06 16:29:34 -0700

    TP-2871: Adding initial code for handling article insertion logic of ads. This is willout the mobile 300x50 ad

--- drupal/sites/all/themes/fresh/theme/fresh/fresh-ad.tpl.php
+++ drupal/sites/all/themes/fresh/theme/fresh/fresh-ad.tpl.php
@@ -1,5 +1,4 @@
 <div class="ad-wrapper">
   <h5 class="ad-label text-center">Advertisement</h5>
-  <img class="ad" src="http://placehold.it/320x50" />
-  <!-- Replace the above with the actual ad tag -->
+  <?php print $ad_tag; ?>
 </div>
