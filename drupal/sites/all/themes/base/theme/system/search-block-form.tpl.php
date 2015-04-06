571438fd2314d49b191d9be66ccda37e9ee21ae1
Scott Knight <sknight17@gmail.com>
2015-04-06 16:59:00 -0700

    TP-2993: added search and styled mobile search

---
+++ drupal/sites/all/themes/base/theme/system/search-block-form.tpl.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @file
+ * Displays the search form block.
+ * Remove all the wrappers Drupal by default adds
+ */
+?>
+<?php print $search_form; ?>
--- drupal/sites/all/themes/fresh/js/script.js
+++ drupal/sites/all/themes/fresh/js/script.js
@@ -7,19 +7,48 @@
 (function ($, Drupal, window, document, undefined) {
 
 
-Drupal.behaviors.mobileMenuToggle = {
-  attach: function(context, settings) {
+  Drupal.behaviors.mobileMenuToggle = {
+    attach: function(context, settings) {
 
-    var $body = $('body');
-    $('.toggle-menu.toggle-left').click(function(){
+      var $body = $('body');
+      $('.toggle-menu.toggle-left').click(function(){
 
-      if ($body.hasClass('mobile-menu-show')) {
-        $body.removeClass("mobile-menu-show" );
-      } else {
-        $body.addClass("mobile-menu-show" );
-      }
-    });
-  }
-};
+        if ($body.hasClass('mobile-menu-show')) {
+          $body.removeClass("mobile-menu-show" );
+        } else {
+          $body.addClass("mobile-menu-show" );
+        }
+      });
+    }
+  };
+
+  Drupal.behaviors.mobileSearchToggle = {
+    attach: function(context, settings) {
+
+      /* Show search field */
+      var $body = $('body');
+      $('.toggle-search').click(function(){
+        if ($body.hasClass('mobile-search-show')) {
+          $body.removeClass("mobile-search-show" );
+        }else{
+          $body.addClass("mobile-search-show" );
+        }
+      });
+
+      /* Hide search if clicked away from */
+      $(document).on('click', function(event) {
+        if (!$(event.target).closest('.search').length) {
+          $body.removeClass("mobile-search-show" );
+        }
+      });
+
+
+      //makes the search go away on focus
+      $('.search input').focus(function() {
+        $(this).val('');
+      });
+
+    }
+  };
 
 })(jQuery, Drupal, this, this.document);
