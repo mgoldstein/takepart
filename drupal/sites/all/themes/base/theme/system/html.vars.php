e8bccf9053ec16135940f75560a0321afcad35aa
Scott Knight <sknight17@gmail.com>
2015-04-06 17:23:26 -0700

    TP-2993: fixed bug where shared assets was overriding takepart css

--- drupal/sites/all/themes/base/theme/system/html.vars.php
+++ drupal/sites/all/themes/base/theme/system/html.vars.php
@@ -7,6 +7,6 @@
 function base_preprocess_html(&$variables){
   /* Add shared assets to all tp4 pages */
   if($shared_assets = variable_get('shared_assets_path')){
-    drupal_add_css($shared_assets, array('type' => 'external'));
+    drupal_add_css($shared_assets, array('type' => 'external', 'weight' => -1));
   }
 }
