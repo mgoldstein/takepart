47a4aef3357e930c7621578a213d5ce3f469b2b4
Tim Heiderich <theiderich@takepart.com>
2015-04-06 18:01:52 -0700

    TP-2875 Added styling and (inactive) support for promoted content.

--- drupal/sites/all/modules/custom/tp_more_on_takepart/templates/tp_more_on_takepart.tpl.php
+++ drupal/sites/all/modules/custom/tp_more_on_takepart/templates/tp_more_on_takepart.tpl.php
@@ -1,13 +1,9 @@
-<footer id="article-footer">
-	<h3><?php print t('Related Stories on TakePart'); ?></h3>
+<footer id="more-on-takepart" class="clearfix">
+	<h2><?php print t('More on TakePart'); ?></h2>
 
 	<?php foreach($variables['article'] as $v) { print $v; } ?>
 
-    <h3 class="top-border"><?php print t('Takepart&#8217;s Most Popular'); ?></h3>
-
 	<?php foreach($variables['global'] as $v) { print $v; } ?>
-
-    <h3><?php print t('From The Web'); ?></h3>
 
     <?php print $variables['taboola']; ?>
 
