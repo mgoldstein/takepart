5b076de73fe711febf6fd19618a2a9db41770cce
Tim Heiderich <theiderich@takepart.com>
2015-04-10 16:07:34 -0700

    TP-2875 Changed naming conventions to provide identifiers for TP-3006.

--- drupal/sites/all/modules/custom/tp_more_on_takepart/templates/tp_more_on_takepart.tpl.php
+++ drupal/sites/all/modules/custom/tp_more_on_takepart/templates/tp_more_on_takepart.tpl.php
@@ -1,9 +1,9 @@
 <footer id="more-on-takepart" class="clearfix">
 	<h2><?php print t('More on TakePart'); ?></h2>
 
-	<?php foreach($variables['article'] as $v) { print $v; } ?>
+	<?php foreach($variables['related'] as $v) { print $v; } ?>
 
-	<?php foreach($variables['global'] as $v) { print $v; } ?>
+	<?php foreach($variables['promoted'] as $v) { print $v; } ?>
 
     <?php print $variables['taboola']; ?>
 
