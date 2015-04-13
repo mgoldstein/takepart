cfc70f6439b25065b8d0c513e7fd0c0262a2011e
Scott Knight <sknight17@gmail.com>
2015-04-13 16:05:43 -0700

    TP-2993: placeholder logo no longer a placeholder

--- drupal/sites/all/themes/fresh/theme/fresh/fresh-mobile-header.vars.php
+++ drupal/sites/all/themes/fresh/theme/fresh/fresh-mobile-header.vars.php
@@ -5,8 +5,12 @@
  */
 function fresh_preprocess_fresh_mobile_header(&$variables){
   /* Grab the logo */
-  $path = drupal_get_path('theme', 'fresh');
-  $logo = theme('image', array('path' => $path));
+  global $base_url;
+  $path = drupal_get_path('theme', 'base');
+  $logo_path = $path. '/images/logo.png';
+  $logo = theme('image', array('path' => $logo_path));
+  $logo = l($logo, $base_url, array('html' => true, 'attributes' => array('class' => array('logo'))));
+  $variables['logo'] = $logo;
 
   /* Grab the search form */
   $search = module_invoke('search_api_page', 'block_view', '2');
