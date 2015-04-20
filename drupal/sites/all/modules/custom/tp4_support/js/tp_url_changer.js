c2b7d844f1b7152301c54e3bbf15abe1d9d2fd57
Roger <rtanyongkul@stauffer.com>
2015-04-20 14:08:02 -0700

    TP-3003: Updating logic for auto url changer

--- drupal/sites/all/modules/custom/tp4_support/js/tp_url_changer.js
+++ drupal/sites/all/modules/custom/tp4_support/js/tp_url_changer.js
@@ -1,7 +1,24 @@
 (function ($, Drupal, window, document, undefined) {
   //creates a drupal behavior
+  /**
+   *
+   *  @function:
+   *    This drupal behavior is used to change the URL and Title on the page.
+   *    Note that some specific browser does not respect title changes
+   *
+   *  @example:
+   *    This can be implemented via adding the following to any html tag
+   *      data-tp-url="/example"
+   *      data-tp-url-title="Test" *Note that some browsers does not respect this
+   *
+   */
   Drupal.behaviors.tp_auto_changer = {
     attach: function(context, settings) {
+      //variables
+      window.lastScrollTop = 0;
+      window.tp_url_orig = window.location.href;
+      window.tp_url_title_orig = document.title;
+      
       //binds to the scroll
       $(window).bind('scroll', function() {
         //default variables for this scope
@@ -11,18 +28,35 @@
         var viewport = {
           top : win.scrollTop(),
           left : win.scrollLeft(),
+          bottom : win.scrollTop() + win.height()
         };
+        
+        //creates offset based on device height
+        var offset_pad = -1 * (win.height() / 2);
+        
+        //checks which direction the scroll is occurring on. note in use but built
+        if (viewport.top < lastScrollTop) {
+          direction = 'up';
+        }
+        else {
+          direction = 'down';
+        }
         
         //does for each data-tp-url
         $('[data-tp-url]').each(function(index, value) {
           var tp_url = $(this).data('tpUrl'); //maps to data-tp-url
           var tp_url_title = $(this).data('tpUrlTitle'); //maps to data-tp-url-title
           var offset = $(this).offset();
+          offset.bottom = $(this).height() + offset.top;
+          
+          //ensures that if the height is bigger then the div then change it to whichever is smaller
+          if (offset_pad < $(this).height() / 2) {
+            offset_pad = -1 * $(this).height() / 2;
+          }
           
           //ensures that the tp url is set before changing
           if (typeof tp_url !== 'undefined') {
-            //compares offset and viewport
-            if (offset.top < viewport.top) {
+            if (offset.top + offset_pad < viewport.top) {
               url = tp_url;
               
               //only change the title if it's not undefined
@@ -33,7 +67,19 @@
               return;
             }
           }
+          
+          //changes back to original page if needed
+          if (index === 0) {
+            //ensures that if user scrolls back to the top that we have the original
+            if (offset.top + offset_pad > viewport.top) {
+              url = tp_url_orig;
+              title = tp_url_title_orig;
+            }
+          }
         });
+        
+        //updates the last scroll var
+        lastScrollTop = viewport.top;
         
         //only change the url if it's not the same
         if (url != window.location.pathname) {
