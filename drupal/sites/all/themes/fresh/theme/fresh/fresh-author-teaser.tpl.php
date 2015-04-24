59c25bdefa5ca3fe36bcd2cad58886d780f3e285
Tim Heiderich <theiderich@takepart.com>
2015-04-24 12:00:50 -0700

    TP-3130 Added support for multiple authors in byline.

--- drupal/sites/all/themes/fresh/theme/fresh/fresh-author-teaser.tpl.php
+++ drupal/sites/all/themes/fresh/theme/fresh/fresh-author-teaser.tpl.php
@@ -1,21 +1,26 @@
 <div class="author-teaser col-xxs-12">
   <div class="line line-style-2"></div>
+<?php foreach($variables['authors'] as $author): ?>
   <div class="row">
     <?php if(!empty($variables['date'])): ?>
       <div class="published-at col-xxs-10 col-xxs-offset-2"><?php print $variables['date']; ?></div>
     <?php endif; ?>
-    <div class="image col-xxs-2"><img class="img-circle" src="<?php print $variables['image']; ?>" /></div>
-    <div class="about col-xxs-10"><?php print $variables['about']; ?></div>
+    <div class="image col-xxs-2"><img class="img-circle" src="<?php print $author['image']; ?>" /></div>
+    <div class="about col-xxs-10"><?php print $author['about']; ?></div>
   </div>
 
   <div class="row">
     <div class="links col-xxs-10 col-xxs-offset-2">
       <ul class="menu horizontal-menu">
-        <?php foreach($variables['links'] as $link): ?>
+        <?php foreach($author['links'] as $link): ?>
           <li><?php print $link; ?></li>
         <?php endforeach; ?>
       </ul>
     </div>
   </div>
+<?php 
+	unset($variables['date']);
+	endforeach;
+?>
   <div class="line line-style-1"></div>
 </div>
