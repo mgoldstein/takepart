8b3939f68e166dbdf21cbf055f7211bf1ffda83a
Scott Knight <sknight17@gmail.com>
2015-06-02 09:52:18 -0700

    TP-3249: MegaSlim Menu display logic complete

---
+++ drupal/sites/all/modules/custom/tp_megaslim_menu/templates/tp-megaslim-menu-item.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file
+ * Documentation on what this does
+ */
+?>
+<div class="mega-teaser">
+  <div class="picture"><?php print $thumbnail; ?></div>
+  <div class="label"><?php print $label; ?></div>
+  <div class="text"><?php print $content; ?></div>
+</div>
---
+++ drupal/sites/all/modules/custom/tp_megaslim_menu/templates/tp-megaslim.tpl.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @file
+ * Documentation goes here
+ */
+?>
+<div class="megaslim">
+  <div class="submenu"><?php print $submenu; ?></div>
+  <div class="more-link"><?php print $more_link; ?></div>
+  <div class="teasers"><?php print $teasers; ?></div>
+</div>
