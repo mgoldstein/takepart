5d86f5138e54c16a12ee718a9ba6f376d0f2349d
Marc Goldstein <mgoldstein@takepart.com>
2015-06-05 14:48:51 -0700

    TP-3365: add optimizely to Fresh

--- drupal/sites/all/themes/fresh/theme/system/html.vars.php
+++ drupal/sites/all/themes/fresh/theme/system/html.vars.php
@@ -1,10 +1,17 @@
 <?php
 
 /**
-* Implements hook_preprocess_html();
-*/
+ * Implements hook_preprocess_html();
+ */
 function fresh_preprocess_html(&$variables) {
-// Pass the digital data to the HTML template.
-$variables['tp_digital_data'] =  isset($variables['page']['tp_digital_data'])
-? $variables['page']['tp_digital_data'] : NULL;
+  // Pass the digital data to the HTML template.
+  $variables['tp_digital_data'] = isset($variables['page']['tp_digital_data']) ? $variables['page']['tp_digital_data'] : NULL;
+
+  drupal_add_js('//cdn.optimizely.com/js/77413453.js', array(
+    'type' => 'external',
+    'scope' => 'header',
+    'group' => JS_DEFAULT,
+    'every_page' => TRUE,
+    'weight' => -1,
+  ));
 }
