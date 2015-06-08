cb8fd50f66c588d7d140cc1330340f60d27f1289
Marc Goldstein <mgoldstein@takepart.com>
2015-06-08 13:00:11 -0700

    Fix for width of Facebook comments

--- drupal/sites/all/themes/fresh/theme/fresh/fresh-fb-comments.tpl.php
+++ drupal/sites/all/themes/fresh/theme/fresh/fresh-fb-comments.tpl.php
@@ -1,3 +1,13 @@
-	<a class="comments-count"><span class="icon i-chat-bubble"></span> Show Comments (<fb:comments-count href="<?php print $variables['url']; ?>" >0</fb:comments-count>)</a>
+<a class="comments-count"><span class="icon i-chat-bubble"></span> Show Comments (<fb:comments-count href="<?php print $variables['url']; ?>" >0</fb:comments-count>)</a>
 
-	<fb:comments data-numposts="1" href="<?php print $variables['url']; ?>" class="fb_comments"></fb:comments>
+<fb:comments id="fb_chat" data-numposts="1"  href="<?php print $variables['url']; ?>" class="fb_comments"></fb:comments>
+
+<script type="text/javascript">
+  var fb_chat = document.getElementById('fb_chat');
+  if (window.innerWidth > 480) {
+    fb_chat.setAttribute("data-width", 640);
+  }
+  else {
+    fb_chat.setAttribute("data-width", 320);
+  }
+</script>
