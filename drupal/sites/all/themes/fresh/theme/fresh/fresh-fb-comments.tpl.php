7772b852d23e5a093e8758d9df07e2cff79ee95c
Marc Goldstein <mgoldstein@takepart.com>
2015-06-09 18:25:33 -0700

    TP-3397: restore anchor name to comments box

--- drupal/sites/all/themes/fresh/theme/fresh/fresh-fb-comments.tpl.php
+++ drupal/sites/all/themes/fresh/theme/fresh/fresh-fb-comments.tpl.php
@@ -1,4 +1,4 @@
-<a class="comments-count"><span class="icon i-chat-bubble"></span> Show Comments (<fb:comments-count href="<?php print $variables['url']; ?>" >0</fb:comments-count>)</a>
+<a class="comments-count" name="article-comments"><span class="icon i-chat-bubble"></span> Show Comments (<fb:comments-count href="<?php print $variables['url']; ?>" >0</fb:comments-count>)</a>
 
 <fb:comments id="fb_chat" data-numposts="1"  href="<?php print $variables['url']; ?>" class="fb_comments"></fb:comments>
 
