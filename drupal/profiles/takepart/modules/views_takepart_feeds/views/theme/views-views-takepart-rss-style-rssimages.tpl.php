d5c7a15a97ed8a0a594ffc51055d8ad27581deed
Marc Goldstein <mgoldstein@takepart.com>
2015-06-10 13:02:48 -0700

    TP-3260: cdata wrappers for title and description due to MS Word characters

--- drupal/profiles/takepart/modules/views_takepart_feeds/views/theme/views-views-takepart-rss-style-rssimages.tpl.php
+++ drupal/profiles/takepart/modules/views_takepart_feeds/views/theme/views-views-takepart-rss-style-rssimages.tpl.php
@@ -16,8 +16,8 @@
     } elseif(preg_match('/Safari/i',$u_agent)) {
       drupal_add_http_header('Content-Type', 'text/xml; charset=utf-8');   
     } else { */
-      drupal_add_http_header('Content-Type', 'application/rss+xml; charset=utf-8');
-	 // drupal_add_http_header('Content-Type', 'text/xml');
+      // drupal_add_http_header('Content-Type', 'application/rss+xml; charset=utf-8');
+	 drupal_add_http_header('Content-Type', 'text/xml');
     //}
     print $xml;
     exit;
