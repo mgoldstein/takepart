e0d3102fd8b783fc37b1c1abaff7be80b5da1f6d
Samrod Shenassa <sshenassa@takepart.com>
2015-06-09 18:09:32 -0700

    TAP-1719 Upated Inlfuence count, icon, and overlay styling in new article template. NOTE: requires shared-assets deploy for the new font.

--- drupal/sites/default/development.settings.php
+++ drupal/sites/default/development.settings.php
@@ -44,7 +44,7 @@ $conf += array(
 );
 
 // Shared Assets
-$conf['shared_assets_path'] = '//s3.amazonaws.com/tab_assets/shared_assets_qa/font.css';
+$conf['shared_assets_path'] = '//s3.amazonaws.com/tab_assets/shared_assets_develop/font.css';
 
 // Scream at the dev, maybe they'll fix something
 ini_set('error_reporting', E_ALL & ~E_NOTICE);
