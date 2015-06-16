150916541dda1feccf9da260ff0c0bd47881b34a
Marc Goldstein <mgoldstein@takepart.com>
2015-06-16 13:17:23 -0700

    TP-3401: remove old fb_comments class

--- drupal/sites/all/themes/fresh/theme/fresh/fresh-fb-comments.tpl.php
+++ drupal/sites/all/themes/fresh/theme/fresh/fresh-fb-comments.tpl.php
@@ -1,6 +1,6 @@
 <a class="comments-count"><span class="icon i-chat-bubble"></span> Show Comments (<fb:comments-count href="<?php print $variables['url']; ?>" >0</fb:comments-count>)</a>
 
-<fb:comments class="fb_chat" data-numposts="1"  href="<?php print $variables['url']; ?>" class="fb_comments"></fb:comments>
+<fb:comments class="fb_chat" data-numposts="1"  href="<?php print $variables['url']; ?>"></fb:comments>
 
 <script type="text/javascript">
   var fb_chat = $(".fb_chat");
