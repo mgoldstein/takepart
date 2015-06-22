db6bf5be61835089e41dbba9855d8f544934e2f0
Matthew Wrather <matt@wrathercreative.com>
2015-06-21 20:18:06 -0700

    TP-3351 add theme hook, preprocess, templates for "more on takepart" video player

--- drupal/sites/all/modules/custom/tp_more_on_takepart/templates/tp_more_on_takepart.tpl.php
+++ drupal/sites/all/modules/custom/tp_more_on_takepart/templates/tp_more_on_takepart.tpl.php
@@ -1,13 +1,22 @@
-<div id="more-on-takepart" class="clearfix">
-  <h2><?php print t('More on TakePart'); ?></h2>
-  <div class="row">
+<div id="more-on-takepart" class="clearfix row">
+  <h2 class="tp-more__title"><?php print t('More on TakePart'); ?></h2>
+<?php if ($variables['promoted_video']) : ?>
+    <div class="col-md-6">
+      <div class="row">
+
+<?php endif; ?>
     <?php foreach($variables['related'] as $v) { print $v; } ?>
 
     <?php foreach($variables['promoted'] as $v) { print $v; } ?>
 
     <?php print $variables['taboola']; ?>
-  </div>
+
+<?php if ($variables['promoted_video']) : ?>
+      </div>
+    </div>
 
   <?php print $variables['promoted_video']; ?>
+
+<?php endif; ?>
 
 </div>
---
+++ drupal/sites/all/modules/custom/tp_more_on_takepart/templates/tp_more_on_takepart_video.tpl.php
@@ -0,0 +1,10 @@
+<?php
+?>
+<div class="col-xxs-12 col-md-6 tp-more__video">
+  <?php print render($featured_video_player); ?>
+  <h4 class="tp-more__video-title">
+    <?php print render($featured_video_title); ?>
+  </h4>
+  <?php print render($sponsored); ?>
+  <div class="tp-more__video-cta"><?php print render($video_link); ?></div>
+</div>
