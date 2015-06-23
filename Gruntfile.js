c08110ca9b1979b3a7577973f73d40feaa8a4288
Scott Knight <sknight17@gmail.com>
2015-06-23 10:46:58 -0700

    TP-3377: removed compass generated sprites from git
    Conflicts:
    	Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,14 +12,10 @@ module.exports = function (grunt) {
                     sassDir: './drupal/sites/all/themes/fresh/sass',
                     cssDir: './drupal/sites/all/themes/fresh/css',
                     imagesDir: './drupal/sites/all/themes/fresh/images',
-<<<<<<< HEAD
                     raw: "add_import_path 'drupal/sites/all/themes/base/sass' \n" +
                     "generated_images_dir = './drupal/sites/all/themes/fresh/images' \n" +
                     "http_images_path = '/sites/all/themes/fresh/images' \n" +
                     "http_generated_images_path = '/sites/all/themes/fresh/images'",
-=======
-                    raw: "add_import_path 'drupal/sites/all/themes/base/sass'",
->>>>>>> bebebf8... TP-3377: Grunt for compiling tp4 and fresh assets
                     environment: 'development',
                     force: true
                 }
@@ -30,15 +26,10 @@ module.exports = function (grunt) {
                     cssDir: './drupal/sites/all/themes/tp4/css',
                     imagesDir: './drupal/sites/all/themes/tp4/images',
                     raw: "add_import_path 'drupal/sites/all/themes/base/sass' \n" +
-                    "extensions_dir  = './drupal/sites/all/themes/tp4/sass-extensions' \n" +
-<<<<<<< HEAD
                     "require 'zen-grids' \n" +
                     "generated_images_dir = './drupal/sites/all/themes/tp4/images' \n" +
                     "http_images_path = '/sites/all/themes/tp4/images' \n" +
                     "http_generated_images_path = '/sites/all/themes/tp4/images'",
-=======
-                    "require 'zen-grids'",
->>>>>>> bebebf8... TP-3377: Grunt for compiling tp4 and fresh assets
                     environment: 'development',
                     force: true
                 }
