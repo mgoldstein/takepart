66c23b457b6d64b1ac3d9109e3928a4b97c179c2
Matthew Wrather <matt@wrathercreative.com>
2015-07-06 17:14:24 -0700

    TP-3483 refactor template to provide classes for styling more on tp title

--- drupal/sites/all/modules/custom/tp_more_on_takepart/templates/tp_more_on_takepart.tpl.php
+++ drupal/sites/all/modules/custom/tp_more_on_takepart/templates/tp_more_on_takepart.tpl.php
@@ -1,5 +1,5 @@
-<div id="more-on-takepart" class="clearfix row">
-  <h2 class="tp-more__title"><?php print t('More on TakePart'); ?></h2>
+<div<?php print $attributes; ?> class="<?php print $classes; ?> clearfix">
+  <h2<?php print $title_attributes; ?>><?php print t('More on TakePart'); ?></h2>
   <?php print render($promos); ?>
   <?php print render($promoted_video); ?>
   <?php print $taboola; ?>
