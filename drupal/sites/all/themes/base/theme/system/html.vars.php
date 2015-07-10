aca0b9690eb7b8ed3eac45befccd94909b24202e
Samrod Shenassa <sshenassa@takepart.com>
2015-07-09 19:19:12 -0700

    TAP-1739 split the font.css link into font.css and takepart_icons.css

--- drupal/sites/all/themes/base/theme/system/html.vars.php
+++ drupal/sites/all/themes/base/theme/system/html.vars.php
@@ -6,7 +6,8 @@
  */
 function base_preprocess_html(&$variables){
   /* Add shared assets to all tp4 pages */
-  if($shared_assets = variable_get('shared_assets_path')){
-    drupal_add_css($shared_assets, array('type' => 'external', 'weight' => -1));
+  if ($shared_assets = variable_get('shared_assets_path')) {
+    drupal_add_css($shared_assets.'font.css',           array('type' => 'external', 'weight' => -1));
+    drupal_add_css($shared_assets.'takepart_icons.css', array('type' => 'external', 'weight' => -1));
   }
 }
