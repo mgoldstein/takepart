c7a314c41ddcce940811e7b1144158983cf523db
Marc Goldstein <mgoldstein@takepart.com>
2015-07-21 11:09:25 -0700

    TP-3488: update Facebook comment box code for 2.0 API

--- drupal/sites/all/themes/fresh/theme/fresh/fresh-fb-comments.tpl.php
+++ drupal/sites/all/themes/fresh/theme/fresh/fresh-fb-comments.tpl.php
@@ -1,11 +1,11 @@
-<a class="comments-count"><span class="icon i-chat-bubble"></span> Show Comments (<fb:comments-count href="<?php print $variables['url']; ?>" >0</fb:comments-count>)</a>
+<a class="comments-count"><span class="icon i-chat-bubble"></span> 
+  Show Comments (<span class="fb-comments-count" data-href="<?php print $variables['url']; ?>"></span>)</a>
 
-<fb:comments class="fb_chat" data-width="320" data-numposts="1"  href="<?php print $variables['url']; ?>"></fb:comments>
+<div class="fb-comments" data-href="<?php print $variables['url']; ?>" data-numposts="1" data-version="v2.3" data-width="320"></div>
 
 <script type="text/javascript">
-  var fb_chat = jQuery(".fb_chat");
+  var fb_chat = jQuery(".fb-comments");
   if (window.innerWidth > 480) {
     jQuery(fb_chat).attr("data-width", 640);
   }
-  
 </script>
