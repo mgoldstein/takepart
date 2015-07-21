d782092b8183203e784080c0d62ff80d9cb95769
Marc Goldstein <mgoldstein@takepart.com>
2015-07-21 12:55:07 -0700

    TP-3495: clean up application.settings.php

--- drupal/sites/default/application.settings.php
+++ drupal/sites/default/application.settings.php
@@ -18,8 +18,6 @@ if (file_exists($environment_settings_filename)) {
 if (isset($conf['memcache_servers']) && $conf['memcache_servers'] === FALSE) {
   unset($conf['memcache_servers']);
 } else {
-  // include_once('./includes/cache.inc');
-  // include_once('./sites/all/modules/contrib/memcache/memcache.inc');
   $conf['cache_backends'][] = 'sites/all/modules/contrib/memcache/memcache.inc';
   $conf += array(
     'cache_default_class' => 'MemCacheDrupal',
@@ -28,15 +26,6 @@ if (isset($conf['memcache_servers']) && $conf['memcache_servers'] === FALSE) {
     'page_cache_invoke_hooks' => FALSE,
     'lock_inc' => 'sites/all/modules/contrib/memcache/memcache-lock.inc',
     'memcache_stampede_protection' => TRUE,
-    /* 'session_inc' => './sites/all/modules/contrib/memcache/memcache-session.inc', */
-    /* 'memcache_bins' => array(
-	 'cache' => 'default',
-	 'cache_filter' => 'default',
-	 'cache_menu' => 'default',
-	 'cache_page' => 'default',
-	 // 'session' => 'default',
-	 'users' => 'default',
-    ), */
     'memcache_servers' => array(
 	 'localhost:11211' => 'default',
     ),
@@ -109,7 +98,6 @@ $conf['bluehornet_api_accounts'] += array(
   ),
 );
 $conf += array('bluehornet_default_account' => 'takepart');
-
 
 if (!empty($conf['reverse_proxy_addresses'])) {
 $ips = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
