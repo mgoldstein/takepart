14b9668a88240b76ba47ff9b258371458dc0e605
Marc Goldstein <mgoldstein@takepart.com>
2015-08-25 13:30:50 -0700

    TP-3561: fix logic for sponsored content. Set status at page level, not node level
    TP-3561: fix logic for sponsored content. Set status at page level, not node level

--- drupal/sites/all/themes/base/theme/page.vars.php
+++ drupal/sites/all/themes/base/theme/page.vars.php
@@ -3,9 +3,24 @@
 /**
  * Implements hook_preprocess_page()
  */
-function base_preprocess_page(&$variables){
+function base_preprocess_page(&$variables) {
   // jquery.cookie plugin is not being loaded for anonymous users and needs to be for TAP
   drupal_add_library('system', 'jquery.cookie');
+
+  /*
+   * Adds <meta property="sponsored" content="Promoted" /> to sponsored content
+   */
+  if (!empty($variables['node']->field_sponsored[$variables['node']->language][0]['tid'])) {
+    $sponsored_metatag = array(
+	 '#type' => 'html_tag',
+	 '#tag' => 'meta',
+	 '#attributes' => array(
+	   'property' => 'sponsored',
+	   'content' => 'Promoted',
+	 )
+    );
+    drupal_add_html_head($sponsored_metatag, 'sponsored_metatag');
+  }
 
   // Add Node-specific page templates
   if (!empty($variables['node'])) {
