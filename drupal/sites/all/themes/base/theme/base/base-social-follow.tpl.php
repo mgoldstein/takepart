1855b95b674452753de12565cc81fa675b350387
Roger <rtanyongkul@stauffer.com>
2015-09-02 10:55:42 -0700

    TP-3594: Updating code for social icons globally

--- drupal/sites/all/themes/base/theme/base/base-social-follow.tpl.php
+++ drupal/sites/all/themes/base/theme/base/base-social-follow.tpl.php
@@ -2,8 +2,8 @@
 <div class="follow-us">
   <ul class="social menu horizontal-menu">
     <li class="item"><a href="//facebook.com/takepart" class="icon i-facebook"></a></li>
-    <li class="item"><a href="//plus.google.com/+TakePart" class="icon i-google_plusone_share"></a></li>
     <li class="item"><a href="//twitter.com/takepart" class="icon i-twitter"></a></li>
+    <li class="item"><a href="//plus.google.com/+TakePart" class="icon i-google_plusone_share"></a></li>
     <li class="item"><a href="//go.takepart.com/clients/takepart/survey.htm" class="icon i-email"></a></li>
   </ul>
 </div>
