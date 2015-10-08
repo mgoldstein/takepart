38948c4f3a51a01e3189b89347d6887c375d3ec9
Jacob Pitassi <jpitassi@stauffer.com>
2015-10-08 14:34:34 -0700

    TP-3695 Mobile Share Buttons

--- drupal/sites/all/modules/custom/tp_social_menu/js/tpsocial.jquery.js
+++ drupal/sites/all/modules/custom/tp_social_menu/js/tpsocial.jquery.js
@@ -5,32 +5,32 @@
   var cpre = 'tp-social-';
   var $window = $(window);
   /*
-   * Scan for sponsored meta tag and pull value if present
-   */
+  * Scan for sponsored meta tag and pull value if present
+  */
   var sponsoredmetatag = $("meta[property='sponsored']").attr("content");
   var default_title = (sponsoredmetatag) ? document.title + ' (' + sponsoredmetatag + ')' : document.title;
   /*
-   *
-   * Use open graph title if present
-   */
+  *
+  * Use open graph title if present
+  */
   var ogtitle = $("meta[property='og:title']").attr("content");
   var twittertitle = $("meta[name='twitter:title']").attr("content");
   if (ogtitle) {
     // If sponsored meta tag present and promoted copy is not in the og:title, then add it
     if (sponsoredmetatag && ogtitle.toLowerCase().indexOf("(" + sponsoredmetatag.toLowerCase() + ")") < 0) {
-	 default_title = ogtitle + " (" + sponsoredmetatag + ")";
+      default_title = ogtitle + " (" + sponsoredmetatag + ")";
     }
     else {
-	 default_title = ogtitle;
+      default_title = ogtitle;
     }
   }
   else if (twittertitle) {
     // If sponsored meta tag present and promoted copy is not in the twitter:title, then add it
     if (sponsoredmetatag && twittertitle.toLowerCase().indexOf("(" + sponsoredmetatag.toLowerCase() + ")") < 0) {
-	 default_title = twittertitle + " (" + sponsoredmetatag + ")";
+      default_title = twittertitle + " (" + sponsoredmetatag + ")";
     }
     else {
-	 default_title = twittertitle;
+      default_title = twittertitle;
     }
   }
 
@@ -59,52 +59,53 @@
 
 
   /*
-   * function for tracking shares of UPS sponsored content
-   */ 
+  * function for tracking shares of UPS sponsored content
+  */
   var upsShare = function (service) {
     // Check for simplereach tags. If they don't exist, this isn't UPS sponsored
     if (typeof __reach_config !== 'undefined' ||
-		  typeof __reach_config.tags !== 'undefined') {
-	 // If free tags are present for sponsored content
-	 if (__reach_config.tags.indexOf('Business') !== -1 ||
-		    __reach_config.tags.indexOf('UPS') !== -1) {
+    typeof __reach_config.tags !== 'undefined') {
+      // If free tags are present for sponsored content
+      if (__reach_config.tags.indexOf('Business') !== -1 ||
+      __reach_config.tags.indexOf('UPS') !== -1) {
 
-	   var appID = '';
-	   var ebRand = Math.random() + '';
-	   ebRand = ebRand * 1000000;
-	   switch (service) {
-		case 'facebook':
-		  appID = '617974';
-		  break;
+        var appID = '';
+        var ebRand = Math.random() + '';
+        ebRand = ebRand * 1000000;
+        switch (service) {
+          case 'facebook':
+          appID = '617974';
+          break;
 
-		case 'twitter':
-		  appID = '617975';
-		  break;
+          case 'twitter':
+          appID = '617975';
+          break;
 
-		case 'mailto':
-		  appID = '617978';
-		  break;
+          case 'mailto':
+          appID = '617978';
+          break;
 
-		default:
-		  appID = '';
-	   }
-	   // Only fire UPS tracking for Facebook, Twitter, and email shares
-	   if (appID !== '') {
-		var s = document.createElement('script');
-		s.type = 'text/javascript';
-		s.src = document.location.protocol + '//bs.serving-sys.com/Serving/ActivityServer.bs?cn=as&amp;ActivityID=' + appID + '&amp;rnd=' + ebRand;
-		(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(s);
-	   }
-	 }
+          default:
+          appID = '';
+        }
+        // Only fire UPS tracking for Facebook, Twitter, and email shares
+        if (appID !== '') {
+          var s = document.createElement('script');
+          s.type = 'text/javascript';
+          s.src = document.location.protocol + '//bs.serving-sys.com/Serving/ActivityServer.bs?cn=as&amp;ActivityID=' + appID + '&amp;rnd=' + ebRand;
+          (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(s);
+        }
+      }
     }
   };
 
   // function to make social share link
   var makeLink = function (args) {
     var $link = $('<a href="#"/>')
-		  .addClass(cpre + args.name)
-		  .addClass(cpre + 'link')
-		  .html(args.display);
+    .addClass(cpre + args.name)
+    .addClass(cpre + 'link')
+    .attr('target', '_blank')
+    .html(args.display);
     return $link;
   };
 
@@ -114,13 +115,13 @@
     var ret = {};
     var prop, i;
     for (i in data) {
-	 if (i.indexOf(prefix_main) === 0) {
-	   prop = i.substring(prefix_main.length);
-	   ret[prop] = data[i];
-	 } else if (i.indexOf(prefix_secondary) === 0) {
-	   prop = i.substring(prefix_secondary.length);
-	   ret[prop] = data[i];
-	 }
+      if (i.indexOf(prefix_main) === 0) {
+        prop = i.substring(prefix_main.length);
+        ret[prop] = data[i];
+      } else if (i.indexOf(prefix_secondary) === 0) {
+        prop = i.substring(prefix_secondary.length);
+        ret[prop] = data[i];
+      }
     }
     return ret;
   };
@@ -129,164 +130,179 @@
     var services = args.services;
 
     return this.each(function () {
-	 var $this = $(this);
+      var $this = $(this);
 
-	 // Loop through the requested services
-	 for (var s in services) {
-	   var service = services[s];
-	   var name = service.name;
+      // Loop through the requested services
+      for (var s in services) {
+        var service = services[s];
+        var name = service.name;
 
-	   // See if the requested service has been added
-	   if (!(name in valid_services)) {
-		continue;
-	   }
-	   var srvc = $.extend({}, valid_services[name], service);
+        // See if the requested service has been added
+        if (!(name in valid_services)) {
+          continue;
+        }
+        var srvc = $.extend({}, valid_services[name], service);
 
-	   // Find the link for the requested service in the node
-	   var $link = $this.find('a.' + cpre + name + ', .' + cpre + name + ' a');
-	   // If none exists, create and append it
-	   if (!$link.length) {
-		$link = makeLink(srvc);
-		var $container = $this.find('.' + cpre + name);
-		if ($container.length) {
-		  $container.append($link);
-		} else {
-		  $this.append($link);
-		}
-	   }
+        // Find the link for the requested service in the node
+        var $link = $this.find('a.' + cpre + name + ', .' + cpre + name + ' a');
+        // If none exists, create and append it
+        if (!$link.length) {
+          $link = makeLink(srvc);
+          var $container = $this.find('.' + cpre + name);
+          if ($container.length) {
+            $container.append($link);
+          } else {
+            $this.append($link);
+          }
+        }
 
-	   // Add service specific arguments to the links'd data object
-	   for (var i in service) {
-		if (typeof service[i] === 'function')
-		  continue;
-		$link.data(dpre + name + i, service[i]);
-	   }
+        // Add service specific arguments to the links'd data object
+        for (var i in service) {
+          if (typeof service[i] === 'function')
+          continue;
+          $link.data(dpre + name + i, service[i]);
+        }
 
-	   // If you are simply updating the services, bail out
-	   if ($this.data(dpre + 'processed'))
-		continue;
+        // If you are simply updating the services, bail out
+        if ($this.data(dpre + 'processed'))
+        continue;
 
-	   // Set up link
-	   var data = $.extend({}, defaults, srvc, get_data($this, dpre + srvc.name, dpre), get_data($link, dpre + srvc.name, dpre));
-	   if (typeof data.prepare === 'function') {
-		data.prepare($link[0], data);
-	   }
+        // Set up link
+        var data = $.extend({}, defaults, srvc, get_data($this, dpre + srvc.name, dpre), get_data($link, dpre + srvc.name, dpre));
+        if (typeof data.prepare === 'function') {
+          data.prepare($link[0], data);
+        }
 
-	   // Bind an event to the link
-	   $link
-			 .on('touchstart click', (function (srvc, $parent, $lnk) {
-			   return function (e) {
-				setTimeout(function (e) {
-				  // TODO: reduce the code duplication
-				  var data = $.extend({}, defaults, args, srvc, get_data($parent, dpre + srvc.name, dpre), get_data($lnk, dpre + srvc.name, dpre));
-				  data.element = this;
+        // Bind an event to the link
+        $link
+        .on('touchstart click', (function (srvc, $parent, $lnk) {
 
-				  if (data.url == '{current}')
-				    data.url = document.location.href;
-				  if (data.url_append != undefined) {
-				    // TODO: more than just {{name}} replacement
-				    data.url_append = data.url_append.replace('{{name}}', data.name);
+          return function (e) {
+            //Disabled links are added during scroll
+            //This should prevent phantom clicks
+            if($lnk.hasClass('disabled')) {
+              return false;
+            }
 
-				    if (data.url.indexOf('?') !== -1 && data.url_append.indexOf('?') !== -1) {
-					 data.url_append = data.url_append.replace('?', '&');
-				    }
-				    data.url += data.url_append;
-				  }
+            // TODO: reduce the code duplication
+            var data = $.extend({}, defaults, args, srvc, get_data($parent, dpre + srvc.name, dpre), get_data($lnk, dpre + srvc.name, dpre));
+            data.element = this;
 
-				  srvc.share(data);
-				  $window.trigger(cpre + 'click', data);
-				  upsShare(data.name);
+            if (data.url == '{current}')
+            data.url = document.location.href;
+            if (data.url_append != undefined) {
+              // TODO: more than just {{name}} replacement
+              data.url_append = data.url_append.replace('{{name}}', data.name);
 
-				  return false;
-				}, 200);
-				e.preventDefault();
-			   }
+              if (data.url.indexOf('?') !== -1 && data.url_append.indexOf('?') !== -1) {
+                data.url_append = data.url_append.replace('?', '&');
+              }
+              data.url += data.url_append;
+            }
 
-			 })(srvc, $this, $link)
-				    );
+            //Call the share, Generate the link and open the share
+            var ret = srvc.share(data);
 
-	   if (typeof data.hoverfocus === 'function') {
-		$link
-			   .on('mouseover focus', (function (srvc, $parent, $lnk) {
-				return function (e) {
-				  var data = $.extend({}, defaults, args, srvc, get_data($parent, dpre + srvc.name, dpre), get_data($lnk, dpre + srvc.name, dpre));
+            //Delay for tracking
+            setTimeout(function(e) {
+              $window.trigger(cpre + 'click', data);
+              upsShare(data.name);
+            }, 200);
 
-				  if (data.url == '{current}')
-				    data.url = document.location.href;
-				  if (data.url_append != undefined) {
-				    // TODO: more than just {{name}} replacement
-				    data.url_append = data.url_append.replace('{{name}}', data.name);
+            //if in Desktop view just do popup/But in mobile do a link click
+            if($('.social-wrapper').hasClass('desktop') || !ret) {
+              e.preventDefault();
+              return false;
+            }
 
-				    if (data.url.indexOf('?') !== -1 && data.url_append.indexOf('?') !== -1) {
-					 data.url_append = data.url_append.replace('?', '&');
-				    }
-				    data.url += data.url_append;
-				  }
+            return ret;
+          }
 
-				  data.element = this;
+        })(srvc, $this, $link)
+      );
 
-				  srvc.hoverfocus(data);
-				}
-			   })(srvc, $this, $link)
-					 );
-	   }
-	 }
+      if (typeof data.hoverfocus === 'function') {
+        $link
+        .on('mouseover focus', (function (srvc, $parent, $lnk) {
+          return function (e) {
+            var data = $.extend({}, defaults, args, srvc, get_data($parent, dpre + srvc.name, dpre), get_data($lnk, dpre + srvc.name, dpre));
 
-	 $this.data(dpre + 'processed', true);
-	 $this.addClass(cpre + 'processed');
-    });
-  };
+            if (data.url == '{current}')
+            data.url = document.location.href;
+            if (data.url_append != undefined) {
+              // TODO: more than just {{name}} replacement
+              data.url_append = data.url_append.replace('{{name}}', data.name);
 
-  var valid_services = {};
+              if (data.url.indexOf('?') !== -1 && data.url_append.indexOf('?') !== -1) {
+                data.url_append = data.url_append.replace('?', '&');
+              }
+              data.url += data.url_append;
+            }
 
-  $.tpsocial = {
-    add_service: function (args) {
-	 valid_services[args.name] = args;
-    },
-    // Load script and run callback
-    queues: {},
-    onces: {},
-    load_script: function (test, url, context, callback, once) {
-	 if (test !== undefined) {
-	   callback.call(context);
-	   return true;
-	 }
+            data.element = this;
 
-	 if ($.tpsocial.queues[url] !== undefined) {
-	   $.tpsocial.queues[url].push(callback);
-	   return;
-	 }
-
-	 $.tpsocial.queues[url] = [];
-	 $.tpsocial.onces[url] = once;
-
-	 var ready = function (s) {
-	   // Use this in IE if we want to track throughout the load.
-	   if (s.readyState === 'loaded' || s.readyState === 'complete')
-		done();
-	 };
-
-	 var done = function () {
-	   for (var i in $.tpsocial.queues[url]) {
-		var cb = $.tpsocial.queues[url][i];
-		if (typeof cb === 'function')
-		  cb.call(context);
-	   }
-
-	   if (typeof $.tpsocial.onces[url] === 'function')
-		$.tpsocial.onces[url].call(context);
-	 };
-
-	 var s = document.createElement('script');
-	 s.type = "text/javascript";
-	 s.onreadystatechange = function (s) {
-	   return function () {
-		ready(s);
-	   };
-	 }(s);
-	 s.onerror = s.onload = done;
-	 s.src = url;
-	 document.getElementsByTagName('head')[0].appendChild(s);
+            srvc.hoverfocus(data);
+          }
+        })(srvc, $this, $link)
+      );
     }
-  };
+  }
+
+  $this.data(dpre + 'processed', true);
+  $this.addClass(cpre + 'processed');
+});
+};
+
+var valid_services = {};
+
+$.tpsocial = {
+  add_service: function (args) {
+    valid_services[args.name] = args;
+  },
+  // Load script and run callback
+  queues: {},
+  onces: {},
+  load_script: function (test, url, context, callback, once) {
+    if (test !== undefined) {
+      callback.call(context);
+      return true;
+    }
+
+    if ($.tpsocial.queues[url] !== undefined) {
+      $.tpsocial.queues[url].push(callback);
+      return;
+    }
+
+    $.tpsocial.queues[url] = [];
+    $.tpsocial.onces[url] = once;
+
+    var ready = function (s) {
+      // Use this in IE if we want to track throughout the load.
+      if (s.readyState === 'loaded' || s.readyState === 'complete')
+      done();
+    };
+
+    var done = function () {
+      for (var i in $.tpsocial.queues[url]) {
+        var cb = $.tpsocial.queues[url][i];
+        if (typeof cb === 'function')
+        cb.call(context);
+      }
+
+      if (typeof $.tpsocial.onces[url] === 'function')
+      $.tpsocial.onces[url].call(context);
+    };
+
+    var s = document.createElement('script');
+    s.type = "text/javascript";
+    s.onreadystatechange = function (s) {
+      return function () {
+        ready(s);
+      };
+    }(s);
+    s.onerror = s.onload = done;
+    s.src = url;
+    document.getElementsByTagName('head')[0].appendChild(s);
+  }
+};
 })(window, jQuery);
