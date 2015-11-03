0f533ab54caa0ebde76714fbdbdd7f2c50251a9d
Marc Goldstein <mgoldstein@takepart.com>
2015-11-03 11:18:31 -0800

    TP-3786: change simplereach to key off of UPS TP free tag

--- drupal/sites/all/modules/custom/tp_theme_support/js/tp_theme_support.js
+++ drupal/sites/all/modules/custom/tp_theme_support/js/tp_theme_support.js
@@ -4,32 +4,30 @@
    *  @function:
    *    Function is used to call the reach
    */
-  window.tp_reach_call = function(data) {
+  window.tp_reach_call = function (data) {
     //assigns the data to the reach config
     window['__reach_config'] = data;
-    
-    //break the function if it contains Business
+
+    //break the function if it contains "UPS TP"
     if (typeof __reach_config !== 'undefined' || typeof __reach_config.tags !== 'undefined') {
-      if ($.inArray('Business', __reach_config.tags) == -1) {
-        return;
-      }
+	 if ($.inArray('UPS TP', __reach_config.tags) === -1) {
+	   return;
+	 }
     }
-    
+
     //only make the call if the SPR has been defined on the page
     if (typeof SPR !== 'undefined') {
-      //call the collect and pass values
-      SPR.Reach.collect(__reach_config);
+	 //call the collect and pass values
+	 SPR.Reach.collect(__reach_config);
     }
     //otherwise we add the script to the page and then call
     else {
-      //code taken from implementation documentation
-      var s = document.createElement('script');
-      s.async = true;
-      s.type = 'text/javascript';
-      s.src = document.location.protocol + '//d8rk54i4mohrb.cloudfront.net/js/reach.js';
-      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(s);
-
+	 //code taken from implementation documentation
+	 var s = document.createElement('script');
+	 s.async = true;
+	 s.type = 'text/javascript';
+	 s.src = document.location.protocol + '//d8rk54i4mohrb.cloudfront.net/js/reach.js';
+	 (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(s);
     }
-  }
-
+  };
 })(jQuery, Drupal, this, this.document);
