d99e99791d985aba5d02fcc75f57af14ec968495
Scott Knight <sknight17@gmail.com>
2015-11-09 14:39:19 -0600

    TP-3670: menu styling

--- drupal/sites/all/themes/base/theme/base-header-transparent.tpl.php
+++ drupal/sites/all/themes/base/theme/base-header-transparent.tpl.php
@@ -3,14 +3,14 @@
   <?php print $variables['logo']; ?>
   <ul class="menu nav-right">
     <li>
-      <div class="user-menu fresh-theme">
-        <div class="tpsLogin"></div>
-      </div>
-    </li>
-    <li>
       <div class="search">
         <div class="toggle-search"><span class="icon i-search"></span></div>
         <?php print $variables['search']; ?>
+      </div>
+    </li>
+    <li>
+      <div class="user-menu fresh-theme">
+        <div class="tpsLogin"></div>
       </div>
     </li>
   </ul>
