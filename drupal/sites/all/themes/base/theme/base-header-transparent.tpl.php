7b5614815c2c74c8c22410dbe133f9699c25a66d
Scott Knight <sknight17@gmail.com>
2015-11-09 19:10:26 -0600

    TP-3670: setup for page slider

--- drupal/sites/all/themes/base/theme/base-header-transparent.tpl.php
+++ drupal/sites/all/themes/base/theme/base-header-transparent.tpl.php
@@ -1,5 +1,5 @@
 <div class="navbar-transparent">
-  <span class="icon i-hamburger"></span>
+  <span class="icon i-hamburger toggle-menu toggle-left"></span>
   <?php print $variables['logo']; ?>
   <ul class="menu nav-right">
     <li>
