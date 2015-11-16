28a1916591f73627e5817271fac31f5d50753c0c
Jacob Pitassi <jpitassi@stauffer.com>
2015-11-16 10:52:40 -0800

    TP-3821 Video Playlist Count

--- drupal/sites/all/modules/custom/tp_videos/templates/playlist-wrapper.tpl.php
+++ drupal/sites/all/modules/custom/tp_videos/templates/playlist-wrapper.tpl.php
@@ -1,5 +1,5 @@
 <div class="playlist-wrapper-wrapper">
-	<div class="playlist-wrapper"><?php $title = 'adfasf'; ?>
+	<div class="playlist-wrapper">
 		<?php if($title != null): ?>
 			<h2 class="playlist-title"><?php print $title; ?> <span class="playlist-count"><?php print count($playlist['#videos']); ?> VIDEOS</span></h2>
 		<?php endif; ?>
