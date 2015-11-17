16043990e50b9f02235cdcc36b3899b1308ae211
Saman Afshar <Saman@safshar-0650.local>
2015-11-16 18:12:00 -0800

    TP-3820 - adding username next to avatar on transparent nav

--- drupal/sites/all/themes/base/theme/base-header-transparent.tpl.php
+++ drupal/sites/all/themes/base/theme/base-header-transparent.tpl.php
@@ -2,11 +2,11 @@
   <div class="toggle-menu toggle-left"></div>
   <?php print $variables['logo']; ?>
   <ul class="menu nav-right">
-    <li class="search-item">
-      <div class="toggle-search"></div>
-    </li>
     <li class="user-menu fresh-theme">
       <div class="tpsLogin"></div>
+    </li>
+    <li class="search-item">
+      <div class="toggle-search"></div>
     </li>
   </ul>
   <div class = "search-close"></div>
