a25fd8b9ac12c84342efc773c6fe8760bbff2337
Jacob Pitassi <jpitassi@stauffer.com>
2015-12-01 11:17:33 -0800

    TP-3791 Preload tray background images

--- drupal/sites/all/modules/custom/tp_campaigns/js/tp_auto_campaign.js
+++ drupal/sites/all/modules/custom/tp_campaigns/js/tp_auto_campaign.js
@@ -47,13 +47,13 @@
     * On the load of the page I init my custom ajax call
     */
    $(document).ready(function() {
-     Drupal.ajax['autocampaign_ajax'].autoCampaign();
+     campaignPreload();
    });
 
+   //This is called through the ajax commands sent from the server.
    Drupal.behaviors.tp_auto_changer = {
      attach: function (context, settings) {
        $(document).delegate('article.node-campaign-page.view-mode-full', 'TAPInvoke', function(ev, data) {
-         console.log('TAPPING');
          if ( window.newTapWidgets == true ) {
            TP.Bootstrapper && new TP.Bootstrapper().start();
            TAP.Widget      && TAP.Widget.addWidgets();
@@ -61,5 +61,26 @@
          }
        });
      }
-   }
+   };
+
+   /**
+    * Preloading the background images
+    */
+   var campaignPreload = function() {
+     var imgs = Drupal.settings.campaignPreload;
+     window.preloaded = 0;
+     for(var i = 0; i < imgs.length; i++) {
+       window['campaignImage'+i] = new Image();
+       window['campaignImage'+i].src = imgs[i];
+       window['campaignImage'+i].onload = function(){
+         window.preloaded++;
+         //Check if all the images have been preloaded then let the trays load
+         if(window.preloaded == Drupal.settings.campaignPreload.length) {
+           Drupal.ajax['autocampaign_ajax'].autoCampaign();
+           delete(window.preloaded);
+         }
+       };
+     }
+   };
+
 })(jQuery, Drupal, this, this.document);
