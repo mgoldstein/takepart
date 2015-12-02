0efc10a878ee50e87350a3c185b9f03c99ea00cf
Jacob Pitassi <jpitassi@stauffer.com>
2015-12-01 17:58:31 -0800

    TP-3791 Campaign speed now with image preloading and viewport loading of trays

--- drupal/sites/all/modules/custom/tp_campaigns/js/tp_auto_campaign.js
+++ drupal/sites/all/modules/custom/tp_campaigns/js/tp_auto_campaign.js
@@ -13,6 +13,8 @@
     */
    Drupal.ajax.prototype.autoCampaign = function() {
      var ajax = this;
+     var traynumb = window.campaignTray + 2;
+     ajax.url = ajax.element_settings.url = ajax.options.url = '/autocampaign/'+Drupal.settings.autoloadCampaigns+'/'+traynumb;
 
      // Do not perform another ajax command if one is already in progress.
      if (ajax.ajaxing) {
@@ -22,6 +24,7 @@
      try {
        window.newTapWidgets = true;
        $.ajax(ajax.options);
+       window.campaignTray++;
      }
      catch (err) {
        alert('An error occurred while attempting to process ' + ajax.options.url);
@@ -35,7 +38,7 @@
     * Define a custom ajax action to load my autocampaign ids.
     */
    var custom_settings = {};
-   custom_settings.url = '/autocampaign/'+Drupal.settings.autoloadCampaigns;
+   custom_settings.url = '/autocampaign/'+Drupal.settings.autoloadCampaigns+'/0';
    custom_settings.event = 'onload';
    custom_settings.keypress = false;
    custom_settings.prevent = false;
@@ -63,24 +66,51 @@
      }
    };
 
-   /**
-    * Preloading the background images
-    */
-   var campaignPreload = function() {
-     var imgs = Drupal.settings.campaignPreload;
-     window.preloaded = 0;
-     for(var i = 0; i < imgs.length; i++) {
-       window['campaignImage'+i] = new Image();
-       window['campaignImage'+i].src = imgs[i];
-       window['campaignImage'+i].onload = function(){
-         window.preloaded++;
-         //Check if all the images have been preloaded then let the trays load
-         if(window.preloaded == Drupal.settings.campaignPreload.length) {
-           Drupal.ajax['autocampaign_ajax'].autoCampaign();
-           delete(window.preloaded);
-         }
-       };
-     }
-   };
+  /**
+  * Preloading the background images
+  */
+  var campaignPreload = function() {
+    var imgs = Drupal.settings.campaignPreload;
+    window.preloaded = 0;
+    //If there are no images to preload just load campaignOnload
+    if(window.preloaded >= Drupal.settings.campaignPreload.length) {
+      campaignOnload();
+    } else {
+      //Loop through image urls loading them into the browser cache
+      for(var i = 0; i < imgs.length; i++) {
+        window['campaignImage'+i] = new Image();
+        window['campaignImage'+i].src = imgs[i];
+        window['campaignImage'+i].onload = campaignOnload;
+      }
+    }
+  };
+
+  var campaignOnload = function() {
+    window.preloaded++;
+    //Check if all the images have been preloaded then let the trays load
+    if(window.preloaded >= Drupal.settings.campaignPreload.length) {
+      window.campaignTray = 0;
+      $(window).bind('scroll.campaignScroll', function(e) {
+        var wrap = $('article.node-campaign-page.view-mode-full'),
+          $win = $(window);
+        var wrapBot = wrap.offset().top + wrap.height();
+        var viewport = {
+          top : $win.scrollTop(),
+          left : $win.scrollLeft(),
+          bottom : $win.scrollTop() + $win.height()
+        };
+
+        if(viewport.bottom + 1000 >= wrapBot){
+          Drupal.ajax['autocampaign_ajax'].autoCampaign();
+        }
+        //garbage collection
+        if(Drupal.settings.campaignItemCount <= window.campaignTray) {
+          $(window).unbind('scroll.campaignScroll');
+          delete(window.campaignTray);
+        }
+      });
+      delete(window.preloaded);
+    }
+  }
 
 })(jQuery, Drupal, this, this.document);
