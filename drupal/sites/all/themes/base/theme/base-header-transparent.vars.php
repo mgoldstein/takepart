a19e5a4eb8eb8323054604fd1e0f78d705fbd6a1
Jacob Pitassi <jpitassi@stauffer.com>
2016-01-12 17:32:47 -0800

    TP-3910 Making the delta a variable that should be placed in the environment settings

--- drupal/sites/all/themes/base/theme/base-header-transparent.vars.php
+++ drupal/sites/all/themes/base/theme/base-header-transparent.vars.php
@@ -7,8 +7,8 @@ function base_preprocess_base_header_transparent(&$variables) {
   //Set logo
   $logo = l('', '<front>', array('attributes' => array('class' => array('logo-transparent'))));
   $variables['logo'] = $logo;
-
+  $block_delta = variable_get('search_api_page_block_delta',2);
   //Set Search
-  $search = module_invoke('search_api_page', 'block_view', '4');
+  $search = module_invoke('search_api_page', 'block_view', $block_delta);
   $variables['search'] = drupal_render($search['content']);
 }
