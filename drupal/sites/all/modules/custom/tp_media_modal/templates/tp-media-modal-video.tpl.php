5c511d520cd4c5e5064a5361641affd7456d3d7d
Scott Knight <sknight17@gmail.com>
2016-02-19 08:29:29 -0600

    TP-3906: campaign video modal- basic functionality

--- drupal/sites/all/modules/custom/tp_media_modal/templates/tp-media-modal-video.tpl.php
+++ drupal/sites/all/modules/custom/tp_media_modal/templates/tp-media-modal-video.tpl.php
@@ -1,6 +1,6 @@
 <span class="primary-button">
-  <a class="show-modal tplinkpos" data-show-modal="video-1" href="#"><?php print $button_content; ?></a>
+  <a class="show-modal tplinkpos" data-show-modal="video-<?php print $id; ?>" href="#"><?php print $button_content; ?></a>
 </span>
-<div class="modal-content" id="video-1">
-  <?php print $video; ?>
+<div class="modal-content" id="video-<?php print $id; ?>">
+	<div class="jwp" data-jwp-file="<?php print $video_file; ?>">&nbsp;</div>
 </div>
