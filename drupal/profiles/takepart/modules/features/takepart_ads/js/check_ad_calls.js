9c4fd211bcf30078378e5ef3da84602c6d746cb4
Marc Goldstein <mgoldstein@takepart.com>
2016-02-19 16:25:53 -0800

    TP-3998 - fix 320 on iOS
    TP-3998 - fix 320 on iOS, replace window.innerWidth with screen.width

--- drupal/profiles/takepart/modules/features/takepart_ads/js/check_ad_calls.js
+++ drupal/profiles/takepart/modules/features/takepart_ads/js/check_ad_calls.js
@@ -2,13 +2,7 @@ function ad_call_check(ad_width) {
   if (ad_width === 728 && window.innerWidth < 768) {
     return false;
   }
-  /*
-  else if (ad_width === 1 && window.innerWidth < 768) {
-    return false;
-  }
-  */
-  else if (ad_width === 320 && ((window.innerWidth > 768)
-    || (jQuery.cookie("close_mobile_ad") !== null))) {
+  else if (ad_width === 320 && ((screen.width > 768))) {
     return false;
   }
   else {
