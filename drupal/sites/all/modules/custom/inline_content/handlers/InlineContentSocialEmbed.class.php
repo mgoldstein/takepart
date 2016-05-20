480cc78c3776dff2e6dd747e2f935ac691ad55ed
Saman Afshar <Saman@safshar-0650.local>
2016-05-19 17:48:20 -0700

    TP-3929 - added validation for emojis to social embed and html block IR

--- drupal/sites/all/modules/custom/inline_content/handlers/InlineContentSocialEmbed.class.php
+++ drupal/sites/all/modules/custom/inline_content/handlers/InlineContentSocialEmbed.class.php
@@ -74,5 +74,11 @@ class InlineContentSocialEmbed extends InlineContentReplacementController {
        form_error($form['field_ic_fia_embed'] ,t('Only embeds from Twitter, Vine and Instagram are supported at this time.'));
     }
 
+   //Catch any emojis on the embed
+   if (preg_match('/([0-9#][\x{20E3}])|[\x{00ae}\x{00a9}\x{203C}\x{2047}\x{2048}\x{2049}\x{3030}\x{303D}\x{2139}\x{2122}\x{3297}\x{3299}][\x{FE00}-\x{FEFF}]?|[\x{2190}-\x{21FF}][\x{FE00}-\x{FEFF}]?|[\x{2300}-\x{23FF}][\x{FE00}-\x{FEFF}]?|[\x{2460}-\x{24FF}][\x{FE00}-\x{FEFF}]?|[\x{25A0}-\x{25FF}][\x{FE00}-\x{FEFF}]?|[\x{2600}-\x{27BF}][\x{FE00}-\x{FEFF}]?|[\x{2900}-\x{297F}][\x{FE00}-\x{FEFF}]?|[\x{2B00}-\x{2BF0}][\x{FE00}-\x{FEFF}]?|[\x{1F000}-\x{1F6FF}][\x{FE00}-\x{FEFF}]?/u', $embed_code)) {
+      form_error($form['field_ic_fia_embed'] , t('There were emojis detected in your embed field. Please manually delete the emojis from the field.'));
+    }
+
   }
+
 }
