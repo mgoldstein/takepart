37bd7e881145903e9be6c55f09db73fa84fe17c9
Lucas K Allmon <LucasKA@users.noreply.github.com>
2016-08-05 16:05:02 -0700

    Gallery Component (#1397)

--- drupal/sites/all/themes/fresh/js/fresh-gallery.js
+++ drupal/sites/all/themes/fresh/js/fresh-gallery.js
@@ -5,5 +5,20 @@
  */
 
 (function ($, Drupal, window, document, undefined) {
+  $(document).ready(function(){
+
+    var galleryData = {
+      "title": $('.node-fresh-gallery').attr('data-tp-og-title'),
+      "adTag": "TP_ROS_728x90_1_Responsive",
+      "adFrequency": 2
+    };
+
+    var jsonId = $('.node-fresh-gallery').attr('data-ddl-page-id');
+    galleryData.photos = eval('gallery_' + jsonId + '_json');
+
+    var galleryElement = $(".gallery-wrapper").get(0);
+
+    showImageGallery(galleryData, galleryElement);
+  });
 
 })(jQuery, Drupal, this, this.document);
