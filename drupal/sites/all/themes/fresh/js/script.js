6c4507bfe9e7a8abbd70f1012372ee95bb83ecdf
Jacob Pitassi <jpitassi@stauffer.com>
2016-09-06 13:23:08 -0700

    highlight text POC

--- drupal/sites/all/themes/fresh/js/script.js
+++ drupal/sites/all/themes/fresh/js/script.js
@@ -192,6 +192,37 @@ var Environment = {
     //TODO: Uncomment once we review how it behaves with Ad
     //$('#page-wrapper.campaign-experience #header').addClass('mobile-ad-loaded');
 
+    $(document.body).on('mouseup', function (evt) {
+      var menu = $('#highlight_menu');
+      var s = window.getSelection(),
+        r = s.getRangeAt(0);
+
+      if (r && s.toString()) {
+        var p = r.getBoundingClientRect();
+
+        if (p.left || p.top) {
+          menu.css({
+            left: (p.left + (p.width / 2)) - (menu.width() / 2),
+            top: ((p.top - menu.height() - 10)+$(window).scrollTop()),
+            display: 'block',
+            opacity: 0
+        })
+        .animate({
+          opacity:1
+        }, 300);
+
+        setTimeout(function() {
+          menu.addClass('highlight_menu_animate');
+        }, 10);
+        return;
+      }
+      }
+      menu.animate({ opacity:0 }, function () {
+        menu.hide().removeClass('highlight_menu_animate');
+      });
+    });
+
+
   });
 
   /**
@@ -334,7 +365,7 @@ var Environment = {
     }
   };
   */
- 
+
   /*
    * Create ambient video on feature article
    */
--- drupal/sites/all/themes/fresh/theme/system/page.tpl.php
+++ drupal/sites/all/themes/fresh/theme/system/page.tpl.php
@@ -34,3 +34,13 @@
   <?php print render($page['footer']); ?>
 </div>
 <?php print render($page['page_bottom']); ?>
+<div id="highlight_menu" style="display:none;">
+
+  <ul class="side-by-side">
+    <li><a target="_blank" href="https://www.facebook.com/dialog/feed?app_id=247137505296280&display=popup&amp;caption=An%20example%20caption&link=https%3A%2F%2Fwww.takepart.com&redirect_uri=http://www.takepart.com"><img width="20px" src="https://cdn1.iconfinder.com/data/icons/logotypes/32/square-facebook-512.png"></a></li>
+    <li><a target="_blank" href="https://twitter.com/intent/tweet?url=http%3A%2F%2Ftakepart.local%2Farticle%2F2016%2F06%2F14%2Frace-fix-airbnb-while-black-problem%3Fcmpid%3Dorganic-share-twitter&amp;via=TakePart&amp;text=The%20Race%20to%20Fix%20the%20Airbnb%20While%20Black%20Problem"><img width="20px" src="https://cdn1.iconfinder.com/data/icons/logotypes/32/square-twitter-512.png"></a></li>
+  </ul>
+
+  <div class="caret"></div>
+
+</div>
