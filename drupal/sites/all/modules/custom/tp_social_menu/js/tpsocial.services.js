66e40ff404e8e8650cb9b21b1d8cf720a6326cb4
Jacob Pitassi <jpitassi@stauffer.com>
2016-09-15 18:12:16 -0700

    TP-4530 changing the quote image

--- drupal/sites/all/modules/custom/tp_social_menu/js/tpsocial.services.js
+++ drupal/sites/all/modules/custom/tp_social_menu/js/tpsocial.services.js
@@ -151,10 +151,9 @@
              method: 'feed',
              display: 'popup',
              link: url,
-             description: args.description,
+             description: args.caption,
              name: args.share_title,
-             caption: args.caption,
-             picture: 'http://res.cloudinary.com/'+Drupal.settings.cloudinary_bucket+'/image/upload/g_north,x_0,y_120,w_'+text_width+',c_fit,l_text:Libre%20Baskerville_36_left_line_spacing_8:'+encodeURI(args.description).replace(/,/g, "%E2%80%9A").replace(/\?/g,"%253F")+'/l_text:arial_20:%20,g_south,x_0,y_-20/ar_1.91,c_fill/quote_text.jpg'
+             picture: 'http://res.cloudinary.com/'+Drupal.settings.cloudinary_bucket+'/image/upload/g_north,x_0,y_120,w_'+text_width+',c_fit,l_text:Libre%20Baskerville_36_left_line_spacing_8:'+encodeURI(args.description).replace(/,/g, "%E2%80%9A").replace(/\?/g,"%253F")+'/l_text:arial_20:%20,g_south,x_0,y_-20/ar_1.91,c_fill/g_north,y_30,l_logo_ol/blank_quote_canvas.jpg'
            },
              function (response) {
                if (response && response.post_id) {
