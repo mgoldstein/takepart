303ba81023109f061616709eeae1a56a0512deba
Jacob Pitassi <jpitassi@stauffer.com>
2016-09-19 10:52:33 -0700

    TP-4531 fixing feature spacing and spacing on the text to image

--- drupal/sites/all/modules/custom/tp_social_menu/js/tpsocial.services.js
+++ drupal/sites/all/modules/custom/tp_social_menu/js/tpsocial.services.js
@@ -146,14 +146,14 @@
     	 get_share_url(args.url, args.title, function (url) {
     	   var parser = document.createElement("a");
     	   parser.href = args.url;
-         var text_width = args.description.length + 450;
+         var text_width = args.description.length + 650;
            FB.ui({
              method: 'feed',
              display: 'popup',
              link: url,
              description: args.caption,
              name: args.share_title,
-             picture: 'http://res.cloudinary.com/'+Drupal.settings.cloudinary_bucket+'/image/upload/g_north,x_0,y_120,w_'+text_width+',c_fit,l_text:Libre%20Baskerville_36_left_line_spacing_8:'+encodeURI(args.description).replace(/,/g, "%E2%80%9A").replace(/\?/g,"%253F")+'/l_text:arial_20:%20,g_south,x_0,y_-20/ar_1.91,c_fill/g_north,y_30,l_logo_ol/blank_quote_canvas.jpg'
+             picture: 'http://res.cloudinary.com/'+Drupal.settings.cloudinary_bucket+'/image/upload/g_north,x_0,y_120,w_'+text_width+',c_fit,l_text:Libre%20Baskerville_36_left_line_spacing_8:'+encodeURI(args.description).replace(/,/g, "%E2%80%9A").replace(/\?/g,"%253F")+'/l_text:arial_20:%20,g_south,x_0,y_-120/ar_1.91,c_fill/g_north,y_35,l_logo200/blank_quote_canvas.jpg'
            },
              function (response) {
                if (response && response.post_id) {
