a9bdea8fc3180364884812d803aa901c5a61a193
Marc Goldstein <mgoldstein@takepart.com>
2016-09-30 16:45:37 -0700

    What happens when we upgrade TP4 to jquery2.x

--- drupal/sites/all/themes/tp4/template.php
+++ drupal/sites/all/themes/tp4/template.php
@@ -110,7 +110,8 @@ function tp4_js_alter(&$javascript) {
   }
 
   /* Update our version of jQuery to 2.x */
-  $jquery_path = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
+  // $jquery_path = 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js';
+  $jquery_path = 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js';
   $javascript['misc/jquery.js']['data'] = $jquery_path;
   $javascript['misc/jquery.js']['type'] = 'external';
   unset($javascript['sites/all/libraries/colorbox/colorbox/jquery.colorbox-min.js']);
